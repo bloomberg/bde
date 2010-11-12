48186fcbfda39b1373eb80dad86d01dd4fe29c4d
mgiroux <mgiroux>
2010-11-12 08:44:58 -0500

    Converted package bde+pcre version BLP_LIB_BDE_BDE_2.4.0 from clearcase

---
+++ groups/bde/bde+pcre/test/pcretest1.cpp
@@ -0,0 +1,24 @@
+#include <cstring>
+
+extern int auxiliary_main(int argc, char **argv);
+
+const char *TEST_INPUT_FILE =
+                    "/bbcm/infrastructure/groups/bde/bde+pcre/test/testinput1";
+
+int main(int argc, char **argv)
+{
+    if (1 == argc) {
+        char *localArgv[2];
+        localArgv[0] = new char[std::strlen(argv[0]) + 1];
+        std::strcpy(localArgv[0], argv[0]);
+        localArgv[1] = new char[std::strlen(TEST_INPUT_FILE) + 1];
+        std::strcpy(localArgv[1], TEST_INPUT_FILE);
+        int ret = auxiliary_main(argc + 1, localArgv);
+        delete [] localArgv[0];
+        delete [] localArgv[1];
+        return ret;
+    }
+    else {
+        return auxiliary_main(argc, argv);
+    }
+}
