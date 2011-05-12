90f4ade909d5c8360d2cc85c1170f72b54105495
Mike Giroux <mgiroux@bloomberg.net>
2011-05-12 13:33:29 -0400

    import groups/bsl/bsl+bslhdrs/test/bsl_string.t.cpp from clearcase dev branch

--- groups/bsl/bsl+bslhdrs/test/bsl_string.t.cpp
+++ groups/bsl/bsl+bslhdrs/test/bsl_string.t.cpp
@@ -1,7 +1,17 @@
 #include <bsl_string.h>
 #include <string>
+
 #ifdef std
 #   error std was not expected to be a macro
 #endif
+
 namespace std { }
-int main() { return 0; }
+
+int main()
+{
+    // make sure 'bslstl_StringArgument' is included by 'bsl_string.h'
+    BloombergLP::bslstl_StringArgument *p = NULL;
+    (void) p;
+    return 0;
+}
+
