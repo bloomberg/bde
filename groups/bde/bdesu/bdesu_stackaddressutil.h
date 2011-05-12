206a8ea5f4d6d8036c2404dff2c6faa043b1929b
Mike Giroux <mgiroux@bloomberg.net>
2011-05-12 13:31:47 -0400

    import groups/bde/bdesu/bdesu_stackaddressutil.h from clearcase dev branch

---
+++ groups/bde/bdesu/bdesu_stackaddressutil.h
@@ -0,0 +1,69 @@
+// bdesu_stackaddressutil.h                                           -*-C++-*-
+#ifndef INCLUDED_BDESU_STACKADDRESSUTIL
+#define INCLUDED_BDESU_STACKADDRESSUTIL
+
+#ifndef INCLUDED_BDES_IDENT
+#include <bdes_ident.h>
+#endif
+BDES_IDENT("$Id: $")
+
+//@PURPOSE: Provide a utility for obtaining return addresses from the stack
+//
+//@CLASSES:
+//   bdesu_StackAddressUtil: utilities for obtaining addresses from the stack
+//
+//@SEE_ALSO: bdesu_stacktrace, bdesu_stacktraceframe
+//
+//@AUTHOR: Oleg Semenov, Bill Chapman
+//
+//@DESCRIPTION: This component provides namespace containing a function that
+// populates an array with an ordered sequence of return addresses from the
+// current thread's function call stack.  Each return address points to the
+// (text) memory location of the first instruction to be executed upon
+// returning from a called routine.
+
+#ifndef INCLUDED_BDESCM_VERSION
+#include <bdescm_version.h>
+#endif
+
+                        // ============================
+                        // class bdesu_StackAddressUtil
+                        // ============================
+
+namespace BloombergLP {
+
+struct bdesu_StackAddressUtil {
+    // This struct provides a namespace for the function to obtain return
+    // addresses from the stack.
+
+    // CLASS METHODS
+    static
+    int getStackAddresses(void   **buffer,
+                          int      maxFrames);
+        // Get an ordered sequence of return addresses from the current
+        // thread's function call stack and load them into the specified array
+        // 'buffer', which is at least the specified 'maxFrames' in length.  A
+        // return address is an address stored on the stack that points to the
+        // first instruction that will be executed after the called subroutine
+        // returns.  If there are more than 'maxFrames' frames on the stack,
+        // only the return addresses for the 'maxFrames' most recent routine
+        // calls are stored.  When this routine completes, 'buffer' will
+        // contain an ordered sequence of return addresses, sorted such that
+        // recent calls occur in the array before calls which took place before
+        // them.  Return the number of stack frames stored into 'buffer' on
+        // success, and a negative value otherwise.  The behavior is undefined
+        // unless 'buffer' has room for at least 'maxFrames' addresses.
+};
+
+}  // close namespace BloombergLP
+
+#endif
+
+// ---------------------------------------------------------------------------
+// NOTICE:
+//      Copyright (C) Bloomberg L.P., 2010
+//      All Rights Reserved.
+//      Property of Bloomberg L.P. (BLP)
+//      This software is made available solely pursuant to the
+//      terms of a BLP license agreement which governs its use.
+// ----------------------------- END-OF-FILE ---------------------------------
