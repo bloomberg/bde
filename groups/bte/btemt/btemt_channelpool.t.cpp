c1e679f89d02f2eba68992a49bd2602f94ced9d5
Rohan Bhindwale <rbhindwale@bloomberg.net>
2011-05-19 13:56:27 -0400

    adding test case to reproduce bug

--- groups/bte/btemt/btemt_channelpool.t.cpp
+++ groups/bte/btemt/btemt_channelpool.t.cpp
@@ -623,6 +623,216 @@ void populateMessage(bcema_Blob      *msg,
 }
 
 //-----------------------------------------------------------------------------
+// CASE 36
+//-----------------------------------------------------------------------------
+
+namespace CASE36 {
+
+Obj       *channelPool;
+int        channelId;
+const int  CACHE_HI_WAT = 6000000;
+
+bcema_PooledBlobBufferFactory blobFactory(256);
+
+int delayBetweenWrites = 1000000;
+int maxWritesPerThread = 10000;
+int maxConsecutiveFailures = 1;
+int minMsgSize = 100;
+int maxMsgSize = 32000;
+
+bcemt_Barrier *barrier;
+
+void poolStateCb(int            state,
+                 int            source,
+                 int            severity)
+{
+    if (veryVerbose) {
+        bcemt_LockGuard<bcemt_Mutex> guard(&coutMutex);
+        bsl::cout << "Pool state callback called with"
+                  << " State: " << state
+                  << " Source: "  << source
+                  << " Severity: " << severity << bsl::endl;
+    }
+}
+
+void channelStateCb(int              channelId,
+                    int              serverId,
+                    int              state,
+                    void            *arg,
+                    int             *id,
+                    bcemt_Barrier   *barrier)
+{
+    if (veryVerbose) {
+        bcemt_LockGuard<bcemt_Mutex> guard(&coutMutex);
+        bsl::cout << "Channel state callback called with"
+                  << " Channel Id: " << channelId
+                  << " Server Id: "  << serverId
+                  << " State: " << state << bsl::endl;
+    }
+    if (btemt_ChannelPool::BTEMT_CHANNEL_UP == state) {
+        *id = channelId;
+        barrier->wait();
+    }
+}
+
+void blobBasedReadCb(int             *needed,
+                     bcema_Blob      *msg,
+                     int              channelId,
+                     void            *arg)
+{
+    if (veryVerbose) {
+        bcemt_LockGuard<bcemt_Mutex> guard(&coutMutex);
+        bsl::cout << "Blob Based Read Cb called with"
+                  << " Channel Id: " << channelId
+                  << " of length: "  << msg->length() << bsl::endl;
+    }
+    *needed = 1;
+    msg->removeAll();
+}
+
+struct TestData {
+  Obj           *d_pool_p;
+  int            d_channelId;
+  bcemt_Barrier *d_barrier_p;
+  bcema_Blob    *d_blob_p;
+};
+
+int write(int nBytes)
+{
+    bcema_Blob blob(&blobFactory);
+    blob.setLength(nBytes);
+    return channelPool->write(channelId, blob);
+}
+
+enum  {
+    ERROR_IMPOSSIBLE_WRITE_SUCCEEDED = 1,
+    ERROR_MAX_POSSIBLE_WRITE_FAILED = 2
+};
+
+const char* checkRcToString(int type)
+{
+    #define CASE(T)	    case T: return #T
+    switch (type) {
+      CASE(ERROR_IMPOSSIBLE_WRITE_SUCCEEDED);
+      CASE(ERROR_MAX_POSSIBLE_WRITE_FAILED);
+    };
+    #undef CASE
+
+    BSLS_ASSERT_OPT(0);
+    return "UNKNOWN";
+}
+
+int check()
+{
+    int rc;
+
+    // try to write too much
+    rc = write(CACHE_HI_WAT + 1);
+    LOOP_ASSERT(rc, rc);
+    if (!rc) return ERROR_IMPOSSIBLE_WRITE_SUCCEEDED;
+
+    // try to write max possible
+    rc = write(CACHE_HI_WAT);
+    LOOP_ASSERT(rc, !rc);
+    if (rc) return ERROR_MAX_POSSIBLE_WRITE_FAILED;
+
+    return 0;
+}
+
+void delay(int delay)
+{
+    int i;
+    for (i = 0; i < delay; ++i);
+    BSLS_ASSERT_OPT(i); // poor way to stop optimize-away
+}
+
+// signal allow writer threads to start writing from the same time (to
+// increase concurrency), barrier could be another way but might be too heavy.
+static bces_AtomicInt sig = 0;
+void signalerThread()
+{
+    while(1) {
+        ++sig;
+
+        delay(delayBetweenWrites);
+    }
+}
+
+void writerThread(unsigned threadIndex)
+{
+    int failures = 0;
+    int consecutiveFailures = 0;
+    int iter;
+    int oldSignal = 0;
+
+    bcema_Blob blob(&blobFactory);
+    for (iter = 0; iter < maxWritesPerThread && 
+                   consecutiveFailures < maxConsecutiveFailures; ++iter) {
+        int randVal = rand_r(&threadIndex);
+        int nBytes  = minMsgSize + randVal % (maxMsgSize - minMsgSize + 1);
+        blob.setLength(nBytes);
+
+//         MTCOUT << "Waiting" << MTENDL;
+
+        while(sig <= oldSignal);
+        oldSignal = sig;
+
+//         MTCOUT << "Before write" << MTENDL;
+
+        int rc = channelPool->write(channelId, blob);
+        LOOP_ASSERT(rc, !rc);
+
+        if (rc == 0) {
+            consecutiveFailures = 0;
+        }
+        else {
+            ++consecutiveFailures;
+            ++failures;
+        }
+    }
+
+    barrier->wait();
+}
+
+void success()
+{
+    std::cout << "no bug found" << std::endl;
+    _exit(0);
+}
+
+bcema_PooledBlobBufferFactory f(1024);
+extern "C" void* threadFunctionCase36(void *data)
+{
+    TestData      *testData = (TestData *) data;
+    Obj           *pool     = testData->d_pool_p;
+    int            id       = testData->d_channelId;
+    bcemt_Barrier *barrier  = testData->d_barrier_p;;
+    
+    bcema_Blob b(&f);
+    b.setLength(CACHE_HI_WAT);
+
+    barrier->wait();
+
+    int rc = pool->write(id, b);
+    LOOP_ASSERT(rc, !rc);
+
+    barrier->wait();
+
+//     int selfId = bcemt_ThreadUtil::selfAsId();
+//     int randSize = rand_r(&selfId);
+//     b.setLength(randSize);
+
+//     int rc = pool->write(id, b);
+//     LOOP_ASSERT(rc, !rc);
+
+//     barrier->wait();
+
+    return 0;
+}
+
+}
+
+//-----------------------------------------------------------------------------
 // CASE 35
 //-----------------------------------------------------------------------------
 
@@ -7508,6 +7718,134 @@ int main(int argc, char *argv[])
 #endif
 
     switch (test) { case 0:  // Zero is always the leading case.
+      case 36: {
+        // --------------------------------------------------------------------
+        // REPRODUCING DRQS 25245489
+        //
+        // Concerns:
+        //
+        // Plan:
+        //
+        // Testing:
+        // --------------------------------------------------------------------
+
+        if (verbose) cout << "\nDRQS 25245489"
+                          << "\n============="
+                          << endl;
+
+        using namespace CASE36;
+
+        btemt_ChannelPoolConfiguration config;
+        config.setMaxThreads(1);
+        config.setMaxConnections(100);
+        config.setReadTimeout(5.0);       // in seconds
+        config.setMetricsInterval(10.0);  // seconds
+        config.setWriteCacheWatermarks(0, CACHE_HI_WAT);
+        if (verbose) {
+          P(config);
+        }
+
+        bcemt_Barrier channelCbBarrier(2);
+        btemt_ChannelPool::ChannelStateChangeCallback channelCb(
+                                       bdef_BindUtil::bind(&channelStateCb,
+                                                           _1, _2, _3, _4,
+                                                           &channelId,
+                                                           &channelCbBarrier));
+
+        btemt_ChannelPool::PoolStateChangeCallback poolCb(&poolStateCb);
+
+        btemt_ChannelPool::BlobBasedReadCallback dataCb(&blobBasedReadCb);
+
+        const int SIZE = 1024;
+        bcema_PooledBlobBufferFactory f(SIZE);
+        bcema_Blob b(&f);
+        b.setLength(CACHE_HI_WAT);
+
+        btemt_ChannelPool mX(channelCb, dataCb, poolCb, config);
+        btemt_ChannelPool mY(channelCb, dataCb, poolCb, config);
+
+        channelPool = &mY;
+
+        ASSERT(0 == mX.start());
+        ASSERT(0 == mY.start());
+
+        bteso_IPv4Address serverAddr;
+        const int SOURCE_ID = 1700;
+        const int SERVER_ID = 1701;
+
+        ASSERT(0 == mX.listen(serverAddr, 5, SERVER_ID));
+
+        ASSERT(0 == mX.getServerAddress(&serverAddr, SERVER_ID));
+
+        int rc = mY.connect(serverAddr,
+                            5,
+                            bdet_TimeInterval(1),
+                            SOURCE_ID);
+        ASSERT(!rc);
+
+        channelCbBarrier.wait();
+        channelCbBarrier.wait();
+
+        const int NT = 2;
+        bcemt_Barrier currBarrier(NT + 1);
+        barrier = &currBarrier;
+
+        rc = check();
+        ASSERT(!rc);
+
+        // signalerThread
+        bcemt_ThreadUtil::Handle handle;
+        rc = bcemt_ThreadUtil::create(&handle, &signalerThread);
+        ASSERT(!rc);
+
+        // fork threads
+        for (int i = 0; i < NT; ++i) {
+          bcemt_ThreadUtil::Handle handle;
+          rc = bcemt_ThreadUtil::create(&handle, 
+                                        bdef_BindUtil::bind(&writerThread, i));
+          ASSERT(!rc);
+        }
+
+        barrier->wait();
+
+        // quick check
+        bcemt_ThreadUtil::sleep(bdet_TimeInterval(1)); // let flush complete
+                                                       // if any
+        rc = check();
+        ASSERT(!rc); // never returns
+
+        // more thorough check
+        bcemt_ThreadUtil::sleep(bdet_TimeInterval(10)); // in case flush did
+                                                        // not complete
+
+        rc = check();
+        ASSERT(!rc); // never returns
+
+        bcemt_ThreadUtil::sleep(bdet_TimeInterval(10));  // in case flush did
+                                                         // not complete
+
+        rc = check();
+        ASSERT(!rc); // never returns
+
+        // Following veriy that the bug is due to d_writeCacheSize mess-up
+        bcemt_ThreadUtil::sleep(bdet_TimeInterval(10));  // in case flush did
+                                                         // not complete
+        if (ERROR_MAX_POSSIBLE_WRITE_FAILED) {
+          rc = channelPool->setWriteCacheHiWatermark(channelId,
+                                                     CACHE_HI_WAT * 2);
+          ASSERT(!rc);
+          
+          rc = write(CACHE_HI_WAT);
+          ASSERT(!rc);
+        }
+        else {
+          rc = channelPool->setWriteCacheHiWatermark(channelId, 1);
+          ASSERT(!rc);
+          
+          rc = write(CACHE_HI_WAT + 1);
+          ASSERT(rc);
+        }
+      } break;
       case 35: {
         // --------------------------------------------------------------------
         // TESTING 'connect' & 'listen' with SocketOpts and clientAddress
