4f40d1c5d2f5d1397a0b9be3c807c53fb32c3a59
Bill Chapman <bchapman2@bloomberg.net>
2011-06-14 13:44:03 -0400

    Save to switch

---
+++ groups/bce/bcemt/bcemt_default.t.cpp
@@ -0,0 +1,112 @@
+// bcemt_default.t.cpp                                                -*-C++-*-
+#include <bcemt_default.h>
+
+#include <bsl_cstdlib.h>  // atoi()
+#include <bsl_iostream.h>
+#include <bsl_limits.h>   // INT_MAX
+
+using namespace BloombergLP;
+using namespace bsl;  // automatically added by script
+
+//=============================================================================
+//                             TEST PLAN
+//-----------------------------------------------------------------------------
+//                              OVERVIEW
+//                              --------
+// This program tests the functionality of the 'bcemt_Default' class.
+//
+//-----------------------------------------------------------------------------
+// [1] Breathing Test
+//=============================================================================
+//                    STANDARD BDE ASSERT TEST MACRO
+//-----------------------------------------------------------------------------
+
+static int testStatus = 0;
+
+static void aSsErT(int c, const char *s, int i) {
+    if (c) {
+        cout << "Error " << __FILE__ << "(" << i << "): " << s
+             << "    (failed)" << endl;
+        if (testStatus >= 0 && testStatus <= 100) ++testStatus;
+    }
+}
+# define ASSERT(X) { aSsErT(!(X), #X, __LINE__); }
+
+//=============================================================================
+//                  STANDARD BDE LOOP-ASSERT TEST MACROS
+//-----------------------------------------------------------------------------
+
+#define LOOP_ASSERT(I,X) { \
+    if (!(X)) { cout << #I << ": " << I << "\n"; aSsErT(1, #X, __LINE__);}}
+
+#define LOOP2_ASSERT(I,J,X) { \
+    if (!(X)) { cout << #I << ": " << I << "\t" << #J << ": " \
+              << J << "\n"; aSsErT(1, #X, __LINE__); } }
+
+#define LOOP3_ASSERT(I,J,K,X) { \
+   if (!(X)) { cout << #I << ": " << I << "\t" << #J << ": " << J << "\t" \
+              << #K << ": " << K << "\n"; aSsErT(1, #X, __LINE__); } }
+
+//=============================================================================
+//                  SEMI-STANDARD TEST OUTPUT MACROS
+//-----------------------------------------------------------------------------
+
+#define P(X) cout << #X " = " << (X) << endl; // Print identifier and value.
+#define Q(X) cout << "<| " #X " |>" << endl;  // Quote identifier literally.
+#define P_(X) cout << #X " = " << (X) << ", " << flush; // P(X) without '\n'
+#define L_ __LINE__                           // current Line number
+
+//=============================================================================
+//                  GLOBAL TYPEDEFS/CONSTANTS FOR TESTING
+//-----------------------------------------------------------------------------
+
+typedef bcemt_Default Obj;
+
+//=============================================================================
+//                              MAIN PROGRAM
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    int test = argc > 1 ? atoi(argv[1]) : 0;
+    int verbose = argc > 2;
+    // int veryVerbose = argc > 3;
+    // int veryVeryVerbose = argc > 4;
+
+    cout << "TEST " << __FILE__ << " CASE " << test << endl;
+
+    switch (test) { case 0:
+      case 1: {
+        // --------------------------------------------------------------------
+        // BREATHING TEST
+        //
+        // Concern:
+        //   That 'defaultThreadStackSize' equals 'nativeThreadStackSize'.
+        // --------------------------------------------------------------------
+
+        if (verbose) cout << "BREATHING TEST\n"
+                             "==============\n";
+
+        const size_t dflt = Obj::defaultThreadStackSize();
+        ASSERT(Obj::nativeDefaultThreadStackSize() == dflt);
+
+        ASSERT(dflt > 0);
+        ASSERT(dflt < INT_MAX);
+      } break;
+      default: {
+        cerr << "WARNING: CASE `" << test << "' NOT FOUND." << endl;
+        testStatus = -1;
+      }
+    }
+
+    return( testStatus );
+}
+
+// ---------------------------------------------------------------------------
+// NOTICE:
+//      Copyright (C) Bloomberg L.P., 2002
+//      All Rights Reserved.
+//      Property of Bloomberg L.P. (BLP)
+//      This software is made available solely pursuant to the
+//      terms of a BLP license agreement which governs its use.
+// ----------------------------- END-OF-FILE ---------------------------------
