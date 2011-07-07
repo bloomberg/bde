e48336a2c8cf9917690c30420ddf918ac682fd88
Alisdair Meredith <ameredith1@bloomberg.net>
2011-07-07 12:29:44 -0400

    WIP adding files for bsls_unspecifiedbool refactoring

---
+++ groups/bsl/bsls/bsls_unspecifiedbool.cpp
@@ -0,0 +1,14 @@
+// bsls_unspecifiedbool.cpp                                          -*-C++-*-
+#include <bsls_unspecifiedbool.h>
+
+#include <bsls_ident.h>
+BSLS_IDENT("$Id$ $CSID$")
+
+// ---------------------------------------------------------------------------
+// NOTICE:
+//      Copyright (C) Bloomberg L.P., 2011
+//      All Rights Reserved.
+//      Property of Bloomberg L.P. (BLP)
+//      This software is made available solely pursuant to the
+//      terms of a BLP license agreement which governs its use.
+// ----------------------------- END-OF-FILE ---------------------------------
