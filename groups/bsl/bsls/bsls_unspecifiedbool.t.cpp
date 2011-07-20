74ca19400bcd9b196c837a9cc02540b4a7a17e11
Alisdair Meredith <ameredith1@bloomberg.net>
2011-07-20 16:19:56 -0400

    Completed test case implementations (but not usage example)

--- groups/bsl/bsls/bsls_unspecifiedbool.t.cpp
+++ groups/bsl/bsls/bsls_unspecifiedbool.t.cpp
@@ -4,9 +4,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>     // atoi()
-#include <string.h>     // memcpy()
-
-//#define TEST_FOR_COMPILE_ERRORS
 
 using namespace BloombergLP;
 
@@ -30,11 +27,12 @@ using namespace BloombergLP;
 // aliasing machinery works as documented.  At last, we must also check that
 // a 'bdema_ManagedPtr' acts exactly as a pointer wherever one is expected.
 //-----------------------------------------------------------------------------
-// [ 2] Basic functionality
+// [ 3] BoolType falseValue();
+// [ 3] BoolType trueValue();
+// [ 2] typedef BoolType 
 //-----------------------------------------------------------------------------
 // [ 1] BREATHING TEST
-// [ 3] USAGE EXAMPLE
-// [-1] VERIFYING FAILURES TO COMPILE
+// [ 4] USAGE EXAMPLE
 
 namespace {
 
@@ -160,7 +158,7 @@ namespace USAGE_EXAMPLE {
       operator BoolType() const {
           return d_ptr
                ? bsls_UnspecifiedBool<simple_pointer>::trueValue()
-               : false;
+               : bsls_UnspecifiedBool<simple_pointer>::falseValue();
       }
 //..
 // blah...
@@ -197,7 +195,7 @@ int main(int argc, char *argv[])
 
 
     switch (test) { case 0:
-      case 3: {
+      case 4: {
         // --------------------------------------------------------------------
         // TESTING USAGE EXAMPLE
         // Concerns:
@@ -211,42 +209,190 @@ int main(int argc, char *argv[])
         // Testing:
         //   USAGE EXAMPLE
         // --------------------------------------------------------------------
+
+        if (verbose) printf("\nTESTING usage example"
+                            "\n---------------------\n");
+
           USAGE_EXAMPLE::runTests();
+      } break;
+      case 3: {
+        // --------------------------------------------------------------------
+        // TESTING true and false values
+        // Concerns:
+        //: 1 The static member functions 'trueValue' and 'falseValue' each
+        //:   return a value of type BoolType.
+        //: 2 'falseValue' returns a value that converts to the 'bool' value
+        //:   'false'
+        //: 3 'trueValue' returns a value that converts to the 'bool' value
+        //:   'true'
+        //:
+        // Plan:
+        //   Incorporate usage example from header into driver, remove leading
+        //   comment characters, and replace 'assert' with 'ASSERT'.
+        //
+        // Testing:
+        //  BoolType falseValue()
+        //  BoolType trueValue()
+        // --------------------------------------------------------------------
+
+        if (verbose) printf("\nTESTING falseValue() and trueValue()"
+                            "\n------------------------------------\n");
+
+        if (verbose) printf("\t1. Functions must return BoolType values\n");
+
+        typedef bsls_UnspecifiedBool<int(int)> HostType;
+        typedef HostType::BoolType        BoolType;
+
+        typedef BoolType (*FuncType)();
+
+        FuncType falseFunc = &HostType::falseValue;
+        FuncType trueFunc  = &HostType::trueValue;
+
+        // Silence unused variable warnings.
+        (void)falseFunc;
+        (void)trueFunc;
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        if (verbose) printf("\t2. falseValue() converts to false\n");
+
+        ASSERT(!HostType::falseValue());
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        if (verbose) printf("\t3. trueValue() converts to true\n");
+
+        ASSERT(HostType::trueValue());
+
       } break;
       case 2: {
         // --------------------------------------------------------------------
-        // TESTING bdema_ManagedPtr_UnspecifiedBoolHelper
+        // TESTING bsls_UnspecifiedBool::BoolType
         //
         // Concerns:
-        //: 1 Objects of type 'bdema_ManagedPtr_UnspecifiedBoolHelper' must be
+        //: 1 Objects of type 'bsls_UnspecifiedBool::BoolType' must be
         //:   implicitly convertible to bool.
-        //: 2 Objects of type 'bdema_ManagedPtr_UnspecifiedBoolHelper' must not
+        //: 2 Objects of type 'bsls_UnspecifiedBool::BoolType' must not
         //:   promote to type 'int'.
-        //: 3 A default constructed 'bdema_ManagedPtr_UnspecifiedBoolHelper'
+        //: 3 A default constructed 'bsls_UnspecifiedBool::BoolType'
         //:   should convert to 'false' when converted to a boolean value, the
         //:   same value as a default constructed 'bool'.
-        //: 4 A 'bdema_ManagedPtr_UnspecifiedBoolHelper' object initialized
+        //: 4 A 'bsls_UnspecifiedBool::BoolType' object initialized
         //:   with the literal '0' should produce the value 'false' when
         //:   converted to a boolean value.
         //: 5 A class with a conversion operator to type
-        //:   'bdema_ManagedPtr_UnspecifiedBoolHelper' should be implicitly
+        //:   'bsls_UnspecifiedBool::BoolType' should be implicitly
         //:   convertible to bool.
         //: 6 Two classes that are implicitly convertible to type
-        //:   'bdema_ManagedPtr_UnspecifiedBoolHelper' should not accidentally
+        //:   'bsls_UnspecifiedBool::BoolType' should not accidentally
         //:   be comparable to each other using 'operator=='.
         //
         // Plan:
         //: 1 blah ...
         //
         // Testing:
-        //   ... list class operations that are tested ...
+        //   typedef bsls_UnspecifiedBool::BoolType
         // --------------------------------------------------------------------
 
         if (verbose) printf("\nTESTING bsls_UnspecifiedBool"
                             "\n----------------------------\n");
 
-        if (verbose) printf("\tTest blah...\n");
+        if (verbose) printf("\t1. BoolType implicitly conversts to bool\n");
 
+        typedef bsls_UnspecifiedBool<int>::BoolType BoolType;
+
+        const BoolType bt = BoolType();
+        if (bt) {
+            ASSERT(false);
+        }
+
+        while (bt) {
+            ASSERT(false);
+        }
+
+        for ( ; bt; ) {
+            ASSERT(false);
+        }
+
+        do {} while (bt);
+
+        if (!bt) {
+            ASSERT(true);
+        }
+        else {
+            ASSERT(false);
+        }
+
+        const bool b = bt;
+        ASSERT(!b);
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        if (verbose) printf("\t2. BoolType does not promote to int\n");
+
+        struct TestPromoteToInt {
+            static bool call(...) { return false; }
+            static bool call(int) { return true;  }
+        };
+
+        ASSERT(TestPromoteToInt::call(0));
+        ASSERT(!TestPromoteToInt::call(bt));
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        if (verbose) printf("\t3. BoolType default value converts to false\n");
+
+        ASSERT(bt == false);
+        ASSERT(false == bt);
+        ASSERT(!bt);
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        if (verbose) printf("\t4. BoolType(0) converts to false\n");
+
+        const BoolType b0 = 0;
+        ASSERT(b0 == false);
+        ASSERT(false == b0);
+        ASSERT(!b0);
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        if (verbose) printf("\t5. Testing BoolType conversion operator\n");
+
+        struct Booleable {
+            typedef bsls_UnspecifiedBool<Booleable>::BoolType BoolType;
+
+            operator BoolType() const { return false; }
+        };
+
+        const Booleable babel;
+        ASSERT(babel == false);
+        ASSERT(false == babel);
+        ASSERT(!babel);
+
+        if (babel) {
+            ASSERT(false);
+        }
+
+        while (babel) {
+            ASSERT(false);
+        }
+
+        for ( ; babel; ) {
+            ASSERT(false);
+        }
+
+        do {} while (babel);
+
+        if (!babel) {
+            ASSERT(true);
+        }
+        else {
+            ASSERT(false);
+        }
+
+        const bool bb = babel;
+        ASSERT(!bb);
       } break;
       case 1: {
         // --------------------------------------------------------------------
@@ -267,49 +413,14 @@ int main(int argc, char *argv[])
         if (verbose) printf("\nBREATHING TEST"
                             "\n==============\n");
 
-      } break;
-      case -1: {
-        // --------------------------------------------------------------------
-        // TESTING ADDITIONAL CONCERNS
-        //
-        // Concerns:
-        //: 1 Two 'bdema_ManagedPtr<T>' objects should not be comparable with
-		//:   the equality operator.
-		//
-        //: 2 Two objects of different instantiations of the 'bdema_ManagedPtr'
-		//:   class template should not be comparable with the equality
-		//:   operator
-        //
-        // Plan:
-        //   The absence of a specific operator will be tested by failing to
-		//   compile test code using that operator.  These tests will be
-		//   configured to compile only when specific macros are defined as
-		//   part of the build configuration, and not routinely tested.
-        //
-        // Testing:
-        //   This test is checking for the *absence* of 'operator=='.
-        // --------------------------------------------------------------------
-//#define BSLS_UNSPECIFIEDBOOL_TEST_FLAG_1
-//#define BSLS_UNSPECIFIEDBOOL_TEST_FLAG_2
-
-#if defined BSLS_UNSPECIFIEDBOOL_TEST_FLAG_1
-		bdema_ManagedPtr<int> x;
-		bdema_ManagedPtr<int> y;
-		// The following two lines should fail to compile
-		ASSERT(x == y);
-		ASSERT(x != y);
-#endif
-
-#if defined BSLS_UNSPECIFIEDBOOL_TEST_FLAG_2
-		bdema_ManagedPtr<int> a;
-		bdema_ManagedPtr<double> b;
-		// The following two lines should fail to compile
-		ASSERT(a == b);
-		ASSERT(a != b);
-
-		ASSERT(b == a);
-		ASSERT(b != a);
-#endif
+        typedef bsls_UnspecifiedBool<void> HostType;
+        ASSERT(!HostType::falseValue());
+        if (HostType::trueValue()) {
+            ASSERT(true);
+        }
+        else {
+            ASSERT(false);
+        }
       } break;
       default: {
         fprintf(stderr, "WARNING: CASE `%d' NOT FOUND.\n", test);
