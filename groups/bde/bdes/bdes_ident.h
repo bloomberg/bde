4f7b039fe8a19ab6b1bc603c3d72c99fb86b0d57
Mike Giroux <mgiroux@bloomberg.net>
2011-11-21 13:20:54 -0500

    sync bsls_ident.h with sysutil_ident.h, and make bdes_ident.h simply rename the bsls_ident.h macros

--- groups/bde/bdes/bdes_ident.h
+++ groups/bde/bdes/bdes_ident.h
@@ -63,129 +63,14 @@
 #endif
 #endif /* __cplusplus */
 
-#ifdef BDES_IDENT_OFF
-
-#define BDES_IDENT(str)
-#define BDES_IDENT_RCSID(tag,str)
-
-#else /* !BDES_IDENT_OFF */
-
-/* BDES_IDENT() - insert string into .comment binary segment (if supported)*/
-
-#if defined(__GNUC__)
-  #if !defined(_AIX)
-    /* does not work with AIX as; might work with GNU as (gas) (not tested) */
-    /* gcc has no pragma equivalent for #ident */
-    #define _BDES_IDENT(str) __asm__(#str);
-    #define BDES_IDENT(str) _BDES_IDENT(.ident str)
-  #endif
-#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
-  /* Sun Studio CC does not support _Pragma() until Sun Studio 12 */
-  #if (!defined(__cplusplus)) || (__SUNPRO_CC >= 0x590)
-    #define _BDES_IDENT(str) _Pragma(#str)
-    #define BDES_IDENT(str) _BDES_IDENT(ident str)
-  #endif
-#elif defined(__IBMC__) || defined(__IBMCPP__)
-  #define _BDES_IDENT(str) _Pragma(#str)
-  #define BDES_IDENT(str) _BDES_IDENT(comment (user, str))
-#elif defined(_MSC_VER) /* Microsoft Visual Studio Compiler */
-  #define BDES_IDENT(str)
-#elif defined(__HP_cc) || defined(__HP_aCC)
-  #define _BDES_IDENT(str) _Pragma(#str)
-  #define BDES_IDENT(str) _BDES_IDENT(versionid str)
+#ifndef INCLUDED_BSLS_IDENT
+#include <bsls_ident.h>
 #endif
 
-#ifndef BDES_IDENT
-#define BDES_IDENT(str)
-#endif
-
-/* multi-level indirection to force macro expansion before concatenation */
-/* (order of concatenation/macro expansion unspecified in ANSI standard) */
-#define BDES_IDENT_JOIN2(x,y) x ## y
-#define BDES_IDENT_JOIN(x,y) BDES_IDENT_JOIN2(x,y)
-
-/* BDES_IDENT_RCSID() - insert ident str (specific to platform/compiler) */
-
-#if defined(__GNUC__)
-  #if !defined(_AIX)
-    #define BDES_IDENT_RCSID(tag,str) BDES_IDENT(str)
-  #else /* _AIX */
-    /* TBD: look further into if we ever use gcc to build prod tasks on AIX */
-    #ifndef lint
-    #define BDES_IDENT_RCSID(tag,str) \
-      static char BDES_IDENT_JOIN(ident_,tag)[] __attribute__((__unused__)) \
-                  = str;
-    #endif
-  #endif
-#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
-  #if (!defined(__cplusplus)) || (__SUNPRO_CC >= 0x590)
-    #define BDES_IDENT_RCSID(tag,str) BDES_IDENT(str)
-  #else /* Sun Studio CC does not support _Pragma() until Sun Studio 12 */
-    #ifndef lint
-    #define BDES_IDENT_RCSID(tag,str) \
-      static char BDES_IDENT_JOIN(ident_,tag)[] = str;
-    #endif
-  #endif
-#elif defined(__IBMC__) || defined(__IBMCPP__)
-  #if (defined(__IBMC__)   && __IBMC__   >= 1010) \
-    ||(defined(__IBMCPP__) && __IBMCPP__ >= 1010)
-    #define BDES_IDENT_RCSID(tag,str) BDES_IDENT(str)
-  #else
-    /* Early versions of IBM xlc did not preserve .comment in binary
-     * executables or pre-linked libraries.  Fixed by IBM in following
-     * releases:
-     * xlC v8.0 in May 2008 PTF (8.0.0.19) with -qxflag=new_pragma_comment_user
-     * xlC v9.0 in July 2008 PTF (9.0.0.5)
-     * xlC v10.1 GA
-     * Enabled above only for xlC 10.1 or later (which can be detected
-     * reliably) (Use C compiler and printf("%s\n", __xlc__) to see x.x.x.x
-     * version, but strings are not comparable in macros)
-     */
-    /* various ways to create C string, including const, volatile, more */
-    /* static char instead of static const char with xlC -qnoro -qnoroconst */
-    /* xlC might end up optimizing this away, anyway */
-    #ifndef lint
-    #define BDES_IDENT_RCSID(tag,str) \
-      static char BDES_IDENT_JOIN(ident_,tag)[] = str;
-    #endif
-  #endif
-#elif defined(_MSC_VER)
-  #define BDES_IDENT_RCSID(tag,str)
-#elif defined(__HP_cc) || defined(__HP_aCC)
-  #define BDES_IDENT_RCSID(tag,str) BDES_IDENT(str)
-#else
-  /* others: add conditions above for compilers able to support BDES_IDENT */
-  #ifndef lint
-  #define BDES_IDENT_RCSID(tag,str) \
-    static char BDES_IDENT_JOIN(ident_,tag)[] = str;
-  #endif
-#endif
-
-#ifndef BDES_IDENT_RCSID
-#define BDES_IDENT_RCSID(tag,str)
-#endif
-
-#endif /* !BDES_IDENT_OFF */
-
-#if defined(__GNUC__)
-  #define BDES_IDENT_PRAGMA_ONCE _Pragma("once")
-#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
-  /* Sun Studio does not support #pragma once.  Instead, it natively detects if
-   * the entire non-comment portion of a file is wrapped by #ifndef, and if so,
-   * optimizes away reopens of the file if the #ifndef condition is false */
-  #define BDES_IDENT_PRAGMA_ONCE
-#elif defined(__IBMC__) || defined(__IBMCPP__)
-  #define BDES_IDENT_PRAGMA_ONCE _Pragma("once")
-#elif defined(_MSC_VER)
-  #define BDES_IDENT_PRAGMA_ONCE
-#elif defined(__HP_cc) || defined(__HP_aCC)
-  /* HP-UX ANSI C compiler does not document support for #pragma once,
-   * but does document that unknown pragmas are ignored */
-  /* UPDATE: aCC A.06.20 fails on this, so disable for now and revisit later */
-  #define BDES_IDENT_PRAGMA_ONCE /*_Pragma("once")*/
-#else
-  #define BDES_IDENT_PRAGMA_ONCE _Pragma("once")
-#endif
+// Use the bsls_ident definitions for all macros.
+#define BDES_IDENT(str)           BSLS_IDENT(str)
+#define BDES_IDENT_RCSID(tag,str) BSLS_IDENT_RCSID(tag,str)
+#define BDES_IDENT_PRAGMA_ONCE    BSLS_IDENT_PRAGMA_ONCE
 BDES_IDENT_PRAGMA_ONCE
 
 #endif /* INCLUDED_BDES_IDENT */
--- groups/bsl/bsls/bsls_ident.h
+++ groups/bsl/bsls/bsls_ident.h
@@ -6,10 +6,22 @@
 //
 //@CLASSES:
 //
-//@AUTHOR: Anthony Comerico (acomeric)
+//@AUTHOR: Anthony Comerico (acomeric), Glenn Strauss (gstrauss)
 //
 //@DESCRIPTION: The purpose of this component is to provide macros for
 // inserting SCM Ids into source files.
+//
+// SCM Ids are useful for identifying source revisions in binaries.
+//   'man ident'
+//   'man strings'  ('strings -a' produces more/noisier information)
+//   'man mcs'      (Solaris-only)
+//
+// 'BSLS_IDENT_PRAGMA_ONCE' can optionally be used in headers and encapsulates
+//   #pragma once
+// a non-standard pragma supported on a number of platforms and which
+// indicates that a header should only be included and parsed once.
+// This can help reduce compile times by eliminating extraneous I/O
+// when headers are included more than once in the same file.
 //
 ///Usage
 ///-----
@@ -36,31 +48,224 @@
 //  BSLS_IDENT("$Id: $")
 //..
 
-#if 0 // TBD, enable this for next release
+/* ident string intentionally omitted for this header (do not add to binaries)*/
+/* Its use is expected to be so extensive that the cost outweighs benefit
+ * of including an ident string for every file that includes this header */
+/* (present here so that programs like update_rcsid do not accidentally add) */
+#if 0
+#define BSLS_IDENT_RCSID(tag,str)
+BSLS_IDENT_RCSID(sysutil_ident_h,"$Id: $")
+#endif
+
+
+#ifdef BSLS_IDENT_OFF
+
+
+#define BSLS_IDENT(str)
+#define BSLS_IDENT_RCSID(tag,str)
+
+
+#else /* !BSLS_IDENT_OFF */
+
+
+/* BSLS_IDENT() - insert string into .comment binary segment (if supported)*/
+
 #if defined(__GNUC__)
   #if !defined(_AIX)
     /* does not work with AIX as; might work with GNU as (gas) (not tested) */
-    #define _BSLS_IDENT(str) asm(#str);
+    /* gcc has no pragma equivalent for #ident */
+    #define _BSLS_IDENT(str) __asm__(#str);
     #define BSLS_IDENT(str) _BSLS_IDENT(.ident str)
-  #else
-    #define BSLS_IDENT(str)
   #endif
-#elif defined(_AIX)
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+  /* Sun Studio CC does not support _Pragma() until Sun Studio 12 */
+  #if (!defined(__cplusplus)) || (__SUNPRO_CC >= 0x590)
+    #define _BSLS_IDENT(str) _Pragma(#str)
+    #define BSLS_IDENT(str) _BSLS_IDENT(ident str)
+  #endif
+#elif defined(__IBMC__) || defined(__IBMCPP__)
   #define _BSLS_IDENT(str) _Pragma(#str)
   #define BSLS_IDENT(str) _BSLS_IDENT(comment (user, str))
-#elif defined(__sun)
+#elif defined(_MSC_VER) /* Microsoft Visual Studio Compiler */
   #define _BSLS_IDENT(str) _Pragma(#str)
-  #define BSLS_IDENT(str) _BSLS_IDENT(ident str)
-#else
-  #define BSLS_IDENT(str)
-#endif
-#else // TBD: Expand to nothing for now
-  #define BSLS_IDENT(str)
+  #define BSLS_IDENT(str) _BSLS_IDENT(comment (user, str))
+#elif defined(__HP_cc) || defined(__HP_aCC)
+  #define _BSLS_IDENT(str) _Pragma(#str)
+  #define BSLS_IDENT(str) _BSLS_IDENT(versionid str)
 #endif
 
-// Unlike most headers, which use 'BSLS_IDENT' as described in the usage, this
-// header must wait till 'BSLS_IDENT' has been define.
-BSLS_IDENT("$Id: $")
+#ifndef BSLS_IDENT
+#define BSLS_IDENT(str)
+#endif
+
+
+/* multi-level indirection to force macro expansion before concatenation */
+/* (order of concatenation/macro expansion unspecified in ANSI standard) */
+#define BSLS_IDENT_JOIN2(x,y) x ## y
+#define BSLS_IDENT_JOIN(x,y) BSLS_IDENT_JOIN2(x,y)
+
+
+/* BSLS_IDENT_RCSID() - insert ident str (specific to platform/compiler) */
+
+#if defined(__GNUC__)
+  #if !defined(_AIX)
+    #define BSLS_IDENT_RCSID(tag,str) BSLS_IDENT(str)
+  #else /* _AIX */
+    /* (XXX: look into further if we ever use gcc to build prod tasks on AIX) */
+    #ifndef lint
+    #define BSLS_IDENT_RCSID(tag,str) \
+      static char BSLS_IDENT_JOIN(ident_,tag)[] __attribute__((__unused__)) = str;
+    #endif
+  #endif
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+  #if (!defined(__cplusplus)) || (__SUNPRO_CC >= 0x590)
+    #define BSLS_IDENT_RCSID(tag,str) BSLS_IDENT(str)
+  #else /* Sun Studio CC does not support _Pragma() until Sun Studio 12 */
+    #ifndef lint
+    #define BSLS_IDENT_RCSID(tag,str) \
+      static char BSLS_IDENT_JOIN(ident_,tag)[] = str;
+    #endif
+  #endif
+#elif defined(__IBMC__) || defined(__IBMCPP__)
+  #if ((defined(__IBMC__)   && __IBMC__   >= 1010) \
+    || (defined(__IBMCPP__) && __IBMCPP__ >= 1010))
+    #define BSLS_IDENT_RCSID(tag,str) BSLS_IDENT(str)
+  #else
+    /* Early versions of IBM xlc did not preserve .comment in binary executables
+     * or pre-linked libraries.  Fixed by IBM in following releases:
+     * xlC v8.0 in May 2008 PTF (8.0.0.19) with -qxflag=new_pragma_comment_user
+     * xlC v9.0 in July 2008 PTF (9.0.0.5)
+     * xlC v10.1 GA
+     * Enabled above only for xlC 10.1 or later (which can be detected reliably)
+     * (Use C compiler and printf("%s\n", __xlc__) to see x.x.x.x version,
+     *  but strings are not comparable in macros)
+     * Note that using updated linker (bind64) (circa late 2010) is also needed
+     * for the ident strings to be present in pre-linked libs and executables.
+     */
+    /* various ways to create C string, including const, volatile, more */
+    /* static char instead of static const char with xlC -qnoro -qnoroconst */
+    /* xlC might end up optimizing this away, anyway */
+    #ifndef lint
+    #define BSLS_IDENT_RCSID(tag,str) \
+      static char BSLS_IDENT_JOIN(ident_,tag)[] = str;
+    #endif
+  #endif
+#elif defined(_MSC_VER)
+  #define BSLS_IDENT_RCSID(tag,str) BSLS_IDENT(str)
+#elif defined(__HP_cc) || defined(__HP_aCC)
+  #define BSLS_IDENT_RCSID(tag,str) BSLS_IDENT(str)
+#else
+  /* others: add conditions above for compilers able to support BSLS_IDENT */
+  #ifndef lint
+  #define BSLS_IDENT_RCSID(tag,str) \
+    static char BSLS_IDENT_JOIN(ident_,tag)[] = str;
+  #endif
+#endif
+
+#ifndef BSLS_IDENT_RCSID
+#define BSLS_IDENT_RCSID(tag,str)
+#endif
+
+
+/* timestamp compilation with BUILDID, if provided.  (occurs once per object)
+ * Use BSLS_IDENT() so only occurs in .comment section if supported.
+ * example usage in a Makefile target:
+ *   $(CC) -c -DBUILDID="$(@F) `date +%Y%m%d_%H%M%S`" -o file.o file.c
+ */
+#ifdef BUILDID
+  #define _BSLS_IDENT_BUILDID_IMP2(str) BSLS_IDENT(#str)
+  #define _BSLS_IDENT_BUILDID_IMP(str) _BSLS_IDENT_BUILDID_IMP2(str)
+  _BSLS_IDENT_BUILDID_IMP($cc: BUILDID $)
+  #undef _BSLS_IDENT_BUILDID_IMP
+  #undef _BSLS_IDENT_BUILDID_IMP2
+#endif
+
+
+#endif /* !BSLS_IDENT_OFF */
+
+
+#if defined(__GNUC__)
+  #define BSLS_IDENT_PRAGMA_ONCE _Pragma("once")
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+  /* Sun Studio does not support #pragma once.  Instead, it natively detects if
+   * the entire non-comment portion of a file is wrapped by #ifndef, and if so,
+   * optimizes away reopens of the file if the #ifndef condition is false */
+  #define BSLS_IDENT_PRAGMA_ONCE
+#elif defined(__IBMC__) || defined(__IBMCPP__)
+  #define BSLS_IDENT_PRAGMA_ONCE _Pragma("once")
+#elif defined(_MSC_VER)
+  #define BSLS_IDENT_PRAGMA_ONCE _Pragma("once")
+#elif (defined(__HP_cc)  && __HP_cc-0  >= 62500) \
+   || (defined(__HP_aCC) && __HP_aCC-0 >= 62500)
+  /* supported in aCC A.06.25 (had not been fully supported in aCC A.06.20) */
+  #define BSLS_IDENT_PRAGMA_ONCE _Pragma("once")
+#else
+  #define BSLS_IDENT_PRAGMA_ONCE _Pragma("once")
+#endif
+BSLS_IDENT_PRAGMA_ONCE
+
+
+/* Technical notes:
+ *
+ * There is a cost to including ident extra information in the binary objects,
+ * which may include disk space usage and runtime memory usage, as well as
+ * side effects such as locality of strings in memory at runtime.
+ *
+ * Some vendors provide a mechanism to include ident strings in a non-loadable
+ * .comment section of the binary so that strings are available for review in
+ * the on-disk binary, but are not loaded into memory at runtime.
+ * (Alternatively, this separate non-loadable section might selectively be
+ * stripped from the binary, saved in a database with the checksum of the
+ * stripped binary, and then the resulting smaller binary moved to production.)
+ *
+ * bsls_ident.h encapsulates ident mechanisms so that includers of this
+ * header need not be concerned with the mechanism applied.  The mechanism may
+ * be changed as better methods become available and the includer can obtain the
+ * changes simply by recompiling (without needing to modify all other source
+ * code).
+ *
+ * 'ident' shows only ident-style strings.  'man ident' for more info.
+ * The tokens passed to the pragmas are not necessarily ident-style.
+ * They are still visible with 'strings -a'.
+ *
+ * IBM xlC supports #pragma comment
+ * IBM xlC warns about #pragma ident and errors for #ident
+ * Sun CC supports #pragma ident and #ident
+ * Sun CC silently ignores #pragma comment
+ * GCC supports #ident
+ * GCC warns about unrecognized #pragma ident and #pragma comment
+ * While these can all be worked around by disabling specific warnings/errors,
+ * doing so might mask other warnings/errors.  This header allows for
+ * encapsulation of all the conditional logic to use the correct pragma with
+ * each compiler without having to duplicate these conditions in every source
+ * file.
+ *
+ * #ident, #pragma ident, #pragma comment each can take a -single- user-defined
+ * token (no concatentation of string constants or preprocessor macro ##
+ * concatentation)  (MS Visual Studio will do string concatenation)
+ *
+ * C99 _Pragma() can expanded in macros and can be used in place of #pragma.
+ * GCC does not implement a #pragma version of gcc #ident preprocessor directive
+ *
+ * AIX 'strip' removes strings inserted by #pragma comment
+ * Solaris mcs -d removes strings inserted by #ident and #pragma ident
+ *   (equivalent to compiling with cc -mr)
+ *   mcs -c uniquifies the strings (equivalent to compiling with cc -mc)
+ *   ('strip' does not remove these strings)
+ * GNU strip: 'strip -R comment' will remove .comment section
+ *
+ * AIX multiple #pragma comment end up concatenated on a single line, allowing
+ * for pasting together of individual tokens.  'ident' prints each ident-style
+ * string on its own line.  Solaris #ident places each token on its own line.
+ *
+ * Sun Studio CC (C++ compiler) < Sun Studio 12 CC do not support _Pragma()
+ * Sun Studio 12 CC supports _Pragma() but clips the first character of string
+ *   (This leads to strings being present in .comment section, minus the first
+ *    character, and then not showing up when 'ident' is run, but present with
+ *    strings -a) (Bug filed with Sun and has been fixed in latest Studio 12).
+ *
+ * HP-UX supports _Pragma() only in ANSI (-Aa) and ANSI extended (-Ae) mode.
+ */
 
 #endif // INCLUDED_BSLS_IDENT
 
