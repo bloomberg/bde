6c66cfbc03ca2ee0a59273b3850505b58a611a0c
Rohan Bhindwale <rbhindwale@bloomberg.net>
2012-01-19 17:15:38 -0500

    incorporating encodeEmptyVectors option

--- groups/bde/bdem/bdem_berencoderoptions.cpp
+++ groups/bde/bdem/bdem_berencoderoptions.cpp
@@ -1,65 +1,60 @@
-// bdem_berencoderoptions.cpp   -*-C++-*-
-#include <bdem_berencoderoptions.h>
+// bdem_berencoderoptions.cpp-- GENERATED FILE - DO NOT EDIT ---*-C++-*-
 
 #include <bdes_ident.h>
 BDES_IDENT_RCSID(bdem_berencoderoptions_cpp,"$Id$ $CSID$")
 
-#include <bdeat_formattingmode.h>
+#include <bdem_berencoderoptions.h>
 
-#include <bsls_assert.h>
-#include <bdeu_chartype.h>
+#include <bdeat_formattingmode.h>
+#include <bdeat_valuetypefunctions.h>
 #include <bdeu_print.h>
 #include <bdeu_printmethods.h>
 #include <bdeu_string.h>
 
-#include <bsl_ostream.h>
-#include <bsl_iomanip.h>
+
+#include <bsls_assert.h>
+
+#include <iomanip>
+#include <limits>
+#include <ostream>
 
 namespace BloombergLP {
 
-                     // ----------------------------------
-                     // class bdem_BerEncoderOptions
-                     // ----------------------------------
+                     // ----------------------------------                     
+                     // class bdem_BerEncoderOptions                     
+                     // ----------------------------------                     
 
 // CONSTANTS
 
 const char bdem_BerEncoderOptions::CLASS_NAME[] = "bdem_BerEncoderOptions";
-    // the name of this class
-const int bdem_BerEncoderOptions::DEFAULT_TRACE_LEVEL = 0;
-    // default value of 'TraceLevel' attribute
-const int bdem_BerEncoderOptions::DEFAULT_BDE_VERSION_CONFORMANCE = 10500;
-    // default value of 'BdeVersionConformance' attribute
+
+const int bdem_BerEncoderOptions::DEFAULT_INITIALIZER_TRACE_LEVEL = 0;
+
+const int bdem_BerEncoderOptions::DEFAULT_INITIALIZER_BDE_VERSION_CONFORMANCE = 10500;
+
+const bool bdem_BerEncoderOptions::DEFAULT_INITIALIZER_ENCODE_EMPTY_VECTOR = true;
 
 const bdeat_AttributeInfo bdem_BerEncoderOptions::ATTRIBUTE_INFO_ARRAY[] = {
     {
         ATTRIBUTE_ID_TRACE_LEVEL,
-        "TraceLevel",             // name
-        sizeof("TraceLevel") - 1, // name length
-        "trace (verbosity) level",  // annotation
-        bdeat_FormattingMode::BDEAT_DEC // formatting mode
+        "TraceLevel",
+        sizeof("TraceLevel") - 1,
+        "",
+        bdeat_FormattingMode::BDEAT_DEC
     },
     {
         ATTRIBUTE_ID_BDE_VERSION_CONFORMANCE,
-        "BdeVersionConformance",             // name
-        sizeof("BdeVersionConformance") - 1, // name length
-        "The largest BDE version that can be assumed of the corresponding "
-        "decoder for the encoded message, expressed as 10000*majorVersion "
-        "+ 100*minorVersion + patchVersion (e.g. 1.5.0 is expressed as 105"
-        "00). Ideally, the BER encoder should be permitted to generate any"
-        " BER that conforms to X.690 (Basic Encoding Rules) and X.694 (map"
-        "ping of XSD to ASN.1). In practice, however, certain unimplemente"
-        "d features and missunderstandings of these standards have resulte"
-        "d in a decoder that cannot accept the full range of legal inputs."
-        " Even when the encoder and decoder are both upgraded to a richer "
-        "subset of BER, the program receiving the encoded values may not h"
-        "ave been recompiled with the latest version and, thus restricting"
-        " the encoder to emit BER that can be understood by the decoder at"
-        " the other end of the wire. If it is that the receiver has a more"
-        " modern decoder, set this variable to a larger value to allow the"
-        " encoder to produce BER that is richer and more standards conform"
-        "ant. The default should be increased only when old copies of the "
-        "decoder are completely out of circulation.",  // annotation
-        bdeat_FormattingMode::BDEAT_DEC // formatting mode
+        "BdeVersionConformance",
+        sizeof("BdeVersionConformance") - 1,
+        "",
+        bdeat_FormattingMode::BDEAT_DEC
+    },
+    {
+        ATTRIBUTE_ID_ENCODE_EMPTY_VECTOR,
+        "EncodeEmptyVectors",
+        sizeof("EncodeEmptyVectors") - 1,
+        "",
+        bdeat_FormattingMode::BDEAT_TEXT
     }
 };
 
@@ -71,45 +66,66 @@ const bdeat_AttributeInfo *bdem_BerEncoderOptions::lookupAttributeInfo(
 {
     switch(nameLength) {
         case 10: {
-            if (bdeu_CharType::toUpper(name[0])=='T'
-             && bdeu_CharType::toUpper(name[1])=='R'
-             && bdeu_CharType::toUpper(name[2])=='A'
-             && bdeu_CharType::toUpper(name[3])=='C'
-             && bdeu_CharType::toUpper(name[4])=='E'
-             && bdeu_CharType::toUpper(name[5])=='L'
-             && bdeu_CharType::toUpper(name[6])=='E'
-             && bdeu_CharType::toUpper(name[7])=='V'
-             && bdeu_CharType::toUpper(name[8])=='E'
-             && bdeu_CharType::toUpper(name[9])=='L')
+            if (name[0]=='T'
+             && name[1]=='r'
+             && name[2]=='a'
+             && name[3]=='c'
+             && name[4]=='e'
+             && name[5]=='L'
+             && name[6]=='e'
+             && name[7]=='v'
+             && name[8]=='e'
+             && name[9]=='l')
             {
                 return &ATTRIBUTE_INFO_ARRAY[ATTRIBUTE_INDEX_TRACE_LEVEL];
             }
         } break;
-        case 21: {
-            if (bdeu_CharType::toUpper(name[0])=='B'
-             && bdeu_CharType::toUpper(name[1])=='D'
-             && bdeu_CharType::toUpper(name[2])=='E'
-             && bdeu_CharType::toUpper(name[3])=='V'
-             && bdeu_CharType::toUpper(name[4])=='E'
-             && bdeu_CharType::toUpper(name[5])=='R'
-             && bdeu_CharType::toUpper(name[6])=='S'
-             && bdeu_CharType::toUpper(name[7])=='I'
-             && bdeu_CharType::toUpper(name[8])=='O'
-             && bdeu_CharType::toUpper(name[9])=='N'
-             && bdeu_CharType::toUpper(name[10])=='C'
-             && bdeu_CharType::toUpper(name[11])=='O'
-             && bdeu_CharType::toUpper(name[12])=='N'
-             && bdeu_CharType::toUpper(name[13])=='F'
-             && bdeu_CharType::toUpper(name[14])=='O'
-             && bdeu_CharType::toUpper(name[15])=='R'
-             && bdeu_CharType::toUpper(name[16])=='M'
-             && bdeu_CharType::toUpper(name[17])=='A'
-             && bdeu_CharType::toUpper(name[18])=='N'
-             && bdeu_CharType::toUpper(name[19])=='C'
-             && bdeu_CharType::toUpper(name[20])=='E')
+        case 17: {
+            if (name[0]=='E'
+             && name[1]=='n'
+             && name[2]=='c'
+             && name[3]=='o'
+             && name[4]=='d'
+             && name[5]=='e'
+             && name[6]=='E'
+             && name[7]=='m'
+             && name[8]=='p'
+             && name[9]=='t'
+             && name[10]=='y'
+             && name[11]=='V'
+             && name[12]=='e'
+             && name[13]=='c'
+             && name[14]=='t'
+             && name[15]=='o'
+             && name[16]=='r')
             {
-                return &ATTRIBUTE_INFO_ARRAY[
-                                 ATTRIBUTE_INDEX_BDE_VERSION_CONFORMANCE];
+                return &ATTRIBUTE_INFO_ARRAY[ATTRIBUTE_INDEX_ENCODE_EMPTY_VECTOR];
+            }
+        } break;
+        case 21: {
+            if (name[0]=='B'
+             && name[1]=='d'
+             && name[2]=='e'
+             && name[3]=='V'
+             && name[4]=='e'
+             && name[5]=='r'
+             && name[6]=='s'
+             && name[7]=='i'
+             && name[8]=='o'
+             && name[9]=='n'
+             && name[10]=='C'
+             && name[11]=='o'
+             && name[12]=='n'
+             && name[13]=='f'
+             && name[14]=='o'
+             && name[15]=='r'
+             && name[16]=='m'
+             && name[17]=='a'
+             && name[18]=='n'
+             && name[19]=='c'
+             && name[20]=='e')
+            {
+                return &ATTRIBUTE_INFO_ARRAY[ATTRIBUTE_INDEX_BDE_VERSION_CONFORMANCE];
             }
         } break;
     }
@@ -122,11 +138,52 @@ const bdeat_AttributeInfo *bdem_BerEncoderOptions::lookupAttributeInfo(int id)
       case ATTRIBUTE_ID_TRACE_LEVEL:
         return &ATTRIBUTE_INFO_ARRAY[ATTRIBUTE_INDEX_TRACE_LEVEL];
       case ATTRIBUTE_ID_BDE_VERSION_CONFORMANCE:
-        return
-           &ATTRIBUTE_INFO_ARRAY[ATTRIBUTE_INDEX_BDE_VERSION_CONFORMANCE];
+        return &ATTRIBUTE_INFO_ARRAY[ATTRIBUTE_INDEX_BDE_VERSION_CONFORMANCE];
+      case ATTRIBUTE_ID_ENCODE_EMPTY_VECTOR:
+        return &ATTRIBUTE_INFO_ARRAY[ATTRIBUTE_INDEX_ENCODE_EMPTY_VECTOR];
       default:
         return 0;
     }
+}
+
+// CREATORS
+
+bdem_BerEncoderOptions::bdem_BerEncoderOptions()
+: d_traceLevel(DEFAULT_INITIALIZER_TRACE_LEVEL)
+, d_bdeVersionConformance(DEFAULT_INITIALIZER_BDE_VERSION_CONFORMANCE)
+, d_encodeEmptyVectors(DEFAULT_INITIALIZER_ENCODE_EMPTY_VECTOR)
+{
+}
+
+bdem_BerEncoderOptions::bdem_BerEncoderOptions(const bdem_BerEncoderOptions& original)
+: d_traceLevel(original.d_traceLevel)
+, d_bdeVersionConformance(original.d_bdeVersionConformance)
+, d_encodeEmptyVectors(original.d_encodeEmptyVectors)
+{
+}
+
+bdem_BerEncoderOptions::~bdem_BerEncoderOptions()
+{
+}
+
+// MANIPULATORS
+
+bdem_BerEncoderOptions&
+bdem_BerEncoderOptions::operator=(const bdem_BerEncoderOptions& rhs)
+{
+    if (this != &rhs) {
+        d_traceLevel = rhs.d_traceLevel;
+        d_bdeVersionConformance = rhs.d_bdeVersionConformance;
+        d_encodeEmptyVectors = rhs.d_encodeEmptyVectors;
+    }
+    return *this;
+}
+
+void bdem_BerEncoderOptions::reset()
+{
+    d_traceLevel = DEFAULT_INITIALIZER_TRACE_LEVEL;
+    d_bdeVersionConformance = DEFAULT_INITIALIZER_BDE_VERSION_CONFORMANCE;
+    d_encodeEmptyVectors = DEFAULT_INITIALIZER_ENCODE_EMPTY_VECTOR;
 }
 
 // ACCESSORS
@@ -160,6 +217,11 @@ bsl::ostream& bdem_BerEncoderOptions::print(
         bdeu_PrintMethods::print(stream, d_bdeVersionConformance,
                                  -levelPlus1, spacesPerLevel);
 
+        bdeu_Print::indent(stream, levelPlus1, spacesPerLevel);
+        stream << "EncodeEmptyVectors = ";
+        bdeu_PrintMethods::print(stream, d_encodeEmptyVectors,
+                                 -levelPlus1, spacesPerLevel);
+
         bdeu_Print::indent(stream, level, spacesPerLevel);
         stream << "]\n";
     }
@@ -178,18 +240,24 @@ bsl::ostream& bdem_BerEncoderOptions::print(
         bdeu_PrintMethods::print(stream, d_bdeVersionConformance,
                                  -levelPlus1, spacesPerLevel);
 
+        stream << ' ';
+        stream << "EncodeEmptyVectors = ";
+        bdeu_PrintMethods::print(stream, d_encodeEmptyVectors,
+                                 -levelPlus1, spacesPerLevel);
+
         stream << " ]";
     }
 
     return stream << bsl::flush;
 }
 
-}  // close namespace BloombergLP;
 
-// GENERATED BY BLP_BAS_CODEGEN_2.1.6 Thu Jun  7 12:17:03 2007
+}  // close namespace BloombergLP
+
+// GENERATED BY BLP_BAS_CODEGEN_3.6.9 Thu Jan 19 14:48:12 2012
 // ----------------------------------------------------------------------------
 // NOTICE:
-//      Copyright (C) Bloomberg L.P., 2007
+//      Copyright (C) Bloomberg L.P., 2012
 //      All Rights Reserved.
 //      Property of Bloomberg L.P. (BLP)
 //      This software is made available solely pursuant to the
