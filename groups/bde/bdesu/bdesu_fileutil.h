7181691073b650b4fc28b005b3ae791ddf01000b
Bill Chapman <bchapman2@bloomberg.net>
2012-02-21 13:24:09 -0500

    Added 'LOCKING CAVEATS' section to component doc, and a reference to it from all locking methods.

--- groups/bde/bdesu/bdesu_fileutil.h
+++ groups/bde/bdesu/bdesu_fileutil.h
@@ -19,6 +19,22 @@ BDES_IDENT("$Id: $")
 //@DESCRIPTION: This component provides a platform-independent interface to
 // filesystem utility methods.
 //
+///LOCKING CAVEATS
+///---------------
+// Locking has the following caveats for the following operating systems:
+//: o On Unix, you can't lock for write a file opened for read only.  On
+//:   Windows, you can.
+//: o On Posix, closing a file releases ALL locks on ALL file descriptors
+//:   referring to that file within the current process.
+//: o On Unix, the child of a fork does not inherit the locks of the parent
+//:   process.
+//: o On Windows, opening a file for write does an implicit lock on the file.
+//:   This implicit lock is, by nature, different from the explicit locks done
+//:   by the 'lock' and 'tryLock' methods.  'open' calls check for such
+//:   implicit locks, it is not possible for another process to open a file
+//:   already opened for write.  On Unix, many processes can open the same file
+//:   for write.
+//
 ///Usage
 ///-----
 ///Example 1: General Usage
@@ -380,7 +396,8 @@ struct bdesu_FileUtil {
         // use by the current *process*, but the behavior is unspecified (and
         // platform-dependent) when either attempting to lock 'fd' multiple
         // times, or attempting to lock another descriptor referring to the
-        // same file, within a single process.
+        // same file, within a single process.  Also see 'LOCKING CAVEATS' in
+        // the component doc.
 
     static int tryLock(FileDescriptor fd, bool lockWrite);
         // Acquire a lock for the file with the specified 'fd' if it is
@@ -395,11 +412,12 @@ struct bdesu_FileUtil {
         // behavior is unspecified (and platform-dependent) when either
         // attempting to lock 'fd' multiple times, or attempting to lock
         // another descriptor referring to the same file, within a single
-        // process.
+        // process.  Also see 'LOCKING CAVEATS' in the component doc.
 
     static int unlock(FileDescriptor fd);
         // Release any lock this process holds on the file with the specified
-        // 'fd'.  Return 0 on success, and a non-zero value otherwise.
+        // 'fd'.  Return 0 on success, and a non-zero value otherwise.  Also
+        // see 'LOCKING CAVEATS' in the component doc.
 
     static int map(FileDescriptor   fd,
                    void           **addr,
