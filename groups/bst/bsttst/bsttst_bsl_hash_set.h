030f94caddc8d409b3bca7b32ef9192e1bf84403
Mike Giroux <mgiroux@bloomberg.net>
2012-05-25 15:17:45 -0400

    move bsl+apache and bsltst to new pkg group bst

---
+++ groups/bst/bsttst/bsttst_bsl_hash_set.h
@@ -0,0 +1,39 @@
+// bsttst_bsl_hash_set.h                                              -*-C++-*-
+#ifndef INCLUDED_BSTTST_BSL_HASH_SET
+#define INCLUDED_BSTTST_BSL_HASH_SET
+
+#ifndef INCLUDED_BSLS_IDENT
+#include <bsls_ident.h>
+#endif
+BSLS_IDENT("$Id: $")
+
+//@PURPOSE: Provide testing in bsl mode for hash_set template
+//
+//@CLASSES: none
+//
+//@AUTHOR: Bill Chapman (bchapman2)
+//
+//@DESCRIPTION: This component provides testing for the hash_set template
+// configuring the include files for bsl mode, in which 'std' is #define'd to
+// 'bsl'.  The std namespace is still accessible as 'native_std'.
+//
+///Usage
+///-----
+// This component is not used by any other component.  It exists for testing
+// purposes only.
+
+#ifndef INCLUDED_BSLSCM_VERSION
+#include <bslscm_version.h>
+#endif
+
+#endif
+
+// ---------------------------------------------------------------------------
+// NOTICE:
+//      Copyright (C) Bloomberg L.P., 2010
+//      All Rights Reserved.
+//      Property of Bloomberg L.P. (BLP)
+//      This software is made available solely pursuant to the
+//      terms of a BLP license agreement which governs its use.
+// ----------------------------- END-OF-FILE ---------------------------------
+
