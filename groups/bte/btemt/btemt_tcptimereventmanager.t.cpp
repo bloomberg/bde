224b55f8e3864ea2ba9573c6ec3bd298b62cfbbf
Rohan Bhindwale <rbhindwale@bloomberg.net>
2012-06-01 16:39:33 -0400

    adding test case

--- groups/bte/btemt/btemt_tcptimereventmanager.t.cpp
+++ groups/bte/btemt/btemt_tcptimereventmanager.t.cpp
@@ -322,6 +322,10 @@ static void dummyFunction(void) {
 
 }
 
+void noopFunction()
+{
+}
+
 enum { NUM_REGISTER_PAIRS = 10 };
 
 bcemt_Barrier *globalBarrier;
@@ -658,7 +662,7 @@ int main(int argc, char *argv[])
     }
 
     switch (test) { case 0:
-      case 15: {
+      case 16: {
           // ----------------------------------------------------------------
           // TESTING USAGE EXAMPLE
           //   The usage example provided in the component header file must
@@ -708,6 +712,31 @@ int main(int argc, char *argv[])
                       << ": Consuming " << item << bsl::endl;
               }
           }
+      } break;
+
+      case 15: {
+        // -----------------------------------------------------------------
+        // TESTING DRQS 29286796
+        //
+        // Concern:
+        //: TBD
+        //
+        // Plan:
+        //: TBD
+        //
+        // Testing:
+        //   DRQS 29286796
+        // -----------------------------------------------------------------
+
+        if (verbose) cout << endl
+                          << "TESTING DRQS 29286796" << endl
+                          << "=====================" << endl;
+
+        bdet_TimeInterval timeout = bdetu_SystemTime::now() + 1;
+        Obj mX;  const Obj& X = mX;
+        void *timerId = mX.registerTimer(timeout, &noopFunction);
+        mX.deregisterTimer(timerId);
+        ASSERT(0 == X.numTimers());
       } break;
 
       case 14: {
