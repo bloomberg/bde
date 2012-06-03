e5a6a7deb353e7a82bc4c24c638646dcc476e1ed
Pablo Halpern <phalpern@halpernwightsoftware.com>
2012-06-03 19:44:32 -0400

    Rewrote bslmf_metaint in terms of bsltt_integerconstant. Reliable conversion to arbitrary bsltt_integer constant not yet working.

--- groups/bsl/bslmf/bslmf_metaint.h
+++ groups/bsl/bslmf/bslmf_metaint.h
@@ -9,8 +9,10 @@ BSLS_IDENT("$Id: $")
 
 //@PURPOSE: Provide a meta-function to map integral constants to unique types.
 //
+//@DEPRECATED: Use 'bslstt_integer_constant' instead.
+//
 //@CLASSES:
-//  bslmf::Tag: meta-function mapping integral constants to C++ types
+//  bslmf::MetaInt: meta-function mapping integral constants to C++ types
 //
 //@AUTHOR: Pablo Halpern (phalpern)
 //
@@ -23,6 +25,12 @@ BSLS_IDENT("$Id: $")
 // so that 'bslmf::MetaInt<0>' is a distinct type from 'bslmf::MetaInt<1>',
 // which is also distinct from 'bslmf::MetaInt<2>', and so on.
 //
+///Usage
+///-----
+// This section illustates intended usage of this component
+//
+///Example 1: Compile-Time Function Dispatching
+/// - - - - - - - - - - - - - - - - - - - - - - 
 // The most common use of this structure is to perform static function
 // dispatching based on a compile-time calculation.  Often the calculation is
 // nothing more than a simple predicate, allowing us to select one of two
@@ -34,13 +42,17 @@ BSLS_IDENT("$Id: $")
 //  template <class T>
 //  void doSomethingImp(T *t, bslmf::MetaInt<0>)
 //  {
-//      // slow but generic implementation
+//      // slow generic implementation
+//      (void) t;
+//      // ...
 //  }
 //
 //  template <class T>
 //  void doSomethingImp(T *t, bslmf::MetaInt<1>)
 //  {
 //      // fast implementation (works only for some T's)
+//      (void) t;
+//      // ...
 //  }
 //
 //  template <class T, bool IsFast>
@@ -49,39 +61,43 @@ BSLS_IDENT("$Id: $")
 //      doSomethingImp(t, bslmf::MetaInt<IsFast>());
 //  }
 //..
-// For some parameter types, the fast version of 'doSomethingImp' is not legal.
 // The power of this approach is that the compiler will compile only the
-// implementation selected by the 'MetaInt' argument.
+// implementation selected by the 'MetaInt' argument.  For some parameter
+// types, the fast version of 'doSomethingImp' would be ill-formed.  This kind
+// of compile-time dispatch prevents the ill-formed version from ever being
+// instantiated.
 //..
-//  void f()
+//  int main()
 //  {
 //      int i;
 //      doSomething<int, true>(&i); // fast version selected for int
 //
 //      double m;
 //      doSomething<double, false>(&m); // slow version selected for double
+//
+//      return 0;
 //  }
 //..
-// Note that an alternative design would be to use template partial
-// specialization instead of standard function overloading to avoid the cost of
-// constructing a 'bslmf::MetaInt' object.
-//
-// In addition to forming new types, the value of the integral paramameter is
-// "saved" in the enum member 'VALUE', and is accessible for use in
-// compile-time or run-time operations.
+///Example 2: Reading the 'VALUE' member
+/// - - - - - - - - - - - - - - - - - - 
+// In addition to forming new types, the value of the integral paramameter to
+// 'MetaInt' is "saved" in the enum member 'VALUE', and is accessible for use
+// in compile-time or run-time operations.
 //..
 //  template <int V>
-//  int g()
+//  unsigned g()
 //  {
 //      bslmf::MetaInt<V> i;
-//      std::cout << i.VALUE << std::endl;
-//      std::cout << bslmf::MetaInt<V>::VALUE << std::endl;
+//      assert(V == i.VALUE);
+//      assert(V == bslmf::MetaInt<V>::VALUE);
 //      return bslmf::MetaInt<V>::VALUE;
 //  }
 //
-//  void h()
+//  int main()
 //  {
-//      1 == g<1>(); // prints the number '1' twice
+//      int v = g<1>();
+//      assert(1 == v);
+//      return 0;
 //  }
 //..
 
@@ -93,6 +109,10 @@ BSLS_IDENT("$Id: $")
 #include <bslmf_tag.h>
 #endif
 
+#ifndef INCLUDED_BSLTT_INTEGERCONSTANT
+#include <bsltt_integerconstant.h>
+#endif
+
 namespace BloombergLP {
 
 namespace bslmf {
@@ -102,16 +122,27 @@ namespace bslmf {
                            // ==============
 
 template <unsigned INT_VALUE>
-struct MetaInt {
+struct MetaInt : public bsltt::integer_constant<unsigned, INT_VALUE> {
     // Instantiating this template produces a distinct type for each
     // non-negative integer value.
 
     // TYPES
-    typedef MetaInt<INT_VALUE> Type;
+    typedef MetaInt<INT_VALUE>    Type;
+    typedef bslmf::Tag<INT_VALUE> Tag;
 
-    typedef bslmf::Tag<INT_VALUE>     Tag;
+    enum { VALUE = int(INT_VALUE) };
 
-    enum { VALUE = (int)INT_VALUE };
+    // CREATORS
+    MetaInt();
+        // Does nothing ('MetaInt' is stateless).
+
+    template <class TYPE>
+    MetaInt(bsltt::integer_constant<TYPE, INT_VALUE>);
+        // Convert from a 'bsltt::integer_constant' with the same value.
+
+    //! MetaInt(const MetaInt&) = default;
+    //! MetaInt& operator=(const MetaInt&) = default;
+    //! ~MetaInt() = default;
 
     // CLASS METHODS
     static Tag& tag();
@@ -120,11 +151,17 @@ struct MetaInt {
         // 'sizeof(f(expr).tag())' returns a different compile-time value
         // depending on the type of the result of calling the 'f' function but
         // does not actually call the 'f' function or the 'tag' method at
-        // run-time.  Note that 'sizeof(f(expr)::Type)' would not be legal.
+        // run-time.  Note that 'f(expr)::VALUE' or 'sizeof(f(expr)::Type)'
+        // would be ill-formed and that 'f(expr).value' is not a compile-time
+        // expression.
 
     // ACCESSORS
-    operator int () const { return VALUE; }
-        // Conversion to integer.  Returns 'VALUE'.
+    operator int () const;
+        // Return 'VALUE'.  (This operator is conversion operator to 'int'.)
+
+    template <class TYPE>
+    operator bsltt::integer_constant<TYPE, INT_VALUE> () const;
+        // Convert to an 'integer_constant' of the same value.
 };
 
 }  // close package namespace
@@ -150,13 +187,48 @@ struct MetaInt {
 #define bslmf_MetaInt bslmf::MetaInt
     // This alias is defined for backward compatibility.
 
+// ===========================================================================
+//                            INLINE FUNCTIONS
+// ===========================================================================
+
+// CREATORS
+template <unsigned INT_VALUE>
+inline
+bslmf::MetaInt<INT_VALUE>::MetaInt()
+{
+}
+
+template <unsigned INT_VALUE>
+template <class TYPE>
+inline
+bslmf::MetaInt<INT_VALUE>::MetaInt(bsltt::integer_constant<TYPE, INT_VALUE>)
+{
+}
+
+// ACCESSORS
+template <unsigned INT_VALUE>
+inline
+bslmf::MetaInt<INT_VALUE>::operator int () const
+{
+    return VALUE;
+}
+
+template <unsigned INT_VALUE>
+template <class TYPE>
+inline
+bslmf::MetaInt<INT_VALUE>::
+    operator bsltt::integer_constant<TYPE, INT_VALUE> () const
+{
+    return bsltt::integer_constant<TYPE, (TYPE) INT_VALUE>();
+}
+
 }  // close enterprise namespace
 
 #endif
 
 // ---------------------------------------------------------------------------
 // NOTICE:
-//      Copyright (C) Bloomberg L.P., 2004
+//      Copyright (C) Bloomberg L.P., 2004-2012
 //      All Rights Reserved.
 //      Property of Bloomberg L.P. (BLP)
 //      This software is made available solely pursuant to the
--- groups/bsl/bslmf/bslmf_metaint.t.cpp
+++ groups/bsl/bslmf/bslmf_metaint.t.cpp
@@ -4,10 +4,11 @@
 
 #include <cstdlib>      // atoi()
 #include <cstring>      // strcmp()
-#include <iostream>
+#include <cstdio>
 
 using namespace BloombergLP;
 using namespace std;
+using namespace bslmf;
 
 //=============================================================================
 //                                TEST PLAN
@@ -15,89 +16,180 @@ using namespace std;
 //                                Overview
 //                                --------
 //-----------------------------------------------------------------------------
-// [ 1] bslmf::MetaInt
 //-----------------------------------------------------------------------------
-// [ 2] USAGE EXAMPLE
+// [ 1] BREATHING TEST
+// [ 3] USAGE EXAMPLE
 //=============================================================================
-//                  STANDARD BDE ASSERT TEST MACRO
-//-----------------------------------------------------------------------------
-static int testStatus = 0;
 
-static void aSsErT(int c, const char *s, int i) {
+//==========================================================================
+//                  STANDARD BDE ASSERT TEST MACRO
+//--------------------------------------------------------------------------
+// NOTE: THIS IS A LOW-LEVEL COMPONENT AND MAY NOT USE ANY C++ LIBRARY
+// FUNCTIONS, INCLUDING IOSTREAMS.
+
+namespace {
+
+int verbose = 0;
+int veryVerbose = 0;
+int veryVeryVerbose = 0;
+int testStatus = 0;
+
+void aSsErT(int c, const char *s, int i) {
     if (c) {
-        cout << "Error " << __FILE__ << "(" << i << "): " << s
-             << "    (failed)" << endl;
+        printf("Error " __FILE__ "(%d): %s    (failed)\n", i, s);
         if (testStatus >= 0 && testStatus <= 100) ++testStatus;
     }
 }
-#define ASSERT(X) { aSsErT(!(X), #X, __LINE__); }
-//-----------------------------------------------------------------------------
-#define LOOP_ASSERT(I,X) { \
-    if (!(X)) { cout << #I << ": " << I << "\n"; aSsErT(1, #X, __LINE__);}}
 
-#define LOOP2_ASSERT(I,J,X) { \
-    if (!(X)) { cout << #I << ": " << I << "\t" << #J << ": " \
-        << J << "\n"; aSsErT(1, #X, __LINE__); } }
+}  // close unnamed namespace
+
+# define ASSERT(X) { aSsErT(!(X), #X, __LINE__); }
 
 //=============================================================================
-#define P(X) cout << #X " = " << (X) << endl; // Print identifier and value.
-#define Q(X) cout << "<| " #X " |>" << endl;  // Quote identifier literally.
-#define P_(X) cout << #X " = " << (X) << ", " << flush; // P(X) without '\n'
-#define L_ __LINE__                           // current Line number
-#define T_() cout << '\t' << flush;           // Print tab w/o linefeed.
+//                  STANDARD BDE LOOP-ASSERT TEST MACROS
+//-----------------------------------------------------------------------------
+// NOTE: This implementation of LOOP_ASSERT macros must use printf since
+//       cout uses new and must not be called during exception testing.
+
+#define LOOP_ASSERT(I,X) { \
+    if (!(X)) { printf("%s", #I ": "); dbg_print(I); printf("\n"); \
+                fflush(stdout); aSsErT(1, #X, __LINE__); } }
+
+#define LOOP2_ASSERT(I,J,X) { \
+    if (!(X)) { printf("%s", #I ": "); dbg_print(I); printf("\t"); \
+                printf("%s", #J ": "); dbg_print(J); printf("\n"); \
+                fflush(stdout); aSsErT(1, #X, __LINE__); } }
+
+#define LOOP3_ASSERT(I,J,K,X) {                    \
+    if (!(X)) { printf("%s", #I ": "); dbg_print(I); printf("\t"); \
+                printf("%s", #J ": "); dbg_print(J); printf("\t"); \
+                printf("%s", #K ": "); dbg_print(K); printf("\n"); \
+                fflush(stdout); aSsErT(1, #X, __LINE__); } }
+
+#define LOOP4_ASSERT(I,J,K,L,X) {                  \
+    if (!(X)) { printf("%s", #I ": "); dbg_print(I); printf("\t"); \
+                printf("%s", #J ": "); dbg_print(J); printf("\t"); \
+                printf("%s", #K ": "); dbg_print(K); printf("\t"); \
+                printf("%s", #L ": "); dbg_print(L); printf("\n"); \
+                fflush(stdout); aSsErT(1, #X, __LINE__); } }
+
+#define LOOP5_ASSERT(I,J,K,L,M,X) {                \
+    if (!(X)) { printf("%s", #I ": "); dbg_print(I); printf("\t"); \
+                printf("%s", #J ": "); dbg_print(J); printf("\t"); \
+                printf("%s", #K ": "); dbg_print(K); printf("\t"); \
+                printf("%s", #L ": "); dbg_print(L); printf("\t"); \
+                printf("%s", #M ": "); dbg_print(M); printf("\n"); \
+                fflush(stdout); aSsErT(1, #X, __LINE__); } }
+
+//=============================================================================
+//                  SEMI-STANDARD TEST OUTPUT MACROS
+//-----------------------------------------------------------------------------
+#define Q(X) printf("<| " #X " |>\n");     // Quote identifier literally.
+#define P(X) dbg_print(#X " = ", X, "\n")  // Print identifier and value.
+#define P_(X) dbg_print(#X " = ", X, ", ") // P(X) without '\n'
+#define L_ __LINE__                        // current Line number
+#define T_ putchar('\t');                  // Print a tab (w/o newline)
 
 //=============================================================================
 //                  GLOBAL TYPEDEFS/CONSTANTS FOR TESTING
 //-----------------------------------------------------------------------------
 
-// verify that the 'VALUE' member is evaluated at compile-time
+template <class TYPE, TYPE VALUE>
+struct matchIntegerConstant
+{
+static bool test(bsltt::integer_constant<TYPE, VALUE>)
+    // Return true when called with an 'integer_constant' of the specified
+    // 'VALUE'.  Does not participate in overload resolution for
+    // 'integer_constant's who's 'value' is not 'VALUE'.
+{
+    return true;
+}
 
-enum {
-    C0 = 1 + bslmf::MetaInt<0>::VALUE,  // 1
-    C1 = 1 + bslmf::MetaInt<1>::VALUE,  // 2
-    C2 = 1 + bslmf::MetaInt<2>::VALUE   // 3
+template <TYPE OTHER_VALUE>
+static bool test(bsltt::integer_constant<TYPE, OTHER_VALUE>)
+    // Return false.  Overload resolution will select this function only when
+    // the argument is other than 'integer_constant<TYPE, VALUE>', i.e., when
+    // the specified 'VALUE' is different from the specified 'OTHER_VALUE'.
+{
+    return false;
+}
 };
 
-template <class T>
-void doSomethingImp(T *t, bslmf::MetaInt<0>)
-{
-    // slow but generic implementation
-}
+//=============================================================================
+//                  CODE FOR TESTING USAGE EXAMPLES
+//-----------------------------------------------------------------------------
 
-template <class T>
-void doSomethingImp(T *t, bslmf::MetaInt<1>)
-{
-    // fast implementation (works only for some T's)
-}
-
-template <class T, bool IsFast>
-void doSomething(T *t)
-{
-    doSomethingImp(t, bslmf::MetaInt<IsFast>());
-}
-
-void f()
-{
-    int i;
-    doSomething<int, true>(&i); // fast version selected for int
-
-    double m;
-    doSomething<double, false>(&m); // slow version selected for double
-}
-
-template <int V>
-int g()
-{
-    bslmf::MetaInt<V> i;
-    std::cout << i.VALUE << std::endl;
-    std::cout << bslmf::MetaInt<V>::VALUE << std::endl;
-    return bslmf::MetaInt<V>::VALUE;
-}
-
-void h()
-{
-    1 == g<1>(); // prints the number '1' twice
-}
+///Example 1: Compile-Time Function Dispatching
+/// - - - - - - - - - - - - - - - - - - - - - - 
+// The most common use of this structure is to perform static function
+// dispatching based on a compile-time calculation.  Often the calculation is
+// nothing more than a simple predicate, allowing us to select one of two
+// functions.  The following function, 'doSomething', uses a fast
+// implementation (e.g., 'memcpy') if the parameterized type allows for such
+// operations, otherwise it will use a more generic and slower implementation
+// (e.g., copy constructor).
+//..
+    template <class T>
+    void doSomethingImp(T *t, bslmf::MetaInt<0>)
+    {
+        // slow generic implementation
+        (void) t;
+        // ...
+    }
+  
+    template <class T>
+    void doSomethingImp(T *t, bslmf::MetaInt<1>)
+    {
+        // fast implementation (works only for some T's)
+        (void) t;
+        // ...
+    }
+  
+    template <class T, bool IsFast>
+    void doSomething(T *t)
+    {
+        doSomethingImp(t, bslmf::MetaInt<IsFast>());
+    }
+//..
+// The power of this approach is that the compiler will compile only the
+// implementation selected by the 'MetaInt' argument.  For some parameter
+// types, the fast version of 'doSomethingImp' would be ill-formed.  This kind
+// of compile-time dispatch prevents the ill-formed version from ever being
+// instantiated.
+//..
+    int usageExample1()
+    {
+        int i;
+        doSomething<int, true>(&i); // fast version selected for int
+  
+        double m;
+        doSomething<double, false>(&m); // slow version selected for double
+  
+        return 0;
+    }
+//..
+///Example 2: Reading the 'VALUE' member
+/// - - - - - - - - - - - - - - - - - - 
+// In addition to forming new types, the value of the integral paramameter to
+// 'MetaInt' is "saved" in the enum member 'VALUE', and is accessible for use
+// in compile-time or run-time operations.
+//..
+    template <int V>
+    unsigned g()
+    {
+        bslmf::MetaInt<V> i;
+        ASSERT(V == i.VALUE);
+        ASSERT(V == bslmf::MetaInt<V>::VALUE);
+        return bslmf::MetaInt<V>::VALUE;
+    }
+  
+    int usageExample2()
+    {
+        int v = g<1>();
+        ASSERT(1 == v);
+        return 0;
+    }
+//..
 
 //=============================================================================
 //                              MAIN PROGRAM
@@ -106,13 +198,14 @@ void h()
 int main(int argc, char *argv[])
 {
     int test = argc > 1 ? atoi(argv[1]) : 0;
-    int verbose = argc > 2;
-    // int veryVerbose = argc > 3;
+    verbose = argc > 2;
+    veryVerbose = argc > 3;
+    veryVeryVerbose = argc > 4;
 
-    cout << "TEST " << __FILE__ << " CASE " << test << endl;
+    printf("TEST " __FILE__ " CASE %d\n", test);
 
     switch (test) { case 0:  // Zero is always the leading case.
-      case 2: {
+      case 3: {
         // --------------------------------------------------------------------
         // TESTING USAGE EXAMPLE
         //   The usage example provided in the component header file must
@@ -126,52 +219,118 @@ int main(int argc, char *argv[])
         //   USAGE EXAMPLE
         // --------------------------------------------------------------------
 
-        if (verbose) cout << endl
-                          << "Testing Usage Example" << endl
-                          << "=====================" << endl;
-        f();
-        if (verbose) {
-            h();
-        }
+        if (verbose) printf("\nTesting USAGE EXAMPLE"
+                            "\n=====================\n");
+
+        usageExample1();
+        usageExample2();
+
+      } break;
+      case 2: {
+        // --------------------------------------------------------------------
+        // TESTING CONVERSION TO integer_constant
+        //
+        // Concerns:
+        //: 1 'MetaInt<V>' is convertible to 'integer_constant<T, V>', where
+        //:   'T' is an integer type.
+        //:
+        //: 2 'MetaInt<V>' is NOT convertible to 'integer_constant<T, X>',
+        //:   where 'T' is an integer type and 'X != V'.
+        //
+        // Plan:
+        //: 1 Create a function template, 'matchIntegerConstant<T, V>' having
+        //:   two overloads: one that takes an argument of type
+        //:   'integer_constant<T, V>' and returns 'true', and another which
+        //:   takes an argument of *any* 'integer_constant' and returns
+        //:   'false'.  For various integer types, 'T', and values 'V',
+        //:   construct rvalues of type 'MetaInt<V>' and call
+        //:   'matchIntegerConstant<T, V>', verifying that it returns 'true'.
+        //:
+        //: 2 For various integer types, 'T', and values 'V' and 'X' such that
+        //:   'V != X', construct rvalues of type 'MetaInt<V>' and call
+        //:   'matchIntegerConstant<T, X>', verifying that it returns 'false'.
+        //
+        // Testing:
+        //     operator bsltt::integer_constant<TYPE, (TYPE)INT_VALUE>() const;
+        // --------------------------------------------------------------------
+
+        if (verbose) printf("\nTESTING CONVERSION TO integer_constant"
+                            "\n======================================\n");
+
+#if 0 // Not working yet
+        if (veryVerbose) printf("Testing good conversions\n");
+        ASSERT(  (matchIntegerConstant<bool, false>::test(MetaInt<false>())));
+        ASSERT(  (matchIntegerConstant<int, 1>::test(MetaInt<1>())));
+        ASSERT(  (matchIntegerConstant<unsigned, 2>::test(MetaInt<2>())));
+        ASSERT(  (matchIntegerConstant<short, -3>::test(MetaInt<-3>())));
+        ASSERT(  (matchIntegerConstant<long, 4>::test(MetaInt<4>())));
+        ASSERT(  (matchIntegerConstant<unsigned char, 'A'>::test(MetaInt<'A'>())));
+
+        if (veryVerbose) printf("Testing bad conversions\n");
+        ASSERT(! (matchIntegerConstant<bool, false>::test(MetaInt<true>())));
+        ASSERT(! (matchIntegerConstant<int, 1>::test(MetaInt<2>())));
+        ASSERT(! (matchIntegerConstant<unsigned, 2>::test(MetaInt<4>())));
+        ASSERT(! (matchIntegerConstant<short, -3>::test(MetaInt<-6>())));
+        ASSERT(! (matchIntegerConstant<long, 4>::test(MetaInt<8>())));
+        ASSERT(! (matchIntegerConstant<unsigned char, 'A'>::test(MetaInt<'Z'>())));
+#endif
+
       } break;
       case 1: {
         // --------------------------------------------------------------------
+        // VALUE TEST
+        //
         // Test Plan:
-        //   Instantiate 'bslmf::MetaInt' with various constant integral
+        //   Instantiate 'MetaInt' with various constant integral
         //   values and verify that their 'VALUE' member is initialized
         //   properly.
         // --------------------------------------------------------------------
 
-        if (verbose) cout << endl
-                          << "bslmf::MetaInt" << endl
-                          << "==============" << endl;
+        if (verbose) printf("\nVALUE TEST"
+                            "\n==========\n");
 
-        ASSERT(1 == C0);
-        ASSERT(2 == C1);
-        ASSERT(3 == C2);
+        // verify that the 'VALUE' member is evaluated at compile-time
+        enum {
+            C1 = bslmf::MetaInt<1>::VALUE,
+            C2 = bslmf::MetaInt<2>::VALUE,
+            C0 = bslmf::MetaInt<0>::VALUE
+        };
 
-        bslmf::MetaInt<0> i0; ASSERT(0 == i0.VALUE);
-        bslmf::MetaInt<1> i1; ASSERT(1 == i1.VALUE);
-        bslmf::MetaInt<2> i2; ASSERT(2 == i2.VALUE);
+        ASSERT(0 == C0);
+        ASSERT(1 == C1);
+        ASSERT(2 == C2);
+
+        bslmf::MetaInt<0> i0;
+        bslmf::MetaInt<1> i1;
+        bslmf::MetaInt<2> i2;
+
+        ASSERT(0 == i0.VALUE);
+        ASSERT(1 == i1.VALUE);
+        ASSERT(2 == i2.VALUE);
+
+        // Check inherited value
+        ASSERT(0 == i0.value);
+        ASSERT(1 == i1.value);
+        ASSERT(2 == i2.value);
 
         ASSERT(-5 == bslmf::MetaInt<(unsigned)-5>::VALUE);
+
       } break;
       default: {
-        cerr << "WARNING: CASE `" << test << "' NOT FOUND." << endl;
+        fprintf(stderr, "WARNING: CASE `%d' NOT FOUND.\n", test);
         testStatus = -1;
       }
     }
 
     if (testStatus > 0) {
-        cerr << "Error, non-zero test status = "
-             << testStatus << "." << endl;
+        fprintf(stderr, "Error, non-zero test status = %d.\n", testStatus);
     }
     return testStatus;
 }
 
 // ---------------------------------------------------------------------------
 // NOTICE:
-//      Copyright (C) Bloomberg L.P., 2002
+//      Copyright (C) Bloomberg L.P., 2002-2012
 //      All Rights Reserved.
 //      Property of Bloomberg L.P. (BLP)
 //      This software is made available solely pursuant to the
