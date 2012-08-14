2df68eb83db499ed77578cd944f72ead53e400fc
Mike Giroux <mgiroux@bloomberg.net>
2012-08-14 12:04:53 -0400

    move testStatus and aSsErT to anon namespace so they are template-accessible

--- groups/bsl/bslstl/bslstl_bitset.t.cpp
+++ groups/bsl/bslstl/bslstl_bitset.t.cpp
@@ -105,15 +105,18 @@ using namespace std;
 //==========================================================================
 //                  STANDARD BDE ASSERT TEST MACRO
 //--------------------------------------------------------------------------
-static int testStatus = 0;
 
-static void aSsErT(int c, const char *s, int i) {
+namespace {
+int testStatus = 0;
+
+void aSsErT(int c, const char *s, int i) {
     if (c) {
         cout << "Error " << __FILE__ << "(" << i << "): " << s
              << "    (failed)" << endl;
         if (testStatus >= 0 && testStatus <= 100) ++testStatus;
     }
 }
+} // close anonymous namespace
 
 # define ASSERT(X) { aSsErT(!(X), #X, __LINE__); }
 //--------------------------------------------------------------------------
