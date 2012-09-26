756a84043574f5a230e7ba13b8807e3159c804ed
Shijin Kong <skong25@bloomberg.net>
2012-09-26 11:08:38 -0400

    Doc and td for bslmf_addlvaluereference

--- groups/bsl/bslmf/bslmf_addlvaluereference.h
+++ groups/bsl/bslmf/bslmf_addlvaluereference.h
@@ -7,6 +7,49 @@
 #endif
 BSLS_IDENT("$Id: $")
 
+//@PURPOSE: Provide a compile-time type transformation to lvalue reference.
+//
+//@CLASSES:
+//  bsl::add_lvalue_reference: standard meta-function for transforming type
+//
+//@SEE_ALSO: bslmf_integralconstant
+//
+//@AUTHOR:
+//
+//@DESCRIPTION: This component defines a meta-function,
+// 'bsl::add_lvalue_reference', which may be used to transfer a type to a
+// reference type.
+//
+// 'bsl::add_lvalue_reference' meets the requirements of the
+// 'add_lvalue_reference' template defined in the C++11 standard
+// [meta.trans.ref].
+//
+///Usage
+///-----
+// In this section we show intended use of this component.
+//
+///Example 1: Transform to Lvalue Reference Types
+/// - - - - - - - - - - - - - - - - - - - - - - -
+// Suppose that we want to transform a couple of types to reference types.
+//
+// Now, we instantiate the 'bsl::add_lvalue_reference' template for these
+// types, and use the 'bsl::is_same' meta-function to assert the 'type' static
+// data member of each instantiation:
+//..
+//  assert(true ==
+//        (bsl::is_same<bsl::add_lvalue_reference<int>::type,   int&>::value));
+//  assert(false ==
+//        (bsl::is_same<bsl::add_lvalue_reference<int>::type,   int >::value));
+//  assert(true ==
+//        (bsl::is_same<bsl::add_lvalue_reference<int&>::type,  int&>::value));
+//#if defined(BSLS_COMPILERFEATURES_SUPPORT_RVALUE_REFERENCES)
+//  assert(true ==
+//        (bsl::is_same<bsl::add_lvalue_reference<int&&>::type, int&>::value));
+//#endif
+//..
+// Note that rvalue is introduced in C++11 and may not be supported by all
+// compilers.
+
 #ifndef INCLUDED_BSLSCM_VERSION
 #include <bslscm_version.h>
 #endif
@@ -53,7 +96,7 @@ struct add_lvalue_reference<TYPE&&>
 
 #endif
 
-}
+}  // close namespace bsl
 
 #endif
 
