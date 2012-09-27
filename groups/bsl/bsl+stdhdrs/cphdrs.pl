d8e4430dec16a7448100dc4c9ee7ff7acc462bb3
Raymond Chiu <schiu49@bloomberg.net>
2012-09-27 09:39:17 -0400

    Remove license accidentally added to a perl script.

--- groups/bsl/bsl+stdhdrs/cphdrs.pl
+++ groups/bsl/bsl+stdhdrs/cphdrs.pl
@@ -50,12 +50,3 @@ if (($source =~ /dummy/) && ($target =~ /dummy/)) {
 
 docopy($source, $target) if (-f $source);
 docopy("$source.SUNWCCh", "$target.SUNWCCh") if (-f "$source.SUNWCCh");
-
-// ----------------------------------------------------------------------------
-// NOTICE:
-//      Copyright (C) Bloomberg L.P., 2012
-//      All Rights Reserved.
-//      Property of Bloomberg L.P.  (BLP)
-//      This software is made available solely pursuant to the
-//      terms of a BLP license agreement which governs its use.
-// ----------------------------- END-OF-FILE ----------------------------------
