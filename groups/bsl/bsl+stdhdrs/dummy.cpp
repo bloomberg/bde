53e8236dbc5d201a4ad2b1e514d0df7b280d0715
(Henry) Mike Verschell <hverschell@bloomberg.net>
2012-09-30 18:30:55 -0400

    add licenses to a few non opensource files

--- groups/bsl/bsl+stdhdrs/dummy.cpp
+++ groups/bsl/bsl+stdhdrs/dummy.cpp
@@ -18,10 +18,9 @@ char stdhdrs_dummy_cpp_this_symbol_avoids_an_empty_package_library;
 
 // ---------------------------------------------------------------------------
 // NOTICE:
-//      Copyright (C) Bloomberg L.P., 2009
+//      Copyright (C) Bloomberg L.P., 2012
 //      All Rights Reserved.
 //      Property of Bloomberg L.P. (BLP)
 //      This software is made available solely pursuant to the
 //      terms of a BLP license agreement which governs its use.
 // ----------------------------- END-OF-FILE ---------------------------------
-
