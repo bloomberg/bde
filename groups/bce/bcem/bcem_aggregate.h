613dbb7eb818fc64b61a60afecd8003c72d3076f
Rohan Bhindwale <rbhindwale@bloomberg.net>
2012-11-07 14:40:11 -0500

    cosmetica

--- groups/bce/bcem/bcem_aggregate.h
+++ groups/bce/bcem/bcem_aggregate.h
@@ -1676,7 +1676,7 @@ class bcem_Aggregate {
         // 'true'.  Note that this function should rarely be needed; generally
         // 'isNul2()' should be used instead; 'isUnset()' will be removed in a
         // future release.
-#endif  // BDE_OMIT_INTERNAL_DEPRECATED
+#endif // BDE_OMIT_INTERNAL_DEPRECATED
 
     int errorCode() const;
         // Return a negative error code describing the the status of this
