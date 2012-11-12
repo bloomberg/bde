910ebc3772f646752215a5b94b189d0f975c27f8
Alexander Beels <abeels@bloomberg.net>
2012-11-12 11:17:03 -0500

    Remove exception for bslalg_constructorproxy test driver on AIX shared library builds.

--- tools/test_filter.py
+++ tools/test_filter.py
@@ -2,7 +2,6 @@
     'bslmf_addreference': [ {'OS': 'Windows'} ],
     'bslstl_iteratorutil': [ {'OS': 'SunOS'} ],
     'bslstl_unorderedmultiset': [ {'OS': 'SunOS'} ],
-    'bslalg_constructorproxy': [ {'OS': 'AIX', 'library': 'shared_library'} ],
     'bsls_atomic' : [
         {'case': 7, 'HOST': 'VM', 'policy': 'skip' },
         {'case': 8, 'HOST': 'VM', 'policy': 'skip' },
