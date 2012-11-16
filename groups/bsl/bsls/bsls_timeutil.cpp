17369ca80d211181db434195b46ee06c240333c5
Alexander Beels <abeels@bloomberg.net>
2012-11-16 14:26:58 -0500

    Separate includes for Darwin and Solaris to highlight their different rationales.

--- groups/bsl/bsls/bsls_timeutil.cpp
+++ groups/bsl/bsls/bsls_timeutil.cpp
@@ -21,9 +21,10 @@ BSLS_IDENT("$Id$ $CSID$")
     #error "Don't know how to get nanosecond time for this platform"
 #endif
 
-#if defined(BSLS_PLATFORM_OS_UNIX) && \
-    !(defined(BSLS_PLATFORM_OS_LINUX) || defined(BSLS_PLATFORM_OS_AIX))
-    #include <sys/time.h>  // gethrtime(), gettimeofday()
+#if defined(BSLS_PLATFORM_OS_SOLARIS)
+    #include <sys/time.h>  // gethrtime()
+#elif defined(BSLS_PLATFORM_OS_DARWIN)
+    #include <sys/time.h>  // gettimeofday()
 #endif
 
 namespace BloombergLP {
