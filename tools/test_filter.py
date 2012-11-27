8378b474fca360696adc8a7c11439ac78912fab4
Alexander Beels <abeels@bloomberg.net>
2012-11-27 17:57:14 -0500

    Remove test driver exceptions for bslstl_iteratorutil and bslstl_unorderedmultiset on Sun

--- tools/test_filter.py
+++ tools/test_filter.py
@@ -1,6 +1,4 @@
 {
-    'bslstl_iteratorutil': [ {'OS': 'SunOS'} ],
-    'bslstl_unorderedmultiset': [ {'OS': 'SunOS'} ],
     'bsls_atomic' : [
         {'case': 7, 'HOST': 'VM', 'policy': 'skip' },
         {'case': 8, 'HOST': 'VM', 'policy': 'skip' },
