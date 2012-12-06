62790a08e0a35365baccc1238d8208db6802de85
Alexander Beels <abeels@bloomberg.net>
2012-12-05 19:48:21 -0500

    Remove stlport text from bslstl_bitset.h

--- groups/bsl/bslstl/bslstl_bitset.h
+++ groups/bsl/bslstl/bslstl_bitset.h
@@ -20,26 +20,6 @@ BSLS_IDENT("$Id: $")
 // '<bsl_bitset.h>' instead and use 'bsl::bitset' directly.  This component
 // implements a static bitset class that is suitable for use as an
 // implementation of the 'std::bitset' class template.
-//
-// Note that the bitset class here is based on STLPort's implementation, with
-// copyright notice as follows:
-//..
-//-----------------------------------------------------------------------------
-// Copyright (c) 1998
-// Silicon Graphics Computer Systems, Inc.
-//
-// Copyright (c) 1999
-// Boris Fomitchev
-//
-// This material is provided "as is", with absolutely no warranty expressed
-// or implied.  Any use is at your own risk.
-//
-// Permission to use or copy this software for any purpose is hereby granted
-// without fee, provided the above notices are retained on all copies.
-// Permission to modify the code and to distribute modified code is granted,
-// provided the above notices are retained, and a notice that the code was
-// modified is included with the above copyright notice.
-//-----------------------------------------------------------------------------
 //..
 //
 ///Usage
