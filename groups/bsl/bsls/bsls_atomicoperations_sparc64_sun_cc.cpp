ba6a76f6292b03130749ade9e26591d94e6ae638
Open Bloomberg <open-tech@bloomberg.net>
2012-12-11 12:00:42 -0500

    Import internal commit 0f93d1a4d00712c7afdf820f6a2ea93bbbb5e6e7 : [APPROVAL] pending/master/bsls-atomics-Solaris-Gcc-Code-Missing-drqs-35034529 to master

--- groups/bsl/bsls/bsls_atomicoperations_sparc64_sun_cc.cpp
+++ groups/bsl/bsls/bsls_atomicoperations_sparc64_sun_cc.cpp
@@ -5,17 +5,23 @@
 #include <bsls_ident.h>
 BSLS_IDENT("$Id$ $CSID$")
 
+#include <bsls_annotation.h>
+
 #if defined(BSLS_PLATFORM_CPU_SPARC_V9) \
     && (defined(BSLS_PLATFORM_CMP_GNU) || defined(BSLS_PLATFORM_CMP_SUN))
 
 namespace BloombergLP {
 
 static
+void bsls_AtomicOperations_Sparc64_AssemblyContainer() BSLS_ANNOTATION_USED;
+    // Never called, just holds asembly code.
+
 void bsls_AtomicOperations_Sparc64_AssemblyContainer()
 {
-        // *** 64 bit operations ***
+                        // *** 64 bit operations ***
 
     // Int64 bsls_AtomicOperations_Sparc64_GetInt64(const volatile Int64*);
+
     asm(".global bsls_AtomicOperations_Sparc64_GetInt64\n"
         ".type bsls_AtomicOperations_Sparc64_GetInt64,#function\n"
         "bsls_AtomicOperations_Sparc64_GetInt64:\n"
@@ -24,6 +30,7 @@ void bsls_AtomicOperations_Sparc64_AssemblyContainer()
         "ldx [%o0], %o0");
 
     // void bsls_AtomicOperations_Sparc64_SetInt64(volatile Int64 *, Int64);
+
     asm(".global bsls_AtomicOperations_Sparc64_SetInt64\n"
         ".type bsls_AtomicOperations_Sparc64_SetInt64,#function\n"
         "bsls_AtomicOperations_Sparc64_SetInt64:\n"
@@ -32,6 +39,7 @@ void bsls_AtomicOperations_Sparc64_AssemblyContainer()
         "membar #StoreLoad");
 
     // Int64 bsls_AtomicOperations_Sparc64_SwapInt64(volatile Int64 *, Int64);
+
     asm(".global bsls_AtomicOperations_Sparc64_SwapInt64\n"
         ".type bsls_AtomicOperations_Sparc64_SwapInt64,#function\n"
         "bsls_AtomicOperations_Sparc64_SwapInt64:\n"
@@ -48,6 +56,7 @@ void bsls_AtomicOperations_Sparc64_AssemblyContainer()
 
     // Int64 bsls_AtomicOperations_Sparc64_SwapInt64AcqRel(
     //                                                volatile Int64 *, Int64);
+
     asm(".global bsls_AtomicOperations_Sparc64_SwapInt64AcqRel\n"
         ".type bsls_AtomicOperations_Sparc64_SwapInt64AcqRel,#function\n"
         "bsls_AtomicOperations_Sparc64_SwapInt64AcqRel:\n"
@@ -63,6 +72,7 @@ void bsls_AtomicOperations_Sparc64_AssemblyContainer()
 
     // Int64 bsls_AtomicOperations_Sparc64_TestAndSwapInt64(
     //                                         volatile Int64 *, Int64, Int64);
+
     asm(".global bsls_AtomicOperations_Sparc64_TestAndSwapInt64\n"
         ".type bsls_AtomicOperations_Sparc64_TestAndSwapInt64,#function\n"
         "bsls_AtomicOperations_Sparc64_TestAndSwapInt64:\n"
@@ -73,6 +83,7 @@ void bsls_AtomicOperations_Sparc64_AssemblyContainer()
 
     // Int64 bsls_AtomicOperations_Sparc64_TestAndSwapInt64AcqRel(
     //                                         volatile Int64 *, Int64, Int64);
+
     asm(".global bsls_AtomicOperations_Sparc64_TestAndSwapInt64AcqRel\n"
        ".type bsls_AtomicOperations_Sparc64_TestAndSwapInt64AcqRel,#function\n"
         "bsls_AtomicOperations_Sparc64_TestAndSwapInt64AcqRel:\n"
@@ -81,6 +92,7 @@ void bsls_AtomicOperations_Sparc64_AssemblyContainer()
         "mov %o2, %o0");
 
     // Int64 bsls_AtomicOperations_Sparc64_AddInt64(volatile Int64 *, Int64);
+
     asm(".global bsls_AtomicOperations_Sparc64_AddInt64\n"
         ".type bsls_AtomicOperations_Sparc64_AddInt64,#function\n"
         "bsls_AtomicOperations_Sparc64_AddInt64:\n"
@@ -97,6 +109,7 @@ void bsls_AtomicOperations_Sparc64_AssemblyContainer()
 
     // Int64 bsls_AtomicOperations_Sparc64_AddInt64Relaxed(
     //                                                volatile Int64 *, Int64);
+
     asm(".global bsls_AtomicOperations_Sparc64_AddInt64Relaxed\n"
         ".type bsls_AtomicOperations_Sparc64_AddInt64Relaxed,#function\n"
         "bsls_AtomicOperations_Sparc64_AddInt64Relaxed:\n"
