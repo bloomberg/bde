4281fff847160d93cd6a6a6efefc80aec6f2302e
Alexander Beels <abeels@bloomberg.net>
2013-01-01 16:04:47 -0500

    Add JUnit-style output format to run_unit_tests.py.
    This patch adds a new output format to the test driver execution script,
    which captures test case output, collects results and emits them in a
    form that other tools can consume. This mode is enabled by specifying
    "--junit=<file.xml>" to the script.

--- tools/run_unit_tests.py
+++ tools/run_unit_tests.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+# Usage: (python2.6) run_unit_tests.py test_driver.t test_driver_flag_file [--abi=<bits>] [--libs=<static or shared>] [--junit=junitoutput.xml]
 # Arguments: test (*.t) and target (*.t.ran)
 # Options  : --abi=<ABI_bits setting>
 #            --libs=<static_library|shared_library>
+#            --junit=<JUnit XML output file>
 
 import os
 import sys
@@ -11,19 +13,246 @@ import platform
 import re
 import threading
 
+from datetime import datetime
 from optparse import OptionParser
+
+import xml.etree.ElementTree as ET
+
+def parseOptions():
+    parser = OptionParser()
+    parser.add_option("--abi", dest="abi")
+    parser.add_option("--lib", dest="lib")
+    parser.add_option("--junit", dest="junit")
+    (options, args) = parser.parse_args()
+
+    return options
+
+commandLineOptions = parseOptions()
+out = None
+
+class TextOutputGenerator:
+    msbuildErrorMarker = re.compile("Error([^:]*):")
+
+    def __init__(self):
+        self.verbose = False
+
+        if PolicyFilter.criteria['OS'] == 'Windows':
+            self.reformatWarnings = True
+        else:
+            self.reformatWarnings = False
+        return
+
+    def reformatWindowsOutput(self, line):
+        # Reformat lines containing 'Error.*:' on Windows,
+        # because msbuild will interpret them as test failures
+        # even when they are legitimate test output.
+        match = TextOutputGenerator.msbuildErrorMarker.search(line)
+        if match:
+            return "%sError%s - %s\n" % (line[:match.start(0)], line[match.start(1):match.end(1)], line[match.end(0):])
+        else:
+            return line
+
+    def startTestSuite(self, name, verbosity, timeout):
+        if verbosity >= 0:
+            self.verbose = True
+        return
+
+    def endTestSuite(self, returncode):
+        return returncode
+
+    def reportTestSuiteError(self, variable, value):
+        print >>sys.stderr, "Invalid value for environment variable '%s': %s" % (variable, value)
+        return
+
+    def startTestCase(self, testNumber):
+        return
+
+    def endTestCase(self):
+        return
+
+    def skipTestCase(self, command):
+        if self.verbose:
+            print "Skipping '%s'\n" % (command)
+        return
+                    
+    def runTestCase(self, command):
+        if self.verbose:
+            print "Running '%s'\n" % (command)
+        return
+
+    def startTestCaseOutput(self):
+        return
+
+    def printTestCaseOutput(self, line):
+        if self.reformatWarnings:
+            print self.reformatWindowsOutput(line) ,
+        else:
+            print line ,
+        return
+
+    def endTestCaseOutput(self):
+        return
+
+    def reportTestCaseError(self, prefix, command, testNumber, error):
+        print >>sys.stderr, "Failed to run %s %d: %s" \
+            % (sys.argv[1], testNumber, error)
+        return
+
+    def reportTestCaseFailure(self, returncode):
+        print >>sys.stderr, "Abnormal test failure: %d" % (returncode)
+        return
+                        
+    def reportExcpectedTestCaseFailure(self, test, testNumber, returncode):
+        print >>sys.stderr, "Test failure for case %d of test %s was expected."\
+                                                 % (testNumber, test)
+        return
+
+    def reportTestCaseTimeout(self):
+        print >>sys.stderr, "Aborting test due to timeout"
+        return
+    
+class JUnitOutputGenerator:
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.suite = ET.Element('testsuite')
+        ET.SubElement(self.suite, 'properties')
+        self.tree = ET.ElementTree(self.suite)
+
+        self.output = None
+        self.currentCase = None
+
+        self.testCount = 0
+        self.failureCount = 0
+        self.errorCount = 0
+        self.skipCount = 0
+        self.timestamp = datetime.utcnow()
+        self.suite.set('timestamp', self.timestamp.isoformat())
+        return
+
+    def startTestSuite(self, name, verbosity, timeout):
+        name = os.path.basename(name)
+        name = name.partition(".")[0]
+        self.suite.set('name', name.partition("_")[0] + "." + name.partition("_")[2])
+        properties = self.suite.find('properties')
+        verbosityProperty = ET.SubElement(properties, 'property')
+        verbosityProperty.set('name', 'verbosity')
+        verbosityProperty.set('value', '%d' % verbosity)
+        timeoutProperty = ET.SubElement(properties, 'property')
+        timeoutProperty.set('name', 'timeout')
+        timeoutProperty.set('value', '%d' % timeout)
+        self.timeout = timeout
+        return
+
+    def endTestSuite(self, returncode):
+        if not self.currentCase.get('status'):
+            # Last case never closed: missing test case
+            self.suite.remove(self.currentCase)
+
+        self.suite.set('tests', '%d' % (self.testCount))
+        self.suite.set('failures', '%d' % (self.failureCount))
+        self.suite.set('errors', '%d' % (self.errorCount))
+        self.suite.set('skipped', '%d' % (self.skipCount))
+
+        endTimestamp = datetime.utcnow()
+        delta = endTimestamp - self.timestamp
+        self.suite.set('time', '%d.%0.6d' % (delta.days * 3600 * 24 + delta.seconds, delta.microseconds))
+
+        self.tree.write(self.fileName)
+        # JUnit-style output means test failures should not stop execution of further tests,
+        # so set the returncode to zero here
+        return 0
+
+    def reportTestSuiteError(self, variable, value):
+        print >>sys.stderr, "Invalid value for environment variable '%s': %s" % (variable, value)
+        properties = self.suite.find('properties')
+        verbosityProperty = ET.SubElement(properties, 'property')
+        verbosityProperty.set('name', 'error')
+        verbosityProperty.set('value', "Invalid value for environment variable '%s': %s" % (variable, value))
+        self.errorCount += 1
+        return
+
+    def startTestCase(self, testNumber):
+        testcase = ET.SubElement(self.suite, 'testcase')
+        testcase.set('name', '%d' % testNumber)
+        self.currentCase = testcase
+        self.testCount += 1
+        self.currentCaseStartTime = datetime.utcnow()
+        return
+
+    def endTestCase(self):
+        if not self.currentCase.get('status'):
+            self.currentCase.set('status', 'passed')
+
+        endTimestamp = datetime.utcnow()
+        delta = endTimestamp - self.currentCaseStartTime
+        self.currentCase.set('time', '%d.%0.6d' % (delta.days * 3600 * 24 + delta.seconds, delta.microseconds))
+        self.currentCaseStartTime = None
+
+        return
+
+    def skipTestCase(self, command):
+        ET.SubElement(self.currentCase, 'skipped')
+        self.skipCount += 1
+        return
+                    
+    def runTestCase(self, command):
+        return
+
+    def startTestCaseOutput(self):
+        self.output = ET.SubElement(self.currentCase, 'system-out')
+        self.outputLines = []
+        return
+
+    def printTestCaseOutput(self, line):
+        self.outputLines.append(line)
+        return
+
+    def endTestCaseOutput(self):
+        self.output.text = ''.join(self.outputLines)
+        self.output = None
+        self.outputLines = None
+        return
+
+    def reportTestCaseError(self, prefix, command, testNumber, error):
+        error = ET.SubElement(self.currentCase, 'error')
+        error.set('message', "%s %s %d: %s" \
+            % (prefix, command, testNumber, error))
+        self.currentCase.set('status', 'error')
+        self.errorCount += 1
+        return
+
+    def reportTestCaseFailure(self, returncode):
+        failure = ET.SubElement(self.currentCase, 'failure')
+        failure.set('type', 'test failure')
+        failure.set('message', "Abnormal test failure: %d" % (returncode))
+        self.currentCase.set('status', 'failed')
+        self.failureCount += 1
+        return
+                        
+    def reportExcpectedTestCaseFailure(self, test, testNumber, returncode):
+        self.currentCase.set('status', 'expected failure')
+        return
+
+    def reportTestCaseTimeout(self):
+        failure = ET.SubElement(self.currentCase, 'failure')
+        failure.set('type', 'timeout')
+        failure.set('message', "Test timed out (timeout was set for %d seconds)" % self.timeout)
+        self.currentCase.set('status', 'failed')
+        self.failureCount += 1
+        return
+    
+def selectOutputType(options):
+    if options.junit:
+        return JUnitOutputGenerator(options.junit)
+    else:
+        return TextOutputGenerator()
 
 class Policy:
     test   = 0
     skip   = 1
     ignore = 2
 
-def determineCriteria():
-    parser = OptionParser()
-    parser.add_option("--abi", dest="abi")
-    parser.add_option("--lib", dest="lib")
-    (options, args) = parser.parse_args()
-
+def determineCriteria(options):
     criteria = {
         'case': None,
         'OS': None,
@@ -80,7 +309,7 @@ class PolicyFilter:
         'ABI': testAny,
         'library': testAny
     }
-    criteria = determineCriteria()
+    criteria = determineCriteria(commandLineOptions)
     policyTable = parsePolicyTable()
 
     @staticmethod
@@ -126,9 +355,15 @@ class TimeoutControl:
 
     def killProcess(self, process):
         with self.timeoutStatusLock:
-            print >>sys.stderr, "Aborting test due to timeout"
             self.isTimedOut = True
-        process.kill()
+        try:
+            process.kill()
+        except OSError, e:
+            if e.errno == 3:
+                # No such process: the process died just before we tried to kill it
+                pass
+            else:
+                out.reportTestCaseError("Failed to kill process on timeout", process.pid, -1, e)
 
     def __init__ (self, process, interval):
         self.isTimedOut = False
@@ -140,26 +375,7 @@ class TimeoutControl:
 
 class TestRunner:
     testOver = re.compile("^WARNING: CASE `[0-9]+' NOT FOUND.")
-    msbuildErrorMarker = re.compile("Error([^:]*):")
     
-    @staticmethod
-    def reformatWindowsOutput(line):
-        # Reformat lines containing 'Error.*:' on Windows,
-        # because msbuild will interpret them as test failures
-        # even when they are legitimate test output.
-        match = TestRunner.msbuildErrorMarker.search(line)
-        if match:
-            return "%sError%s - %s\n" % (line[:match.start(0)], line[match.start(1):match.end(1)], line[match.end(0):])
-        else:
-            return line
-            
-    @staticmethod
-    def printOutput(line):
-        if PolicyFilter.criteria['OS'] == 'Windows':
-            print >>sys.stderr, TestRunner.reformatWindowsOutput(line) ,
-        else:
-            print line ,
-
     @staticmethod
     def isLastLine(line):
         return TestRunner.testOver.match(line)
@@ -168,6 +384,7 @@ class TestRunner:
     def runTest(test, verbosityLevel, timeout):
         failures = 0
         testNumber = 1
+
         while True:
             if PolicyFilter.criteria['OS'] == 'Windows' \
                     and not test.lower().endswith('.exe'):
@@ -178,14 +395,15 @@ class TestRunner:
             policy = PolicyFilter.getTestPolicy(test, testNumber)
                 
             args = [program, str(testNumber)]
+            out.startTestCase(testNumber)
             if verbosityLevel >= 0:
                 args.extend(['v' for n in range(verbosityLevel)])
-                if policy == Policy.skip:
-                    TestRunner.printOutput("Skipping '%s'\n" % (' '.join(args)))
-                else:
-                    TestRunner.printOutput("Running '%s'\n" % (' '.join(args)))
+
+            if policy == Policy.skip:
+                out.skipTestCase(' '.join(args))
+            else:
+                out.runTestCase(' '.join(args))
                         
-            if policy != Policy.skip:
                 try:
                     
                     child = subprocess.Popen(args, \
@@ -197,31 +415,32 @@ class TestRunner:
                     output = child.stdout.readline()
                     # N.B. EOF is represented as an empty string.
                     # Empty line is represented as "\n"
-                    while output:
-                        if not TestRunner.isLastLine(output):
-                            TestRunner.printOutput(output)
-                        output = child.stdout.readline()
+                    if output:
+                        out.startTestCaseOutput()
+                        
+                        while output:
+                            if not TestRunner.isLastLine(output):
+                                out.printTestCaseOutput(output)
+                            output = child.stdout.readline()
 
+                        out.endTestCaseOutput()
                     returncode = child.wait()
 
                     timer.cancel()
 
                     if timer.timedOut():
-                        return 126
+                        returncode = 126
                     
                 except OSError, e:
                     # Couldn't start process, probably missing file
-                    print >>sys.stderr, "Failed to run %s %d: %s" \
-                        % (sys.argv[1], testNumber, e)
+                    out.reportTestCaseError("Failed to run", sys.argv[1], testNumber, e)
                     return -1
                 except ValueError, e:
                     # Invalid argument
-                    print >>sys.stderr, "Invalid argument to Popen while running %s %d: %s" \
-                        % (sys.argv[1], testNumber, e)
+                    out.reportTestCaseError("Invalid argument to Popen while running", sys.argv[1], testNumber, e)
                     return -1
                 except Exception, e:
-                    print >>sys.stderr, "ERROR while executing %s %d: %s" \
-                        % (sys.argv[1], testNumber, e)
+                    out.reportTestCaseError("ERROR while executing", sys.argv[1], testNumber, e)
                     return -1
                 
                 # On Linux, at least, returncode is always forced to unsigned,
@@ -231,24 +450,29 @@ class TestRunner:
                     # Missing test.  We're done.
                     return failures
                 
-                if returncode != 0:
+                if returncode == 126:
+                    out.reportTestCaseTimeout()
+                    failures += 1
+                elif returncode != 0:
                     if policy == Policy.ignore:
-                        print >>sys.stderr, "Test failure for case %d of test %s was expected."\
-                                                 % (testNumber, test)
+                        out.reportExcpectedTestCaseFailure(test, testNumber, returncode)
                     else:
                         # Test failure. Report it.
-                        print >>sys.stderr, "Abnormal test failure: %d" % (returncode)
+                        out.reportTestCaseFailure(returncode)
                         failures += 1
 
+            out.endTestCase()
             # Next test
             testNumber += 1
+
+out = selectOutputType(commandLineOptions)
 
 verbosityLevel = -1
 if 'V' in os.environ:
     try:
         verbosityLevel = int(os.environ['V'])
     except ValueError:
-        print >>sys.stderr, "Invalid value for environment variable 'V': %s" % (os.environ['V'])
+        out.reportTestSuiteError("V", os.environ['V'])
 
 # Timeout is specified in seconds
 timeout = 0
@@ -256,10 +480,13 @@ if 'TIMEOUT' in os.environ:
     try:
         timeout = int(os.environ['TIMEOUT'])
     except ValueError:
-        print >>sys.stderr, "Invalid value for environment variable 'TIMEOUT': %s" % (os.environ['TIMEOUT'])
-    
+        out.reportTestSuiteError("TIMEOUT", os.environ['TIMEOUT'])
+
+out.startTestSuite(sys.argv[1], verbosityLevel, timeout)
 
 returncode = TestRunner.runTest(sys.argv[1], verbosityLevel, timeout)
+
+returncode = out.endTestSuite(returncode)
 
 if returncode == 0:
     # We passed the test, mark the target
