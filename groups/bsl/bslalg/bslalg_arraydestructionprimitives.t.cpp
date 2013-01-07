bc00c8c210b913fda9e5bafcb98f437eb243b42f
Wei-Yeh Lee <weiyeh@inprotopia.com>
2013-01-07 12:23:58 -0500

    alpha sort includes

--- groups/bsl/bslalg/bslalg_arraydestructionprimitives.t.cpp
+++ groups/bsl/bslalg/bslalg_arraydestructionprimitives.t.cpp
@@ -4,14 +4,14 @@
 
 #include <bslalg_scalarprimitives.h>             // for testing only
 
-#include <bslmf_istriviallycopyable.h>           // for testing only
 #include <bslma_allocator.h>                     // for testing only
 #include <bslma_default.h>                       // for testing only
 #include <bslma_testallocator.h>                 // for testing only
+#include <bslmf_istriviallycopyable.h>           // for testing only
 #include <bsls_alignmentutil.h>                  // for testing only
-#include <bsls_objectbuffer.h>                   // for testing only
 #include <bsls_assert.h>                         // for testing only
 #include <bsls_asserttest.h>                     // for testing only
+#include <bsls_objectbuffer.h>                   // for testing only
 #include <bsls_types.h>                          // for testing only
 
 #include <stdio.h>
