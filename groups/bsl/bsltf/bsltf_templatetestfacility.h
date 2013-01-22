162ee1f164bb5770bd83027c9fa5261eaec173ac
Alisdair Meredith <ameredith1@bloomberg.net>
2013-01-22 17:55:19 -0500

    bsltf workaround for MSVC
    It looks like Microsoft cannot handle pointer-to
    -member types in bslalg_scalarprimitives.  Will
    need to investigate this further in 'bslalg' test
    drivers later, but for now this workaround appears
    to solve the problem for 'bsltf'.  Currently the
    workaround is enabled unless we define a macro with
    a crazy but descriptive name.  I will go back and
    remove this original version of the code once I
    verify that the Microsoft workaround, using simple
    assignment to initialize the result buffer, does
    not break on other platforms.  If that turns out
    to be an issue, we will simply rename the crazy
    macro as a platform-detect macro for the MSVC
    compiler.

--- groups/bsl/bsltf/bsltf_templatetestfacility.h
+++ groups/bsl/bsltf/bsltf_templatetestfacility.h
@@ -1132,6 +1132,7 @@ TemplateTestFacility::MethodPtr TemplateTestFacility::create<
     return 0;
 }
 
+#if defined(AJM_THINKS_THIS_SHOULD_WORK_BUT_MICROSOFT_DISAGREES)
 template <>
 inline
 void TemplateTestFacility::emplace<TemplateTestFacility::MethodPtr>(
@@ -1912,6 +1913,149 @@ void TemplateTestFacility::emplace<TemplateTestFacility::MethodPtr>(
       } break;
     }
 }
+#else
+template <>
+inline
+void TemplateTestFacility::emplace<TemplateTestFacility::MethodPtr>(
+                                   TemplateTestFacility::MethodPtr *address,
+                                   int                              identifier,
+                                   bslma::Allocator                *)
+{
+    BSLS_ASSERT_SAFE(address);
+    BSLS_ASSERT_SAFE(0 <= identifier);  BSLS_ASSERT_SAFE(identifier < 128);
+
+    switch (identifier) {
+      case   0: *address = &TemplateTestFacility_StubClass::method<  0>; break;
+      case   1: *address = &TemplateTestFacility_StubClass::method<  1>; break;
+      case   2: *address = &TemplateTestFacility_StubClass::method<  2>; break;
+      case   3: *address = &TemplateTestFacility_StubClass::method<  3>; break;
+      case   4: *address = &TemplateTestFacility_StubClass::method<  4>; break;
+      case   5: *address = &TemplateTestFacility_StubClass::method<  5>; break;
+      case   6: *address = &TemplateTestFacility_StubClass::method<  6>; break;
+      case   7: *address = &TemplateTestFacility_StubClass::method<  7>; break;
+      case   8: *address = &TemplateTestFacility_StubClass::method<  8>; break;
+      case   9: *address = &TemplateTestFacility_StubClass::method<  9>; break;
+      case  10: *address = &TemplateTestFacility_StubClass::method< 10>; break;
+      case  11: *address = &TemplateTestFacility_StubClass::method< 11>; break;
+      case  12: *address = &TemplateTestFacility_StubClass::method< 12>; break;
+      case  13: *address = &TemplateTestFacility_StubClass::method< 13>; break;
+      case  14: *address = &TemplateTestFacility_StubClass::method< 14>; break;
+      case  15: *address = &TemplateTestFacility_StubClass::method< 15>; break;
+      case  16: *address = &TemplateTestFacility_StubClass::method< 16>; break;
+      case  17: *address = &TemplateTestFacility_StubClass::method< 17>; break;
+      case  18: *address = &TemplateTestFacility_StubClass::method< 18>; break;
+      case  19: *address = &TemplateTestFacility_StubClass::method< 19>; break;
+      case  20: *address = &TemplateTestFacility_StubClass::method< 20>; break;
+      case  21: *address = &TemplateTestFacility_StubClass::method< 21>; break;
+      case  22: *address = &TemplateTestFacility_StubClass::method< 22>; break;
+      case  23: *address = &TemplateTestFacility_StubClass::method< 23>; break;
+      case  24: *address = &TemplateTestFacility_StubClass::method< 24>; break;
+      case  25: *address = &TemplateTestFacility_StubClass::method< 25>; break;
+      case  26: *address = &TemplateTestFacility_StubClass::method< 26>; break;
+      case  27: *address = &TemplateTestFacility_StubClass::method< 27>; break;
+      case  28: *address = &TemplateTestFacility_StubClass::method< 28>; break;
+      case  29: *address = &TemplateTestFacility_StubClass::method< 29>; break;
+      case  30: *address = &TemplateTestFacility_StubClass::method< 30>; break;
+      case  31: *address = &TemplateTestFacility_StubClass::method< 31>; break;
+      case  32: *address = &TemplateTestFacility_StubClass::method< 32>; break;
+      case  33: *address = &TemplateTestFacility_StubClass::method< 33>; break;
+      case  34: *address = &TemplateTestFacility_StubClass::method< 34>; break;
+      case  35: *address = &TemplateTestFacility_StubClass::method< 35>; break;
+      case  36: *address = &TemplateTestFacility_StubClass::method< 36>; break;
+      case  37: *address = &TemplateTestFacility_StubClass::method< 37>; break;
+      case  38: *address = &TemplateTestFacility_StubClass::method< 38>; break;
+      case  39: *address = &TemplateTestFacility_StubClass::method< 39>; break;
+      case  40: *address = &TemplateTestFacility_StubClass::method< 40>; break;
+      case  41: *address = &TemplateTestFacility_StubClass::method< 41>; break;
+      case  42: *address = &TemplateTestFacility_StubClass::method< 42>; break;
+      case  43: *address = &TemplateTestFacility_StubClass::method< 43>; break;
+      case  44: *address = &TemplateTestFacility_StubClass::method< 44>; break;
+      case  45: *address = &TemplateTestFacility_StubClass::method< 45>; break;
+      case  46: *address = &TemplateTestFacility_StubClass::method< 46>; break;
+      case  47: *address = &TemplateTestFacility_StubClass::method< 47>; break;
+      case  48: *address = &TemplateTestFacility_StubClass::method< 48>; break;
+      case  49: *address = &TemplateTestFacility_StubClass::method< 49>; break;
+      case  50: *address = &TemplateTestFacility_StubClass::method< 50>; break;
+      case  51: *address = &TemplateTestFacility_StubClass::method< 51>; break;
+      case  52: *address = &TemplateTestFacility_StubClass::method< 52>; break;
+      case  53: *address = &TemplateTestFacility_StubClass::method< 53>; break;
+      case  54: *address = &TemplateTestFacility_StubClass::method< 54>; break;
+      case  55: *address = &TemplateTestFacility_StubClass::method< 55>; break;
+      case  56: *address = &TemplateTestFacility_StubClass::method< 56>; break;
+      case  57: *address = &TemplateTestFacility_StubClass::method< 57>; break;
+      case  58: *address = &TemplateTestFacility_StubClass::method< 58>; break;
+      case  59: *address = &TemplateTestFacility_StubClass::method< 59>; break;
+      case  60: *address = &TemplateTestFacility_StubClass::method< 60>; break;
+      case  61: *address = &TemplateTestFacility_StubClass::method< 61>; break;
+      case  62: *address = &TemplateTestFacility_StubClass::method< 62>; break;
+      case  63: *address = &TemplateTestFacility_StubClass::method< 63>; break;
+      case  64: *address = &TemplateTestFacility_StubClass::method< 64>; break;
+      case  65: *address = &TemplateTestFacility_StubClass::method< 65>; break;
+      case  66: *address = &TemplateTestFacility_StubClass::method< 66>; break;
+      case  67: *address = &TemplateTestFacility_StubClass::method< 67>; break;
+      case  68: *address = &TemplateTestFacility_StubClass::method< 68>; break;
+      case  69: *address = &TemplateTestFacility_StubClass::method< 69>; break;
+      case  70: *address = &TemplateTestFacility_StubClass::method< 70>; break;
+      case  71: *address = &TemplateTestFacility_StubClass::method< 71>; break;
+      case  72: *address = &TemplateTestFacility_StubClass::method< 72>; break;
+      case  73: *address = &TemplateTestFacility_StubClass::method< 73>; break;
+      case  74: *address = &TemplateTestFacility_StubClass::method< 74>; break;
+      case  75: *address = &TemplateTestFacility_StubClass::method< 75>; break;
+      case  76: *address = &TemplateTestFacility_StubClass::method< 76>; break;
+      case  77: *address = &TemplateTestFacility_StubClass::method< 77>; break;
+      case  78: *address = &TemplateTestFacility_StubClass::method< 78>; break;
+      case  79: *address = &TemplateTestFacility_StubClass::method< 79>; break;
+      case  80: *address = &TemplateTestFacility_StubClass::method< 80>; break;
+      case  81: *address = &TemplateTestFacility_StubClass::method< 81>; break;
+      case  82: *address = &TemplateTestFacility_StubClass::method< 82>; break;
+      case  83: *address = &TemplateTestFacility_StubClass::method< 83>; break;
+      case  84: *address = &TemplateTestFacility_StubClass::method< 84>; break;
+      case  85: *address = &TemplateTestFacility_StubClass::method< 85>; break;
+      case  86: *address = &TemplateTestFacility_StubClass::method< 86>; break;
+      case  87: *address = &TemplateTestFacility_StubClass::method< 87>; break;
+      case  88: *address = &TemplateTestFacility_StubClass::method< 88>; break;
+      case  89: *address = &TemplateTestFacility_StubClass::method< 89>; break;
+      case  90: *address = &TemplateTestFacility_StubClass::method< 90>; break;
+      case  91: *address = &TemplateTestFacility_StubClass::method< 91>; break;
+      case  92: *address = &TemplateTestFacility_StubClass::method< 92>; break;
+      case  93: *address = &TemplateTestFacility_StubClass::method< 93>; break;
+      case  94: *address = &TemplateTestFacility_StubClass::method< 94>; break;
+      case  95: *address = &TemplateTestFacility_StubClass::method< 95>; break;
+      case  96: *address = &TemplateTestFacility_StubClass::method< 96>; break;
+      case  97: *address = &TemplateTestFacility_StubClass::method< 97>; break;
+      case  98: *address = &TemplateTestFacility_StubClass::method< 98>; break;
+      case  99: *address = &TemplateTestFacility_StubClass::method< 99>; break;
+      case 100: *address = &TemplateTestFacility_StubClass::method<100>; break;
+      case 101: *address = &TemplateTestFacility_StubClass::method<101>; break;
+      case 102: *address = &TemplateTestFacility_StubClass::method<102>; break;
+      case 103: *address = &TemplateTestFacility_StubClass::method<103>; break;
+      case 104: *address = &TemplateTestFacility_StubClass::method<104>; break;
+      case 105: *address = &TemplateTestFacility_StubClass::method<105>; break;
+      case 106: *address = &TemplateTestFacility_StubClass::method<106>; break;
+      case 107: *address = &TemplateTestFacility_StubClass::method<107>; break;
+      case 108: *address = &TemplateTestFacility_StubClass::method<108>; break;
+      case 109: *address = &TemplateTestFacility_StubClass::method<109>; break;
+      case 110: *address = &TemplateTestFacility_StubClass::method<110>; break;
+      case 111: *address = &TemplateTestFacility_StubClass::method<111>; break;
+      case 112: *address = &TemplateTestFacility_StubClass::method<112>; break;
+      case 113: *address = &TemplateTestFacility_StubClass::method<113>; break;
+      case 114: *address = &TemplateTestFacility_StubClass::method<114>; break;
+      case 115: *address = &TemplateTestFacility_StubClass::method<115>; break;
+      case 116: *address = &TemplateTestFacility_StubClass::method<116>; break;
+      case 117: *address = &TemplateTestFacility_StubClass::method<117>; break;
+      case 118: *address = &TemplateTestFacility_StubClass::method<118>; break;
+      case 119: *address = &TemplateTestFacility_StubClass::method<119>; break;
+      case 120: *address = &TemplateTestFacility_StubClass::method<120>; break;
+      case 121: *address = &TemplateTestFacility_StubClass::method<121>; break;
+      case 122: *address = &TemplateTestFacility_StubClass::method<122>; break;
+      case 123: *address = &TemplateTestFacility_StubClass::method<123>; break;
+      case 124: *address = &TemplateTestFacility_StubClass::method<124>; break;
+      case 125: *address = &TemplateTestFacility_StubClass::method<125>; break;
+      case 126: *address = &TemplateTestFacility_StubClass::method<126>; break;
+      case 127: *address = &TemplateTestFacility_StubClass::method<127>; break;
+    }
+}
+#endif
 
 template <class TYPE>
 inline
