8c4a6cd65484d5e5d2132cb2bf944a6f0d7af8c3
Mike Giroux <mgiroux@bloomberg.net>
2013-02-01 14:13:41 -0500

    rename the 2 dummy.cpp files to avoid having 2 objects with same name in archive

--- groups/bsl/bsl+bslhdrs/bslhdrs_dummy.cpp
+++ groups/bsl/bsl+bslhdrs/bslhdrs_dummy.cpp
@@ -1,24 +1,22 @@
-// dummy.cpp                                                          -*-C++-*-
+// bslhdrs_dummy.cpp                                                  -*-C++-*-
 
-#ifndef INCLUDED_BSLS_IDENT
 #include <bsls_ident.h>
-#endif
 BSLS_IDENT("$Id: $")
 
 //@PURPOSE: Workaround for build tool.
 //
-//@AUTHOR: Arthur Chiu (achiu21)
+//@AUTHOR: Mike Giroux (mgiroux)
 //
 //@DESCRIPTION: This is a workaround for the build tool since the tool requires
 // at least one implementation file in a package.
 
 // This symbol exists to avoid 'getarsym' errors when linking tests against the
-// 'bsl+stdhdrs' package library on SunOS with gcc.
-char stdhdrs_dummy_cpp_this_symbol_avoids_an_empty_package_library;
+// 'bsl+bslhdrs' package library on SunOS with gcc.
+char bslhdrs_dummy_cpp_this_symbol_avoids_an_empty_package_library;
 
 // ----------------------------------------------------------------------------
 // NOTICE:
-//      Copyright (C) Bloomberg L.P., 2012
+//      Copyright (C) Bloomberg L.P., 2009
 //      All Rights Reserved.
 //      Property of Bloomberg L.P. (BLP)
 //      This software is made available solely pursuant to the
