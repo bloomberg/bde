3c473057d3374145803ab55fe2d9fac0441ba4c8
Mike Giroux <mgiroux@bloomberg.net>
2013-02-11 15:20:03 -0500

    disable this test driver on Linux/clang to avoid OOM machine crashes

--- groups/bte/bteso/bteso_ipv4address.t.cpp
+++ groups/bte/bteso/bteso_ipv4address.t.cpp
@@ -134,6 +134,11 @@ typedef bdex_TestOutStream Out;
 
 int main(int argc, char *argv[])
 {
+#if defined(BSLS_PLATFORM_OS_LINUX) && defined(BSLS_PLATFORM_CMP_CLANG)
+    // drqs 38679960 - clang can use up all the memory on Linux compiling
+    // this test driver, crashing the machine.
+    return -1;
+#else
     int test = argc > 1 ? atoi(argv[1]) : 0;
     int verbose = argc > 2;
     int veryVerbose = argc > 3;
@@ -1466,6 +1471,7 @@ int main(int argc, char *argv[])
     }
 
     return testStatus;
+#endif // defined(BSLS_PLATFORM_OS_LINUX) && defined(BSLS_PLATFORM_CMP_CLANG)
 }
 
 // ---------------------------------------------------------------------------
