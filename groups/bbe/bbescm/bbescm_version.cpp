d57de7a6dcd45fc8e00dc263867903a6d3a1451e
Alisdair Meredith <ameredith1@bloomberg.net>
2013-02-26 13:58:17 -0500

    Fixing whitespace issues

--- groups/bbe/bbescm/bbescm_version.cpp
+++ groups/bbe/bbescm/bbescm_version.cpp
@@ -24,7 +24,7 @@ namespace BloombergLP {
                                              "." STRINGIFY(BBE_VERSION_PATCH) \
                                              BDE_VERSION_RELEASETYPE
 
-const char *bbescm_Version::d_ident = "$Id: " BBESCM_VERSION_STRING" $";
+const char *bbescm_Version::d_ident = "$Id: " BBESCM_VERSION_STRING " $";
 const char *bbescm_Version::d_what  = "@(#)"  BBESCM_VERSION_STRING;
 
 const char *bbescm_Version::d_version           = BBESCM_VERSION_STRING;
