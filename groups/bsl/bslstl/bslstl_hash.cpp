cbd3c8c37c3bf77f5b843eae8bcf879c0aede1b6
Alisdair Meredith <ameredith1@bloomberg.net>
2013-02-26 19:28:13 -0500

    Remove support for hash<const char *> as string
    This patch removes the defintion of the explicit class template specialization
    of 'hash' that would treat 'const char *' arguments as string values, rather
    than as pointer values.  Attempting to use this template instantiation will
    be a compile-time error, prompting users to examine any remaining code using
    the old behavior and make appropriate changes.  If, however, they are using
    the component correctly and expecting to hash the pointer value, there is no
    alternative component to replace their usage with - this is the intended
    long term solution.  In that case, the user can define a build macro to
    re-enable the dispatch to the general 'TYPE *' partial specialization.

--- groups/bsl/bslstl/bslstl_hash.cpp
+++ groups/bsl/bslstl/bslstl_hash.cpp
@@ -4,37 +4,10 @@
 #include <bsls_ident.h>
 BSLS_IDENT("$Id$ $CSID$")
 
-#ifndef BDE_OMIT_INTERNAL_DEPRECATED // DEPRECATED
-
-#include <stdio.h>  // for 'printf'
-
-#endif  // BDE_OMIT_INTERNAL_DEPRECATED
-
-namespace bsl
-{
-
-#ifndef BDE_OMIT_INTERNAL_DEPRECATED  // DEPRECATED
-std::size_t hash<const char *>::operator()(const char *x) const
-{
-    static bool firstCall = true;
-    if (firstCall) {
-        firstCall = false;
-
-        printf("ERROR: bsl::hash called for 'const char *',"
-               " see {TEAM BDEI:STANDARD HASH<GO}\n");
-    }
-
-    unsigned long result = 0;
-
-    for (; *x; ++x) {
-        result = 5 * result + *x;
-    }
-
-    return std::size_t(result);
-}
-#endif  // BDE_OMIT_INTERNAL_DEPRECATED
+namespace bsl {
 
 }  // close namespace bsl
+
 // ---------------------------------------------------------------------------
 // NOTICE:
 //      Copyright (C) Bloomberg L.P., 2012
