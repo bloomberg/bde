f8eb0de611962af77a95f8b3eeb0dd61c2aea173
Alexander Beels <abeels@bloomberg.net>
2013-04-21 19:52:05 -0400

    [APPROVAL] add-internal-build-tool-support-revised to master

--- groups/bsl/bsl+bslhdrs/bslhdrs_dummy.cpp
+++ groups/bsl/bsl+bslhdrs/bslhdrs_dummy.cpp
@@ -1,4 +1,16 @@
-int main() { return -1; }
+// bslhdrs_dummy.cpp                                                  -*-C++-*-
+
+#include <bsls_ident.h>
+BSLS_IDENT("$Id: $")
+
+//@PURPOSE: Workaround for build tool.
+//
+//@DESCRIPTION: This is a workaround for the build tool since the tool requires
+// at least one implementation file in a package.
+
+// This symbol exists to avoid 'getarsym' errors when linking tests against the
+// 'bsl+bslhdrs' package library on SunOS with gcc.
+char bslhdrs_dummy_cpp_this_symbol_avoids_an_empty_package_library;
 
 // ----------------------------------------------------------------------------
 // Copyright (C) 2013 Bloomberg L.P.
