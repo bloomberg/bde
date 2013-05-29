ce8a9e5698821b696fd4e21cfc8da5ddba145f85
Alisdair Meredith <ameredith1@bloomberg.net>
2013-05-29 16:17:22 -0400

    Address review comments

--- groups/bce/bcema/bcema_sharedptr.h
+++ groups/bce/bcema/bcema_sharedptr.h
@@ -1661,7 +1661,7 @@ class bcema_SharedPtr {
         // 'if' or 'while' statement), but does *not* allow shared pointers to
         // be compared (e.g., via '<' or '>').
 
-    typename ::bsl::add_lvalue_reference<TYPE>::type
+    typename bsl::add_lvalue_reference<TYPE>::type
     operator[](bsl::ptrdiff_t index) const;
         // Return a reference to the modifiable object at the specified 'index'
         // offset in object referred to by this shared pointer.  The behavior
@@ -1671,7 +1671,7 @@ class bcema_SharedPtr {
         // shared pointer refers to an array of 'TYPE' objects.  Note that this
         // is logically equivalent to '*(ptr() + index)'.
 
-    typename ::bsl::add_lvalue_reference<TYPE>::type
+    typename bsl::add_lvalue_reference<TYPE>::type
     operator*() const;
         // Return a reference to the modifiable object referred to by this
         // shared pointer.  The behavior is undefined unless this shared
@@ -2614,7 +2614,7 @@ bcema_SharedPtr<TYPE>::operator bcema_SharedPtr_UnspecifiedBool() const
 
 template <class TYPE>
 inline
-typename ::bsl::add_lvalue_reference<TYPE>::type
+typename bsl::add_lvalue_reference<TYPE>::type
 bcema_SharedPtr<TYPE>::operator[](bsl::ptrdiff_t index) const
 {
     return *(d_ptr_p + index);
@@ -2622,7 +2622,7 @@ bcema_SharedPtr<TYPE>::operator[](bsl::ptrdiff_t index) const
 
 template <class TYPE>
 inline
-typename ::bsl::add_lvalue_reference<TYPE>::type
+typename bsl::add_lvalue_reference<TYPE>::type
 bcema_SharedPtr<TYPE>::operator*() const
 {
     return *d_ptr_p;
