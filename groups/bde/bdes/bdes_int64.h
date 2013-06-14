6e5eef4f75d83651711cf0cc1b87db50ef5ff609
Alisdair Meredith <ameredith1@bloomberg.net>
2013-06-14 11:34:55 -0400

    Remove now-dead deprecated code
    This initial patch removes macros and other identifiers
    from deprected compoents, where they no longer show as
    beging used in OpenGrok.  First pass looks into only
    the 'bdes' package.

--- groups/bde/bdes/bdes_int64.h
+++ groups/bde/bdes/bdes_int64.h
@@ -1,4 +1,4 @@
-// bdes_int64.h                 -*-C++-*-
+// bdes_int64.h                                                       -*-C++-*-
 #ifndef INCLUDED_BDES_INT64
 #define INCLUDED_BDES_INT64
 
@@ -7,31 +7,16 @@
 #endif
 BDES_IDENT("$Id: $")
 
-//@PURPOSE: Provide namespace for platform-dependent 64-bit integer types.
+//@PURPOSE: Provide a header file to satisfy old redundant #includes.
 //
-//@DEPRECATED: Use 'bsls_types' instead.
+//@DEPRECATED: This component should not be used in new code, and will be
+// deleted in the near future.  Please see 'bdef_function', 'bdef_bind', etc.
+// for alternatives that should be used for all future development.
 //
-//@CLASSES:
-//   bdes_Int64: namespace for platform-dependent 64-bit integer types
-//
-//@AUTHOR: Tom Marshall (tmarshal)
-//
-//@DESCRIPTION: This component provides platform-independent type names for
-// both signed and unsigned 64 bit integers.  For healthy platforms, the
-// component simply provides a BDE-standard 'typedef' for the non-standard
-// native type name.  For defective platforms or compilers, a bde-relevant
-// subset of the needed functionality can be implemented.
-//
-// The types (and platform-specific 'typedef's) exposed here are intended for
-// use by 'bdes_platformutil' only (and not directly by any other client).
-// This component exists to factor out various pathologies, present and future,
-// with 64-bit integers.
-//
-// !WARNING!  If a given platform has a defective native 64-bit integer type,
-// then there is no guarantee that the types supplied here will be suitable
-// *except* as they are explicitly used by other BDE components.  If a user
-// encounters problems with the types, please contact the author or a BDE
-// library maintenance engineer.
+//@DESCRIPTION: This component has been deleted, but there remains legacy code
+// still including this header, without relying on its contents.  To avoid
+// immediately breaking that code, we retain this effectively empty header for
+// another BDE release cycle, while notifying the owners of the legacy code.
 //
 ///USAGE
 ///-----
@@ -41,11 +26,10 @@ BDES_IDENT("$Id: $")
 #include <bslscm_version.h>  // required by 'bdes'
 #endif
 
-// TBD The following inclusion and the 'typedef' below have been commented out
-// in anticipation of this component's retirement in BDE 2.17.
-// #ifndef INCLUDED_BSLS_INT64
-// #include <bsls_int64.h>
-// #endif
+#ifndef BDE_OMIT_INTERNAL_DEPRECATED
+#ifndef BDE_DONT_ALLOW_TRANSITIVE_INCLUDES
+
+// Transitive inclusions may be depended on by some Robo code.
 
 #ifndef INCLUDED_BSLS_PLATFORM
 #include <bsls_platform.h>
@@ -55,18 +39,14 @@ BDES_IDENT("$Id: $")
 #include <bsls_types.h>
 #endif
 
-namespace BloombergLP {
-
-// TBD see above
-// typedef bsls_Int64 bdes_Int64;
-
-}  // close namespace BloombergLP
+#endif // BDE_DONT_ALLOW_TRANSITIVE_INCLUDES
+#endif // BDE_OMIT_INTERNAL_DEPRECATED
 
 #endif
 
 // ---------------------------------------------------------------------------
 // NOTICE:
-//      Copyright (C) Bloomberg L.P., 2002
+//      Copyright (C) Bloomberg L.P., 2013
 //      All Rights Reserved.
 //      Property of Bloomberg L.P. (BLP)
 //      This software is made available solely pursuant to the
