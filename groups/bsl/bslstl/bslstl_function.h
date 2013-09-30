beb915ea5453049cbf64ebd05951cd60a6be6d8e
Pablo Halpern <phalpern@halpernwightsoftware.com>
2013-09-29 21:28:43 -0400

    Skeleton of bsl::function; no implementation

--- groups/bsl/bslstl/bslstl_function.h
+++ groups/bsl/bslstl/bslstl_function.h
@@ -49,6 +49,10 @@ BSL_OVERRIDES_STD mode"
 #include <bsls_alignmentutil.h>
 #endif
 
+#ifndef INCLUDED_BSLS_EXCEPTIONUTIL
+#include <bsls_exceptionutil.h>
+#endif
+
 #ifndef INCLUDED_BSLMA_ALLOCATOR
 #include <bslma_allocator.h>
 #endif
@@ -58,12 +62,6 @@ namespace bsl {
 
 template <class FUNC>
 class function;  // Primary template declared but not defined.
-
-}
-
-namespace BloombergLP {
-
-namespace bslstl {
 
                         // ================================
                         // class template Function_ArgTypes
@@ -140,7 +138,7 @@ class Function_Rep {
 
     union InplaceBuffer {
         // This 'struct' defines the storage area for a functor
-        // representation.  Its design uses the "small-object optimization" in
+        // representation.  The design uses the "small-object optimization" in
         // an attempt to avoid allocations for objects that are no larger than
         // 'InplaceBuffer'.  When using the in-place representation, the
         // invocable, whether a function pointer or function object (if it
@@ -153,7 +151,7 @@ class Function_Rep {
         // Note that union members other than 'd_object_p' are just fillers to
         // make sure that a function or member function pointer can fit
         // without allocation and that alignment is respected.  The 'd_minbuf'
-        // member ensures that 'InplaceBuffer' is at large enough so that
+        // member ensures that 'InplaceBuffer' is at least large enough so that
         // modestly-complex functors (e.g., functors that own embedded
         // arguments, such as 'bdef_Bind' objects) to be constructed in-place
         // without triggering further allocation.  The benefit of avoiding
@@ -197,270 +195,89 @@ class Function_Rep {
 
 };
 
-}  // close package namespace
-
-}  // close enterprise namespace
-
-namespace bsl {
 
                     // =======================
                     // class template function
                     // =======================
 
-template <class FUNC>
-class function;  // Primary template is not defined.
-
 #if !BSLS_COMPILERFEATURES_SIMULATE_CPP11_FEATURES
 
 template <class RET, class... ARGS>
-class function<RET(ARGS...)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS...)>
+class function<RET(ARGS...)> : public Function_ArgTypes<RET(ARGS...)>
 {
+    // An instantiation of this class template generalizes the notion of a
+    // pointer to a function taking the specified 'ARGS' types and returning
+    // the specified 'RET' type, i.e., a function pointer of type
+    // 'RET(*)(ARGS)'.  An object of this class wraps a run-time invokable
+    // object specified at construction, such as a function pointer, member
+    // function pointer, or functor.  Note that 'function' is defined only for
+    // template parameters that specify a function prototype; the primary
+    // template (taking an arbitrary template parameter) is not defined.
+
 public:
+    // PUBLIC TYPES
     typedef RET result_type;
+
+    // CREATORS
+    function() BSLS_NOTHROW_SPEC;
+    function(nullptr_t) BSLS_NOTHROW_SPEC;
+    function(const function&);
+    template<class F> function(F);
+    template<class A> function(allocator_arg_t, const A&) BSLS_NOTHROW_SPEC;
+    template<class A> function(allocator_arg_t, const A&,
+                               nullptr_t) BSLS_NOTHROW_SPEC;
+    template<class A> function(allocator_arg_t, const A&,
+                               const function&);
+    template<class F, class A> function(allocator_arg_t, const A&, F);
+
+#ifdef BSLS_COMPILERFEATURES_SUPPORT_RVALUE_REFERENCES
+    function(function&&);
+    template<class A> function(allocator_arg_t, const A&, function&&);
+#endif
+
+    ~function();
+
+    // MANIPULATORS
+    function& operator=(const function&);
+#ifdef BSLS_COMPILERFEATURES_SUPPORT_RVALUE_REFERENCES
+    function& operator=(function&&);
+#endif
+    function& operator=(nullptr_t);
+    template<class F>
+    function& operator=(F&&);
+
+    // TBD: Need to implement reference_wrapper.
+    // template<class F>
+    // function& operator=(reference_wrapper<F>) BSLS_NOTHROW_SPEC;
+
+    void swap(function&) BSLS_NOTHROW_SPEC;
+    template<class F, class A> void assign(F&&, const A&);
+
+    explicit operator bool() const BSLS_NOTHROW_SPEC;
+
+    R operator()(ArgTypes...) const;
+
+    const std::type_info& target_type() const BSLS_NOTHROW_SPEC;
+    template<class T> T* target() BSLS_NOTHROW_SPEC;
+    template<class T> const T* target() const BSLS_NOTHROW_SPEC;
 };
 
-#elif BSLS_COMPILERFEATURES_SIMULATE_VARIADIC_TEMPLATES
-// {{{ BEGIN GENERATED CODE
-// The following section is automatically generated.  **DO NOT EDIT**
-// Generator command line: sim_cpp11_features.pl bslstl_function.h bslstl_function.t.cpp
+// FREE FUNCTIONS
+template <class R, class... ARGS>
+bool operator==(const function<R(ARGS...)>&, nullptr_t) BSL_NOTHROW_SPEC;
 
-template <class RET>
-class function<RET()> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET()>
-{
-public:
-    typedef RET result_type;
-};
+template <class R, class... ARGS>
+bool operator==(nullptr_t, const function<R(ARGS...)>&) BSL_NOTHROW_SPEC;
 
-template <class RET, class ARGS_01>
-class function<RET(ARGS_01)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01)>
-{
-public:
-    typedef RET result_type;
-};
+template <class R, class... ARGS>
+bool operator!=(const function<R(ARGS...)>&, nullptr_t) BSL_NOTHROW_SPEC;
 
-template <class RET, class ARGS_01,
-                     class ARGS_02>
-class function<RET(ARGS_01,
-                   ARGS_02)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02)>
-{
-public:
-    typedef RET result_type;
-};
+template <class R, class... ARGS>
+bool operator!=(nullptr_t, const function<R(ARGS...)>&) BSL_NOTHROW_SPEC;
 
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03)>
-{
-public:
-    typedef RET result_type;
-};
+template <class R, class... ARGS>
+void swap(function<R(ARGS...)>&, function<R(ARGS...)>&);
 
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03,
-                     class ARGS_04>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03,
-                   ARGS_04)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03,
-                                                          ARGS_04)>
-{
-public:
-    typedef RET result_type;
-};
-
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03,
-                     class ARGS_04,
-                     class ARGS_05>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03,
-                   ARGS_04,
-                   ARGS_05)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03,
-                                                          ARGS_04,
-                                                          ARGS_05)>
-{
-public:
-    typedef RET result_type;
-};
-
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03,
-                     class ARGS_04,
-                     class ARGS_05,
-                     class ARGS_06>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03,
-                   ARGS_04,
-                   ARGS_05,
-                   ARGS_06)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03,
-                                                          ARGS_04,
-                                                          ARGS_05,
-                                                          ARGS_06)>
-{
-public:
-    typedef RET result_type;
-};
-
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03,
-                     class ARGS_04,
-                     class ARGS_05,
-                     class ARGS_06,
-                     class ARGS_07>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03,
-                   ARGS_04,
-                   ARGS_05,
-                   ARGS_06,
-                   ARGS_07)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03,
-                                                          ARGS_04,
-                                                          ARGS_05,
-                                                          ARGS_06,
-                                                          ARGS_07)>
-{
-public:
-    typedef RET result_type;
-};
-
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03,
-                     class ARGS_04,
-                     class ARGS_05,
-                     class ARGS_06,
-                     class ARGS_07,
-                     class ARGS_08>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03,
-                   ARGS_04,
-                   ARGS_05,
-                   ARGS_06,
-                   ARGS_07,
-                   ARGS_08)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03,
-                                                          ARGS_04,
-                                                          ARGS_05,
-                                                          ARGS_06,
-                                                          ARGS_07,
-                                                          ARGS_08)>
-{
-public:
-    typedef RET result_type;
-};
-
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03,
-                     class ARGS_04,
-                     class ARGS_05,
-                     class ARGS_06,
-                     class ARGS_07,
-                     class ARGS_08,
-                     class ARGS_09>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03,
-                   ARGS_04,
-                   ARGS_05,
-                   ARGS_06,
-                   ARGS_07,
-                   ARGS_08,
-                   ARGS_09)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03,
-                                                          ARGS_04,
-                                                          ARGS_05,
-                                                          ARGS_06,
-                                                          ARGS_07,
-                                                          ARGS_08,
-                                                          ARGS_09)>
-{
-public:
-    typedef RET result_type;
-};
-
-template <class RET, class ARGS_01,
-                     class ARGS_02,
-                     class ARGS_03,
-                     class ARGS_04,
-                     class ARGS_05,
-                     class ARGS_06,
-                     class ARGS_07,
-                     class ARGS_08,
-                     class ARGS_09,
-                     class ARGS_10>
-class function<RET(ARGS_01,
-                   ARGS_02,
-                   ARGS_03,
-                   ARGS_04,
-                   ARGS_05,
-                   ARGS_06,
-                   ARGS_07,
-                   ARGS_08,
-                   ARGS_09,
-                   ARGS_10)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS_01,
-                                                          ARGS_02,
-                                                          ARGS_03,
-                                                          ARGS_04,
-                                                          ARGS_05,
-                                                          ARGS_06,
-                                                          ARGS_07,
-                                                          ARGS_08,
-                                                          ARGS_09,
-                                                          ARGS_10)>
-{
-public:
-    typedef RET result_type;
-};
-
-#else
-// The generated code below is a workaround for the absence of perfect
-// forwarding in some compilers.
-
-template <class RET, class... ARGS>
-class function<RET(ARGS...)> :
-        public BloombergLP::bslstl::Function_ArgTypes<RET(ARGS...)>
-{
-public:
-    typedef RET result_type;
-};
-
-// }}} END GENERATED CODE
 #endif
 
 }  // close namespace bsl
@@ -469,23 +286,163 @@ public:
 //                      TEMPLATE DEFINITIONS
 // ===========================================================================
 
-namespace BloombergLP {
-
-namespace bslstl {
-
-// CLASS METHODS
+#if !BSLS_COMPILERFEATURES_SIMULATE_CPP11_FEATURES
 
 // CREATORS
+template <class R, class... ARGS>
+bsl::function<R(ARGS...)>::function() BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+bsl::function<R(ARGS...)>::function(nullptr_t) BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+bsl::function<R(ARGS...)>::function(const function&)
+{
+}
+
+template <class R, class... ARGS>
+template<class F>
+bsl::function<R(ARGS...)>::function(F)
+{
+}
+
+template <class R, class... ARGS>
+template<class A>
+bsl::function<R(ARGS...)>::function(allocator_arg_t, const A&)
+    BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+template<class A>
+bsl::function<R(ARGS...)>::function(allocator_arg_t, const A&,
+                                    nullptr_t) BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+template<class A>
+bsl::function<R(ARGS...)>::function(allocator_arg_t, const A&,
+                                    const function&)
+{
+}
+
+template <class R, class... ARGS>
+template<class F, class A>
+bsl::function<R(ARGS...)>::function(allocator_arg_t, const A&, F)
+{
+}
+
+#ifdef BSLS_COMPILERFEATURES_SUPPORT_RVALUE_REFERENCES
+
+template <class R, class... ARGS>
+bsl::function<R(ARGS...)>::function(function&&)
+{
+}
+
+template <class R, class... ARGS>
+template<class A>
+bsl::function<R(ARGS...)>::function(allocator_arg_t, const A&, function&&)
+{
+}
+
+#endif //  BSLS_COMPILERFEATURES_SUPPORT_RVALUE_REFERENCES
+
+template <class R, class... ARGS>
+bsl::function<R(ARGS...)>::~function()
+{
+}
 
 // MANIPULATORS
+template <class R, class... ARGS>
+function& bsl::function<R(ARGS...)>::operator=(const function&)
+{
+}
 
-// ACCESSORS
+#ifdef BSLS_COMPILERFEATURES_SUPPORT_RVALUE_REFERENCES
 
-// FREE OPERATORS
+template <class R, class... ARGS>
+function& bsl::function<R(ARGS...)>::operator=(function&&)
+{
+}
 
-}  // close package namespace
+#endif
 
-}  // close enterprise namespace
+template <class R, class... ARGS>
+function& bsl::function<R(ARGS...)>::operator=(nullptr_t)
+{
+}
+
+template <class R, class... ARGS>
+template<class F>
+function& bsl::function<R(ARGS...)>::operator=(F&&)
+{
+}
+
+// TBD: Need to implement reference_wrapper.
+// template <class R, class... ARGS>
+// template<class F>
+// function& bsl::function<R(ARGS...)>::operator=(reference_wrapper<F>)
+//     BSLS_NOTHROW_SPEC
+
+template <class R, class... ARGS>
+void bsl::function<R(ARGS...)>::swap(function&) BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+template<class F, class A>
+void bsl::function<R(ARGS...)>::assign(F&&, const A&)
+{
+}
+
+template <class R, class... ARGS>
+bsl::function<R(ARGS...)>::operator bool() const BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+R bsl::function<R(ARGS...)>::operator()(ArgTypes...) const
+{
+}
+
+template <class R, class... ARGS>
+const std::type_info&
+bsl::function<R(ARGS...)>::target_type() const BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+template<class T>
+T* bsl::function<R(ARGS...)>::target() BSLS_NOTHROW_SPEC
+{
+}
+
+template <class R, class... ARGS>
+template<class T>
+const T* bsl::function<R(ARGS...)>::target() const BSLS_NOTHROW_SPEC
+{
+}
+
+// FREE FUNCTIONS
+template <class R, class... ARGS>
+bool operator==(const function<R(ARGS...)>&, nullptr_t) BSL_NOTHROW_SPEC;
+
+template <class R, class... ARGS>
+bool operator==(nullptr_t, const function<R(ARGS...)>&) BSL_NOTHROW_SPEC;
+
+template <class R, class... ARGS>
+bool operator!=(const function<R(ARGS...)>&, nullptr_t) BSL_NOTHROW_SPEC;
+
+template <class R, class... ARGS>
+bool operator!=(nullptr_t, const function<R(ARGS...)>&) BSL_NOTHROW_SPEC;
+
+template <class R, class... ARGS>
+void swap(function<R(ARGS...)>&, function<R(ARGS...)>&);
 
 #endif // ! defined(INCLUDED_BSLSTL_FUNCTION)
 
