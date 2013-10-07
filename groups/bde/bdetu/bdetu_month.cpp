62f4c8e2eae2d36e21c082f52f7eab1dd7a0e337
Andrew Lin <alin197@bloomberg.net>
2013-10-07 16:45:05 -0400

    Using bdet_MonthOfYear in place of the depricated bdet_Month.

--- groups/bde/bdetu/bdetu_month.cpp
+++ groups/bde/bdetu/bdetu_month.cpp
@@ -1,5 +1,6 @@
 // bdetu_month.cpp   -*-C++-*-
 #include <bdetu_month.h>
+#include <bdet_monthofyear.h>
 
 #include <bdes_ident.h>
 BDES_IDENT_RCSID(bdetu_month_cpp,"$Id$ $CSID$")
@@ -12,7 +13,7 @@ namespace BloombergLP {
 
 static int min(int a, int b) { return a < b ? a : b; }
 
-int bdetu_Month::numMonthsInRange(bdet_Month::Month month,
+int bdetu_Month::numMonthsInRange(bdet_MonthOfYear::Month month,
                                   const bdet_Date&  date1,
                                   const bdet_Date&  date2)
 {
@@ -28,7 +29,7 @@ int bdetu_Month::numMonthsInRange(bdet_Month::Month month,
     date2.getYearMonthDay(&y2, &m2, &day2NotUsed);
 
     while (m1 != m2) {
-        count += bdet_Month::Month(m2) == month;
+        count += bdet_MonthOfYear::Month(m2) == month;
         --m2;
 
         if (!m2) {
@@ -39,17 +40,17 @@ int bdetu_Month::numMonthsInRange(bdet_Month::Month month,
 
     BSLS_ASSERT(m2 == m1);
 
-    return count + y2 - y1 + (bdet_Month::Month(m2) == month);
+    return count + y2 - y1 + (bdet_MonthOfYear::Month(m2) == month);
 }
 
-bdet_Date bdetu_Month::nextMonth(bdet_Month::Month month,
+bdet_Date bdetu_Month::nextMonth(bdet_MonthOfYear::Month month,
                                  const bdet_Date&  date)
 {
     int y, m, d;
 
     date.getYearMonthDay(&y, &m, &d);
 
-    if (bdet_Month::Month(m) >= month) {
+    if (bdet_MonthOfYear::Month(m) >= month) {
         ++y;
     }
 
@@ -58,14 +59,14 @@ bdet_Date bdetu_Month::nextMonth(bdet_Month::Month month,
     return bdet_Date(y, month, day);
 }
 
-bdet_Date bdetu_Month::previousMonth(bdet_Month::Month month,
+bdet_Date bdetu_Month::previousMonth(bdet_MonthOfYear::Month month,
                                      const bdet_Date&  date)
 {
     int y, m, d;
 
     date.getYearMonthDay(&y, &m, &d);
 
-    if (bdet_Month::Month(m) <= month) {
+    if (bdet_MonthOfYear::Month(m) <= month) {
         --y;
     }
 
@@ -74,7 +75,7 @@ bdet_Date bdetu_Month::previousMonth(bdet_Month::Month month,
     return bdet_Date(y, month, day);
 }
 
-bdet_Date bdetu_Month::adjustMonth(bdet_Month::Month month,
+bdet_Date bdetu_Month::adjustMonth(bdet_MonthOfYear::Month month,
                                    const bdet_Date&  date,
                                    int               count)
 {
@@ -85,10 +86,10 @@ bdet_Date bdetu_Month::adjustMonth(bdet_Month::Month month,
     date.getYearMonthDay(&y, &m, &d);
 
     if (count > 0) {
-        y += count - (bdet_Month::Month(m) < month);
+        y += count - (bdet_MonthOfYear::Month(m) < month);
     }
     else {                              // count < 0
-        y += count + (bdet_Month::Month(m) > month);
+        y += count + (bdet_MonthOfYear::Month(m) > month);
     }
 
     int day = min(d, bdeimp_DateUtil::lastDayOfMonth(y, month));
@@ -96,7 +97,7 @@ bdet_Date bdetu_Month::adjustMonth(bdet_Month::Month month,
     return bdet_Date(y, month, day);
 }
 
-bdet_Date bdetu_Month::ceilAdjustMonth(bdet_Month::Month month,
+bdet_Date bdetu_Month::ceilAdjustMonth(bdet_MonthOfYear::Month month,
                                        const bdet_Date&  date,
                                        int               count)
 {
@@ -113,7 +114,7 @@ bdet_Date bdetu_Month::ceilAdjustMonth(bdet_Month::Month month,
     return bdet_Date(y, month, day);
 }
 
-bdet_Date bdetu_Month::floorAdjustMonth(bdet_Month::Month month,
+bdet_Date bdetu_Month::floorAdjustMonth(bdet_MonthOfYear::Month month,
                                         const bdet_Date&  date,
                                         int               count)
 {
