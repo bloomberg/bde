f7196981141575c1e680656bc599f87f6c78aae7
Alisdair Meredith <ameredith1@bloomberg.net>
2014-02-01 18:37:14 -0500

    Windows build issues for BDE 2.20
    This patch addresses failures to build test drivers
    all the way up through to bae/bte.  It does not
    address the 'bas' issues that arise from 'bsl' now
    having a rarely-used header that now directly
    includes 'windows.h'.  These concerns will need to
    be negotiated with the 'bas' team.

--- groups/bde/bdem/bdem_choicearray.t.cpp
+++ groups/bde/bdem/bdem_choicearray.t.cpp
@@ -37,6 +37,12 @@
 #include <bsl_sstream.h>
 #include <bsl_string.h>
 
+#ifdef BSLS_PLATFORM_OS_WINDOWS
+// Undefine some awkwardly named Windows macros that interfere with this cpp
+// file, but only after the last #include.
+# undef ERROR
+#endif
+
 using namespace BloombergLP;
 using namespace bsl;  // automatically added by script
 
