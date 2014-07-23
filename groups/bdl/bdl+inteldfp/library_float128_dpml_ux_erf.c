5fa7abe34b5a072a4beb31301aa787534665142d
ADAM David Alan Martin <amartin172@bloomberg.net>
2014-07-23 14:02:57 -0400

    Safely reference IntelDFP sources in bdl+inteldfp
    The waf and bde_build.pl build systems require that all source
    and header files to be build must be in the root directory of the
    package.  Intel's DFP library files used to be manually "flattened"
    into the intel+dfp directory, using the 'flatten.sh' script.  To
    facilitate easier building, and prepare for DFP release, these
    flattened wrapper files have been added to the git repo.

---
+++ groups/bdl/bdl+inteldfp/library_float128_dpml_ux_erf.c
@@ -0,0 +1,8 @@
+#include <bsls_platform.h>
+
+#if BSLS_PLATFORM_OS_LINUX && BSLS_PLATFORM_CMP_GNU
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#include "LIBRARY/float128/dpml_ux_erf.c"
+#pragma GCC diagnostic pop
+#endif
