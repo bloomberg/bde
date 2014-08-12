f13b3384cecd20807de3619d721abaad8820bf1e
Mike Giroux <mgiroux@bloomberg.net>
2014-08-12 12:38:07 -0400

    [APPROVAL] INTERNAL fix_deprecated_macro_in_pair_drqs54946578 to master
    Base commit: 94812f24e0d449c8091156c382e35fa3c5e23ba4

--- groups/bsl/bslstl/bslstl_pair.h
+++ groups/bsl/bslstl/bslstl_pair.h
@@ -254,11 +254,6 @@ BSL_OVERRIDES_STD mode"
 #include <bslma_allocator.h>
 #endif
 
-#if !defined(BDE_OMIT_DEPRECATED_INTERNAL_ONLY)
-#ifndef INCLUDED_BSLMA_MANAGEDPTR_PAIRPROXY
-#include <bslma_managedptr_pairproxy.h>
-#endif
-#endif
 
 #ifndef INCLUDED_BSLMA_USESBSLMAALLOCATOR
 #include <bslma_usesbslmaallocator.h>
@@ -534,14 +529,6 @@ class pair : public Pair_Imp<T1, T2,
         // undefined unless 'T1' is constructible from 'U1' and 'T2' is
         // constructible from from 'U2'.
 
-#if !defined(BDE_OMIT_DEPRECATED_INTERNAL_ONLY)
-    template <typename U1, typename U2>
-    pair(const BloombergLP::bslma::ManagedPtr_PairProxy<U1, U2>&
-                                                              rhs); // IMPLICIT
-        // Create a pair that has the same value as the specified 'rhs' pair
-        // proxy.  The behavior is undefined unless 'T1' is constructible from
-        // 'U1' and 'T2' is constructible from from 'U2'.
-#endif
 
     ~pair();
         // Destroy this object.  Call destructors on 'first' and 'second'.
@@ -876,14 +863,6 @@ pair<T1, T2>::pair(const native_std::pair<U1, U2>&  rhs,
 {
 }
 
-#if !defined(BDE_OMIT_DEPRECATED_INTERNAL_ONLY)
-template <typename T1, typename T2>
-template <typename U1, typename U2>
-pair<T1, T2>::pair(const BloombergLP::bslma::ManagedPtr_PairProxy<U1, U2>& rhs)
-: Base(rhs.first, rhs.second)
-{
-}
-#endif
 
 template <typename T1, typename T2>
 inline
