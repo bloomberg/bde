9dff23d878879423ac9d8a1b20d1cbcadf70330a
Nathan Myers <nmyers12@bloomberg.net>
2014-12-08 14:04:56 -0500

    coded

---
+++ groups/bsl/bslstl/bslstl_referencewrapper.h
@@ -0,0 +1,169 @@
+// bslstl_referencewrapper.h                                           -*-C++-*-
+#ifndef INCLUDED_BSLSTL_REFERENCE_WRAPPER
+#define INCLUDED_BSLSTL_REFERENCE_WRAPPER
+
+#ifndef INCLUDED_BSLS_IDENT
+#include <bsls_ident.h>
+#endif
+BSLS_IDENT("$Id: $")
+
+//@PURPOSE: Wrap a reference in a copyable, assignable object
+//
+//@CLASSES:
+//  bsl::reference_wrapper<T>
+//
+//@DESCRIPTION:
+//  reference_wrapper simply wraps a reference into a copyable, assignable
+//  object to allow it to be stored in a place that cannot normally hold a
+//  reference, such as a standard container.  Because it is convertible to
+//  T&, it can be used with functions that take a T&.
+//
+//  Helper functions bsl::ref and bsl::cref may be used to generate
+//  reference_wrapper objects more concisely than with the constructor.
+
+// Prevent 'bslstl' headers from being included directly in 'BSL_OVERRIDES_STD'
+// mode.  Doing so is unsupported, and is likely to cause compilation errors.
+#if defined(BSL_OVERRIDES_STD) && !defined(BSL_STDHDRS_PROLOGUE_IN_EFFECT)
+#error "<bslstl_referencewrapper.h> header can't be included directly in \
+BSL_OVERRIDES_STD mode"
+#endif
+
+#ifndef INCLUDED_BSLSCM_VERSION
+#include <bslscm_version.h>
+#endif
+#ifndef INCLUDED_BSLS_UTIL
+#include <bsls_util.h>
+#endif
+
+namespace Bloomberg_LP {
+namespace bsl {
+
+template <typename T>
+class reference_wrapper <T>
+{
+  public:
+    // PUBLIC TYPES
+
+    typedef T type;
+
+    // CREATORS
+
+    reference_wrapper(T& t);
+    reference_wrapper(const reference_wrapper& t);
+        // Wrap a reference to specified argument 't'.
+
+    // ASSIGNMENT
+
+    reference_wrapper& operator=(const reference_wrapper& ref) const;
+        // Return a copy of the specified 'ref'.
+
+    // ACCESSORS
+
+    operator T&() const;
+        // implicitly convert '*this' to a reference to T.
+ 
+    T& get() const;
+        // Return the reference contructed or assigned to '*this'.
+
+
+  private:
+    T* d_p;
+
+#if 201103L <= __cplusplus
+    reference_wrapper(reference_wrapper&&) = delete;
+#endif
+};
+
+    // The following helper functions 'ref()' and 'cref()' each return a
+    // wrapped reference to the specified argument 't'.  They are intended as
+    // abbreviations, solely for convenience in writing and reading code that
+    // uses reference wrappers.
+
+template <typename T>
+reference_wrapper<T> ref(T& t);
+
+template <typename T>
+reference_wrapper<T> ref(reference_wrapper<T> t);
+
+template <typename T>
+reference_wrapper<const T> cref(const T& t);
+
+template <typename T>
+reference_wrapper<const T> cref(reference_wrapper<T> t);
+
+#if 201103L <= __cplusplus
+template <typename T>
+void ref(T&&) = delete;
+template <typename T>
+void cref(const T&&) = delete;
+#endif
+
+
+}  // namespace bsl
+
+//                      INLINE FUNCTION DEFINITIONS
+// ============================================================================
+
+namespace bsl {
+
+    // reference_wrapper<T> members
+
+template <typename T>
+inline reference_wrapper<T>::reference_wrapper(T& t)
+: d_p(BSLS_UTIL_ADDRESSOF(t)) {}
+
+template <typename T>
+inline reference_wrapper<T>::reference_wrapper(const reference_wrapper<T>& ref)
+: d_p(ref.d_p) {}
+
+template <typename T>
+inline reference_wrapper<T>& reference_wrapper<T>::operator=(
+                                          const reference_wrapper<T> ref&) const
+{ d_p = ref.d_p; }
+
+template <typename T>
+inline reference_wrapper<T>::operator T&() const
+{ return *d_p; }
+
+template <typename T>
+inline T& reference_wrapper<T>::get() const
+{ return *d_p; }
+
+    // ref(), cref()
+
+template <typename T>
+inline reference_wrapper<T> ref(T& t)
+{ return reference_wrapper<T>(t); }
+
+template <typename T>
+inline reference_wrapper<T> ref(reference_wrapper<T> t)
+{ return reference_wrapper<T>(t); }
+
+template <typename T>
+inline reference_wrapper<const T> cref(const T& t)
+{ return reference_wrapper<const T>(t); }
+
+template <typename T>
+inline reference_wrapper<const T> cref(reference_wrapper<T> t)
+{ return reference_wrapper<const T>(*t.get()); }
+
+}  // namespace bsl
+}  // namespace BloombergLP
+
+#endif
+
+// ----------------------------------------------------------------------------
+// Copyright 2013 Bloomberg Finance L.P.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------- END-OF-FILE ----------------------------------
---
+++ groups/bsl/bslstl/bslstl_referencewrapper.t.cpp
@@ -0,0 +1,180 @@
+// bslstl_referencewrapper.t.cpp                                       -*-C++-*-
+#include <bslstl_referencewrapper.h>
+
+#include <bsls_bsltestutil.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//=============================================================================
+//                              TEST PLAN
+//-----------------------------------------------------------------------------
+//                              Overview
+//                              --------
+// The component under test provides no functionality per se.  Its sole purpose
+// is capture a type in an assignable, copyable object. Therefore, it suffices
+// to verify, via appropriate declarations, that the names required are defined,
+// and usable as intended
+// 
+//-----------------------------------------------------------------------------
+// [ 1] BREATHING TEST
+
+//=============================================================================
+//                  STANDARD BDE ASSERT TEST MACRO
+//-----------------------------------------------------------------------------
+// NOTE: THIS IS A LOW-LEVEL COMPONENT AND MAY NOT USE ANY C++ LIBRARY
+// FUNCTIONS, INCLUDING IOSTREAMS.
+static int testStatus = 0;
+
+namespace {
+
+void aSsErT(bool b, const char *s, int i) {
+    if (b) {
+        printf("Error " __FILE__ "(%d): %s    (failed)\n", i, s);
+        if (testStatus >= 0 && testStatus <= 100) ++testStatus;
+    }
+}
+
+}  // close unnamed namespace
+
+//=============================================================================
+//                       STANDARD BDE TEST DRIVER MACROS
+//-----------------------------------------------------------------------------
+
+#define ASSERT       BSLS_BSLTESTUTIL_ASSERT
+
+#define Q   BSLS_BSLTESTUTIL_Q   // Quote identifier literally.
+#define P   BSLS_BSLTESTUTIL_P   // Print identifier and value.
+#define P_  BSLS_BSLTESTUTIL_P_  // P(X) without '\n'.
+#define T_  BSLS_BSLTESTUTIL_T_  // Print a tab (w/o newline).
+#define L_  BSLS_BSLTESTUTIL_L_  // current Line number
+
+// ============================================================================
+//                  NEGATIVE-TEST MACRO ABBREVIATIONS
+// ----------------------------------------------------------------------------
+
+#define ASSERT_SAFE_PASS(EXPR) BSLS_ASSERTTEST_ASSERT_SAFE_PASS(EXPR)
+#define ASSERT_SAFE_FAIL(EXPR) BSLS_ASSERTTEST_ASSERT_SAFE_FAIL(EXPR)
+#define ASSERT_PASS(EXPR)      BSLS_ASSERTTEST_ASSERT_PASS(EXPR)
+#define ASSERT_FAIL(EXPR)      BSLS_ASSERTTEST_ASSERT_FAIL(EXPR)
+#define ASSERT_OPT_PASS(EXPR)  BSLS_ASSERTTEST_ASSERT_OPT_PASS(EXPR)
+#define ASSERT_OPT_FAIL(EXPR)  BSLS_ASSERTTEST_ASSERT_OPT_FAIL(EXPR)
+
+// ============================================================================
+//                       GLOBAL TEST VALUES
+// ----------------------------------------------------------------------------
+
+static bool             verbose;
+static bool         veryVerbose;
+static bool     veryVeryVerbose;
+static bool veryVeryVeryVerbose;
+
+//=============================================================================
+//                  GLOBAL TYPEDEFS/CONSTANTS FOR TESTING
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+//                               TEST FACILITIES
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+//                                USAGE EXAMPLE
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+//                                 MAIN PROGRAM
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    int test = argc > 1 ? atoi(argv[1]) : 0;
+    verbose = argc > 2;
+    veryVerbose = argc > 3;
+    veryVeryVerbose = argc > 4;
+    veryVeryVeryVerbose = argc > 5;
+
+    printf("TEST " __FILE__ " CASE %d\n", test);
+
+    switch (test) { case 0:
+      case 1: {
+        // --------------------------------------------------------------------
+        // BREATHING TEST:
+        //   
+        //
+        // Plan:
+        //   Define a dummy type, wrap it, and use the wrapper.
+        //
+        // Testing:
+        //   This "test" *exercises* basic functionality.
+        // --------------------------------------------------------------------
+
+        if (verbose) printf("\nBREATHING TEST"
+                            "\n==============\n");
+
+        struct dummy { void use() {} void const_use() const {} };
+        dummy a;
+        const dummy b;
+
+        bsl::reference_wrapper<dummy> rwa(a);
+        bsl::reference_wrapper<const dummy> rwca(a);
+        bsl::reference_wrapper<const dummy> rwcb(b);
+
+        bsl::reference_wrapper<dummy> copyrwa(rwa);
+        bsl::reference_wrapper<const dummy> copyrwca(rwca);
+        bsl::reference_wrapper<const dummy> copyrwcb(rwcb);
+
+        copyrwa = a;
+        copyrwca = a;
+        copyrwcb = b;
+
+        copyrwa = rwa;
+        copyrwca = rwca;
+        copyrwcb = rwcb;
+        
+        dummy& rax = rwa;
+        const dummy& rcax = rwca;
+        const dummy& rcbx = rwcb;
+        
+        dummy& ray = bsl::ref(a);
+        const dummy& rcay = bsl::cref(a);
+        const dummy& rcby = bsl::cref(b);
+
+        bsl::reference_wrapper<dummy> copyrwaz(bsl::ref(rwa));
+        bsl::reference_wrapper<const dummy> copyrwcaz(bsl::ref(rwca));
+        bsl::reference_wrapper<const dummy> copyrwcbz(bsl::ref(rwcb));
+
+        ASSERT(&copyrwa.get() == &a);
+        ASSERT(&copyrwca.get() == &a);
+        ASSERT(&copyrwcb.get() == &b);
+        ASSERT(&copyrwaz.get() == &a);
+        ASSERT(&copyrwcaz.get() == &a);
+        ASSERT(&copyrwcbz.get() == &b);
+
+      } break;
+      default: {
+        fprintf(stderr, "WARNING: CASE `%d' NOT FOUND.\n", test);
+        testStatus = -1;
+      }
+    }
+
+    if (testStatus > 0) {
+        fprintf(stderr, "Error, non-zero test status = %d.\n", testStatus);
+    }
+    return testStatus;
+}
+
+// ----------------------------------------------------------------------------
+// Copyright 2013 Bloomberg Finance L.P.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------- END-OF-FILE ----------------------------------
