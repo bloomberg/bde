629d9476253d86574163810c104db756552630b0
Henry (Mike) Verschell <hversche@bloomberg.com>
2015-02-04 17:15:29 -0500

    Fix test driver build on osx

--- groups/bsl/bsls/bsls_protocoltest.t.cpp
+++ groups/bsl/bsls/bsls_protocoltest.t.cpp
@@ -797,9 +797,7 @@ int main(int argc, char *argv[])
                 ASSERT(status.failures() == 0);
                 ASSERT(status.last());
 
-#if !(defined(BSLS_PLATFORM_CMP_CLANG) &&                                     \
-       (BSLS_PLATFORM_CMP_VER_MAJOR < 3 ||                                    \
-        (BSLS_PLATFORM_CMP_VER_MAJOR == 3 && BSLS_PLATFORM_CMP_VER_MINOR < 5)))
+#ifndef BSLS_PLATFORM_CMP_CLANG
                 // this test is broken on Clang
 
                 if (veryVerbose) printf("\t\tfailure case\n");
