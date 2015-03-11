c68eac3c6ac8af2a471e4560c9920bd336bac41b
Edmond Kwan <ekwan11@bloomberg.net>
2015-03-11 18:28:32 -0400

    updated copy rights to 2015

--- groups/bsl/bsl+bslhdrs/bsl_system_error.h
+++ groups/bsl/bsl+bslhdrs/bsl_system_error.h
@@ -41,7 +41,7 @@ namespace bsl {
 #endif
 
 // ----------------------------------------------------------------------------
-// Copyright 2013 Bloomberg Finance L.P.
+// Copyright 2015 Bloomberg Finance L.P.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
