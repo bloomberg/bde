c9e4b7aa25f4e4bf80c80d4adaa96349583dfdef
Jeffrey Mendelsohn <JMENDELSOHN4@bloomberg.net>
2015-04-28 08:49:02 -0400

    incorporated CW review comments

--- groups/bdl/bdlc/bdlc_packedintarray.h
+++ groups/bdl/bdlc/bdlc_packedintarray.h
@@ -52,7 +52,7 @@ BSLS_IDENT("$Id: $")
 // We can use 'bdlc::PackedIntArray' to efficiently store this data.
 //
 // First, we declare and define a 'my_Date' class.  This class is very similar
-// to 'bdet_date', and therefore is elided for the sake of compactness.
+// to 'bdlt::Date', and therefore is elided for the sake of compactness.
 //..
 //                              // =======
 //                              // my_Date
@@ -2508,10 +2508,17 @@ struct UsesBslmaAllocator<bdlc::PackedIntArray<TYPE> > : bsl::true_type {};
 #endif
 
 // ----------------------------------------------------------------------------
-// NOTICE:
-// Copyright (C) Bloomberg L.P., 2014
-// All Rights Reserved.
-// Property of Bloomberg L.P.  (BLP)
-// This software is made available solely pursuant to the
-// terms of a BLP license agreement which governs its use.
-// ---------------------------------- END-OF-FILE -----------------------------
+// Copyright 2015 Bloomberg Finance L.P.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------- END-OF-FILE ----------------------------------
--- groups/bdl/bdlc/bdlc_packedintarray.t.cpp
+++ groups/bdl/bdlc/bdlc_packedintarray.t.cpp
@@ -270,7 +270,7 @@ typedef bslx::TestOutStream Out;
 // We can use 'bdlc::PackedIntArray' to efficiently store this data.
 //
 // First, we declare and define a 'my_Date' class.  This class is very similar
-// to 'bdet_date', and therefore is elided for the sake of compactness.
+// to 'bdlt::Date', and therefore is elided for the sake of compactness.
 //..
                                 // =======
                                 // my_Date
@@ -7714,10 +7714,17 @@ int main(int argc, char *argv[])
 }
 
 // ----------------------------------------------------------------------------
-// NOTICE:
-// Copyright (C) Bloomberg L.P., 2014
-// All Rights Reserved.
-// Property of Bloomberg L.P.  (BLP)
-// This software is made available solely pursuant to the
-// terms of a BLP license agreement which governs its use.
-// ---------------------------------- END-OF-FILE -----------------------------
+// Copyright 2015 Bloomberg Finance L.P.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------- END-OF-FILE ----------------------------------
