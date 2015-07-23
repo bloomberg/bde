139a95860e99f8eb0f9115f7a0856831889bc9dd
Alisdair Meredith <ameredith1@bloomberg.net>
2015-07-23 16:57:03 -0400

    Remove broken 'using' from test driver
    This test driver retained a 'using namespace std', despite none
    of the headers included above it actually declaring anything in
    namespace std.

--- groups/bsl/bslmf/bslmf_functionpointertraits.t.cpp
+++ groups/bsl/bslmf/bslmf_functionpointertraits.t.cpp
@@ -11,7 +11,6 @@
 #include <stdlib.h>           // atoi()
 
 using namespace BloombergLP;
-using namespace std;
 
 //=============================================================================
 //                             TEST PLAN
