4dc19795ad94c993375734cecaab20b93e5dbe5b
(Henry) Mike Verschell <hverschell@bloomberg.net>
2015-07-24 17:03:20 -0400

    [APPROVAL] INTERNAL pending/master/stringref-width_drqs-62452070 to master
    Base commit: 5cb99991d768499fae31d58b30b8fdf56ecde702

--- groups/bsl/bslstl/bslstl_stringref.h
+++ groups/bsl/bslstl/bslstl_stringref.h
@@ -1394,8 +1394,9 @@ bslstl::operator<<(std::basic_ostream<CHAR_TYPE>& stream,
     }
     else {
         stringRef.write(stream);
-        stream.width(0);
     }
+
+    stream.width(0);
 
     return stream;
 }
