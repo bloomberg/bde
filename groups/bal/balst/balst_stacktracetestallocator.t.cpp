32e74f006e54c8f70a666e1bcab8af188f2ef232
Bill Chapman <bchapman2@bloomberg.net>
2015-08-10 11:57:33 -0400

    balst: -- kill tabs

--- groups/bal/balst/balst_stacktracetestallocator.t.cpp
+++ groups/bal/balst/balst_stacktracetestallocator.t.cpp
@@ -1186,8 +1186,8 @@ int main(int argc, char *argv[])
         bdlsu::FilesystemUtil::remove("shipscrew.txt");
 
 #ifdef BSLS_PLATFORM_OS_WINDOWS
-	// 'remove' above uses the default allocator on Windows, so suppress
-	// the default allocator check at the end.
+        // 'remove' above uses the default allocator on Windows, so suppress
+        // the default allocator check at the end.
 
         expectedDefaultAllocations = -1;
 #endif
