3deb2e155aaa4c9d76f5deaeb996fcebae87fd88
Hyman Rosen <hrosen4@bloomberg.net>
2015-08-11 18:50:18 -0400

    Open source btlscm cleanup

--- groups/btl/btlscm/btlscm_versiontag.t.cpp
+++ groups/btl/btlscm/btlscm_versiontag.t.cpp
@@ -54,7 +54,7 @@ int newFunction()
 // Not defined and never called due to conditional compilation
 
 //=============================================================================
-//                  MAIN PROGRAM
+//                                MAIN PROGRAM
 //-----------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
 
     switch (test) { case 0:
       case 2: {
-        //--------------------------------------------------------------------
+        //---------------------------------------------------------------------
         // TEST USAGE EXAMPLE
         //
         // Concern:
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
         //
         // Testing:
         //   USAGE EXAMPLE
-        //--------------------------------------------------------------------
+        //---------------------------------------------------------------------
 
         if (verbose) bsl::printf("\nTEST USAGE EXAMPLE"
                                  "\n==================\n");
@@ -97,7 +97,7 @@ int main(int argc, char *argv[])
       } break;
 
       case 1: {
-        //--------------------------------------------------------------------
+        //---------------------------------------------------------------------
         // TEST VERSION CONSISTENCY
         //
         // Concerns:
@@ -112,7 +112,7 @@ int main(int argc, char *argv[])
         //   BTL_VERSION
         //   BTL_VERSION_MAJOR
         //   BTL_VERSION_MINOR
-        //--------------------------------------------------------------------
+        //---------------------------------------------------------------------
 
         if (verbose) bsl::printf("\nTEST VERSION CONSISTENCY"
                                  "\n========================\n");
@@ -138,11 +138,18 @@ int main(int argc, char *argv[])
     return testStatus;
 }
 
-// ---------------------------------------------------------------------------
-// NOTICE:
-//      Copyright (C) Bloomberg L.P., 2015
-//      All Rights Reserved.
-//      Property of Bloomberg L.P. (BLP)
-//      This software is made available solely pursuant to the
-//      terms of a BLP license agreement which governs its use.
-// ----------------------------- END-OF-FILE ---------------------------------
+// ----------------------------------------------------------------------------
+// Copyright 2015 Bloomberg Finance L.P.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------- END-OF-FILE ----------------------------------
