16c785e6ab44cab73d3f3db24affcf22ec04e5c8
Rohan Bhindwale <rbhindwale@bloomberg.net>
2015-08-20 15:08:28 -0400

    cleaning up btlso_event

--- groups/btl/btlso/btlso_event.h
+++ groups/btl/btlso/btlso_event.h
@@ -7,23 +7,155 @@
 #endif
 BSLS_IDENT("$Id: $")
 
-
 //@PURPOSE: Provide component to describe an event on a socket.
 //
 //@CLASSES:
-//      btlso::Event: representation of an event on a socket
+//  btlso::Event: representation of an event on a socket
 //  btlso::EventHash: namespace for a hash function
 //
 //@AUTHOR: Daniel Glaser (dglaser),  Herve Bronnimann (hbronnim)
 //
-//@DESCRIPTION: This component provides a straightforward attribute class,
-// 'btlso::Event', which represents an event on a particular socket.  The two
-// attributes are the socket handle and the event type.  The recognized events
-// are 'ACCEPT', 'CONNECT', 'READ', and 'WRITE'.  Accessors and manipulators
-// are provided for the socket handle and the event type.  In order to
-// facilitate storing 'btlso::Event' objects into 'bsl::unordered_map' or other
-// unordered associative containers, a hash functor, 'btlso::EventHash', is also
-// provided.
+//@DESCRIPTION: This component provides an unconstrained (value-semantic)
+// attribute class, 'btlso::Event', which represents an event on a particular
+// socket.  The two attributes are the socket handle and the event type.  The
+// recognized events are 'btlso::EventType::e_ACCEPT',
+// 'btlso::EventType::e_CONNECT', 'btlso::EventType::e_READ', and
+// 'btlso::EventType::e_WRITE'.  Accessors and manipulators are provided for
+// the socket handle and the event type.  In order to facilitate storing
+// 'btlso::Event' objects into associative containers, a hash functor,
+// 'btlso::EventHash', is also provided.
+//
+///Attributes
+///----------
+//..
+//  Name    Type                 Constraints
+//  ------  ----                 -----------
+//  handle  SocketHandle::Handle none
+//  type    EventType::Type      none
+//..
+//: o 'handle': The socket handle on which the event is registered.
+//:
+//: o 'type': The event type corresponding to the event registered.
+//
+///Usage
+///-----
+// This section illustrates intended use of this component.
+//
+///Example 1: Implementing an event manager
+/// - - - - - - - - - - - - - - - - - - - -
+// First, we define a class, 'my_EventManager', that allows clients to register
+// an asynchronous callback that is invoked when a certain event on a socket
+// handle is complete.  As an example, objects of this class would allow users
+// to register a read callback that is invoked when there is data available on
+// a socket.
+//
+// The definition of the 'my_EventManager' class is provided below with
+// non-relevant portions elided for brevity:
+//..
+//  class my_EventManager {
+//      // This class allows clients to register asynchronous callbacks that
+//      // are invoked when certain events fire on a socket handle.
+//
+//    public:
+//      // TYPES
+//      typedef bdlf::Function<void (*)()> Callback;
+//
+//    private:
+//      typedef bsl::unordered_map<btlso::Event,
+//                                 Callback,
+//                                 btlso::EventHash> CallbacksMap;
+//
+//      // DATA
+//      CallbacksMap d_callbacks;
+//
+//      . . .
+//
+//    public:
+//      // CREATORS
+//      my_EventManager(bslma::Allocator *basicAllocator = 0);
+//          // Create a 'my_EventManager' object.  Optionally specify a
+//          // 'basicAllocator' used to supply memory.  If 'basicAllocator' is
+//          // 0, the currently installed default allocator is used.
+//
+//      ~my_EventManager();
+//
+//      // MANIPULATORS
+//      int registerEvent(btlso::SocketHandle::Handle handle,
+//                        btlso::EventType::Type      eventType,
+//                        Callback&                   callback);
+//          // Register the specified 'callback' to be invoked when the
+//          // specified 'eventType' fires on the specified 'handle'.  Return
+//          // 0 on success and non-zero value otherwise.
+//
+//      . . .
+//  };
+//..
+// Then, we provide the function definitions for the 'my_EventManager' class:
+//..
+//  // CREATORS
+//  my_EventManager::my_EventManager(bslma::Allocator *basicAllocator)
+//  : d_callbacks(basicAllocator)
+//  {
+//  }
+//
+//  my_EventManager::~my_EventManager()
+//  {
+//  }
+//
+//  // MANIPULATORS
+//  int my_EventManager::registerEvent(btlso::SocketHandle::Handle handle,
+//                                     btlso::EventType::Type      eventType,
+//                                     Callback&                   callback)
+//  {
+//..
+// Next, we create a 'btlso_Event' object
+//..
+//      btlso::Event event(handle, eventType);
+//..
+// Then, we confirm if a callback is already registered for 'event'.  If so we
+// simply update the callback:
+//..
+//      CallbacksMap::iterator iter = d_callbacks.find(event);
+//      if (d_callbacks.end() != iter) {
+//          iter->second = callback;
+//          return 0;
+//      }
+//..
+// Next, we add the event to the callbacks map:
+//..
+//      bool insertedEvent = d_callbacks.insert(
+//                                     bsl::make_pair(event, callback)).second;
+//      assert(insertedEvent);
+//
+//      return 0;
+//  }
+//..
+// Now, we specify a read callback:
+//..
+//  void readCallback(btlso::SocketHandle::Handle handle)
+//      // Process data read from the specified 'handle'.
+//  {
+//      // . . .
+//  }
+//..
+// Finally, we construct a 'my_EventManager' object and register an event:
+//..
+//      my_EventManager             manager;
+//
+//      btlso::SocketHandle::Handle handle;
+//
+//      // Open the socket 'handle'
+//
+//      // . . .
+//
+//      btlso::EventType::Type    eventType = btlso::EventType::e_READ;
+//      my_EventManager::Callback callback  = bdlf::BindUtil::bind(
+//                                                               &readCallback,
+//                                                               handle);
+//
+//      const int rc = manager.registerEvent(handle, eventType, callback);
+//      assert(0 == rc);
+//..
 
 #ifndef INCLUDED_BTLSCM_VERSION
 #include <btlscm_version.h>
@@ -37,29 +169,27 @@ BSLS_IDENT("$Id: $")
 #include <btlso_eventtype.h>
 #endif
 
-#ifndef INCLUDED_BSLALG_TYPETRAITS
-#include <bslalg_typetraits.h>
+#ifndef INCLUDED_BSLMF_ISTRIVIALLYCOPYABLE
+#include <bslmf_istriviallycopyable.h>
 #endif
 
-#ifndef INCLUDED_BSLALG_TYPETRAITBITWISECOPYABLE
-#include <bslalg_typetraitbitwisecopyable.h>
-#endif
-
-#ifndef INCLUDED_BSL_CSTDDEF
-#include <bsl_cstddef.h>
+#ifndef INCLUDED_BSLMF_NESTEDTRAITDECLARATION
+#include <bslmf_nestedtraitdeclaration.h>
 #endif
 
 namespace BloombergLP {
 
 namespace btlso {
-                             // =================
+
+                             // ===========
                              // class Event
-                             // =================
+                             // ===========
 
 class Event {
     // This attribute class represents an event on a socket.  The recognized
-    // events are ACCEPT, CONNECT, READ, and WRITE.  Accessors and manipulators
-    // are provided for the socket handle and the event type.
+    // events are 'btlso::EventType::e_ACCEPT', 'btlso::EventType::e_CONNECT',
+    // 'btlso::EventType::e_READ', and 'btlso::EventType::e_WRITE'.  Accessors
+    // and manipulators are provided for the socket handle and the event type.
 
     // DATA
     SocketHandle::Handle d_handle;
@@ -70,16 +200,15 @@ class Event {
 
   public:
     // TRAITS
-    BSLALG_DECLARE_NESTED_TRAITS(Event,
-                                 bslalg::TypeTraitBitwiseCopyable);
+    BSLMF_NESTED_TRAIT_DECLARATION(Event, bsl::is_trivially_copyable)
 
     // CREATORS
+    Event(const SocketHandle::Handle& handle,
+          const EventType::Type       type);
+        // Construct an event of type 'type' on socket 'handle'.
+
     Event(const Event& original);
         // Copy constructor.
-
-    Event(const SocketHandle::Handle& handle,
-                const EventType::Type       type);
-        // Construct an event of type 'type' on socket 'handle'.
 
     ~Event();
         // Destroy this event object.
@@ -111,14 +240,14 @@ bool operator!=(const Event& lhs, const Event& rhs);
     // and only if they have the same socket handle and event type field
     // values.
 
-                           // =====================
+                           // ===============
                            // class EventHash
-                           // =====================
+                           // ===============
 
 struct EventHash {
     // This 'struct' provides a functor that computes a hash value for a
-    // 'Event' object, and is suitable for use with 'bsl::unordered_map'
-    // or other unordered associative containers.
+    // 'Event' object, and is suitable for use with 'bsl::unordered_map' or
+    // other unordered associative containers.
 
     // ACCESSORS
     bsl::size_t operator()(const Event& event) const;
@@ -129,9 +258,9 @@ struct EventHash {
 //                        INLINE FUNCTION DEFINITIONS
 // ============================================================================
 
-                             // -----------------
+                             // -----------
                              // class Event
-                             // -----------------
+                             // -----------
 
 // CREATORS
 inline
@@ -142,8 +271,7 @@ Event::Event(const Event& original)
 }
 
 inline
-Event::Event(const SocketHandle::Handle& handle,
-                         const EventType::Type       type)
+Event::Event(const SocketHandle::Handle& handle, const EventType::Type type)
 : d_handle(handle)
 , d_type(type)
 {
@@ -179,6 +307,7 @@ EventType::Type Event::type() const
 {
     return d_type;
 }
+
 }  // close package namespace
 
 // FREE OPERATORS
