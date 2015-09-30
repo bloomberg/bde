937c701e3d5befa42cd0c2c19ddca28217fe61bc
Hyman Rosen <hrosen4@bloomberg.net>
2015-09-30 19:27:57 -0400

    Errors exposed in Windows.

--- groups/btl/btlmt/btlmt_tcptimereventmanager.t.cpp
+++ groups/btl/btlmt/btlmt_tcptimereventmanager.t.cpp
@@ -406,7 +406,7 @@ void *registerThread(void *arg)
                        &dummyFunction);
         if (veryVerbose) {
             printf("Thread %d: Iteration (O) %d\n",
-                   (int) bslmt::ThreadUtil::self(),
+                   (int) bslmt::ThreadUtil::selfIdAsInt(),
                    i);
         }
         mX->registerSocketEvent(fd,
@@ -430,7 +430,7 @@ void *registerThread(void *arg)
                                 btlso::EventType::e_READ, callback);
         if (veryVerbose) {
             printf("Thread %d: Iteration (C)%d\n",
-                   (int) bslmt::ThreadUtil::self(),
+                   (int) bslmt::ThreadUtil::selfIdAsInt(),
                    i);
         }
         LOOP_ASSERT(i, mX->isRegistered(fd,
