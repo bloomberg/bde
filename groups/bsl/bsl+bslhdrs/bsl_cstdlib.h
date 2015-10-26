c025680e76fed36a4b39b75698cd96a83e352733
(Henry) Mike Verschell <hverschell@bloomberg.net>
2015-10-26 18:10:34 -0400

    [APPROVAL] INTERNAL pending/releases/2.24.x/add-bslcstdlib-export-drqs-69830273 to releases/2.24.x
    Base commit: 67452176cdd0d334dcdcec57396ea48c22da2859

--- groups/bsl/bsl+bslhdrs/bsl_cstdlib.h
+++ groups/bsl/bsl+bslhdrs/bsl_cstdlib.h
@@ -50,6 +50,7 @@ namespace bsl
     using native_std::qsort;
     using native_std::rand;
     using native_std::realloc;
+    using native_std::size_t;
     using native_std::srand;
     using native_std::strtod;
     using native_std::strtol;
@@ -57,7 +58,6 @@ namespace bsl
     using native_std::system;
     using native_std::wcstombs;
     using native_std::wctomb;
-
 }
 
 #endif
