9f283565be2de28628cf7af38cc15fd5b44eaed0
Alexander Beels <abeels@bloomberg.net>
2016-02-05 17:22:05 -0500

    Publish master to opensource
    commit 1ad050d4dcf8477112250ce6d670017e9cbd22d9
    Merge: 80cc5ab 28c27a6
    Date:   Wed Jan 27 14:53:08 2016 -0500
    
        Merge pull request #116 from hrosen4/bdlmt_threadpool-dtor-doc_drqs-76512668
    
        bdlmt_threadpool: Correct/clarify destructor documentation.
    
    commit 28c27a62aa483dba1c0fc390a8fc9bd665ea74e4
    Date:   Tue Jan 19 18:01:19 2016 -0500
    
        Correct/clarify destructor documentation.

--- groups/bdl/bdlmt/bdlmt_threadpool.h
+++ groups/bdl/bdlmt/bdlmt_threadpool.h
@@ -536,7 +536,7 @@ class ThreadPool {
         // milliseconds), and using the optionally specified 'basicAllocator'.
 
     ~ThreadPool();
-        // Drain all pending jobs and destroy this thread pool.
+        // Call 'shutdown()' and destroy this thread pool.
 
     // MANIPULATORS
     void drain();
