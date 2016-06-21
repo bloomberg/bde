b0363ff44f06702a33ec7e8dd4c2b2d77a6cb212
Hyman Rosen <hrosen4@bloomberg.net>
2016-06-21 11:42:33 -0400

    Use bsl::strcmp.

--- groups/bal/balb/balb_performancemonitor.cpp
+++ groups/bal/balb/balb_performancemonitor.cpp
@@ -420,8 +420,8 @@ int balb::PerformanceMonitor::Collector<bsls::Platform::OsLinux>
     int numNodes = scandir(taskFilename.str().c_str(), &entry, 0, 0);
     for (int nodeIndex = 0; nodeIndex < numNodes; ++nodeIndex) {
         if (DT_DIR == entry[nodeIndex]->d_type &&
-            strcmp(".",  entry[nodeIndex]->d_name) != 0 &&
-            strcmp("..", entry[nodeIndex]->d_name) != 0) {
+            bsl::strcmp(".",  entry[nodeIndex]->d_name) != 0 &&
+            bsl::strcmp("..", entry[nodeIndex]->d_name) != 0) {
             ++numThreads;
         }
         bsl::free(entry[nodeIndex]);
