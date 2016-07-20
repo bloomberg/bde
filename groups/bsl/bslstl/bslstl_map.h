504692fedf02da8417161d22f3f7f12e313438d2
Alexander Beels <abeels@bloomberg.net>
2016-07-20 13:57:55 -0400

    Adopt commit 34ba321 from master: Merge pull request #524 from cwilson/small-map-deque-fixes-for-icbeta-drqs-84793398
    commit 34ba32130cc7ba92ff21eb35d48ecbe5ff838fd6
    Merge: f6fadc6 e4d93ac
    Author: Henry Verschell <hverschell@bloomberg.net>
    Date:   Thu Jun 30 15:12:33 2016 -0400
    
        Merge pull request #524 from cwilson/small-map-deque-fixes-for-icbeta-drqs-84793398
    
        Small map and deque fixes for ICBETA (DRQS 84793398)
    
    commit e4d93ac49bfc1ce39af9827131099e09243e1f50
    Author: Clay Wilson <cwilson9@bloomberg.net>
    Date:   Wed Jun 29 16:58:56 2016 -0400
    
        added a note regarding 'select_on_container_copy_construction'
    
    commit 88f4faf688cfdea7e940899cdf6e98ba81e64b9a
    Author: Clay Wilson <cwilson9@bloomberg.net>
    Date:   Wed Jun 29 11:53:10 2016 -0400
    
        fix two small C++11 issues (map, deque) for ICBETA
    
        1 The wrong allocator is used for a temporary in 'map::operator[](key_type&&)'.
        2 'select_on_container_copy_construction' is not used in deque's 1-arg copy
          ctor.

--- groups/bsl/bslstl/bslstl_map.h
+++ groups/bsl/bslstl/bslstl_map.h
@@ -533,6 +533,14 @@ BSL_OVERRIDES_STD mode"
 #include <bslalg_typetraithasstliterators.h>
 #endif
 
+#ifndef INCLUDED_BSLMA_CONSTRUCTIONUTIL
+#include <bslma_constructionutil.h>
+#endif
+
+#ifndef INCLUDED_BSLMA_DEFAULT
+#include <bslma_default.h>
+#endif
+
 #ifndef INCLUDED_BSLMA_DESTRUCTORGUARD
 #include <bslma_destructorguard.h>
 #endif
@@ -856,9 +864,11 @@ class map {
         // pairs contained in this map.  Use the allocator returned by
         // 'bsl::allocator_traits<ALLOCATOR>::
         // select_on_container_copy_construction(original.get_allocator())' to
-        // allocate memory.  This method requires that the (template parameter)
-        // types 'KEY' and 'VALUE' both be 'copy-insertable' into this map (see
-        // {Requirements on 'KEY' and 'VALUE'}).
+        // allocate memory.  If the (template parameter) type 'ALLOCATOR' is
+        // 'bsl::allocator' (the default), the currently installed default
+        // allocator is used.  This method requires that the (template
+        // parameter) types 'KEY' and 'VALUE' both be 'copy-insertable' into
+        // this map (see {Requirements on 'KEY' and 'VALUE'}).
 
     map(BloombergLP::bslmf::MovableRef<map> original);
         // Create a map having the same value as the specified 'original'
@@ -2226,10 +2236,10 @@ VALUE& map<KEY, VALUE, COMPARATOR, ALLOCATOR>::operator[](
     BloombergLP::bslalg::RbTreeNode *node =
      BloombergLP::bslalg::RbTreeUtil::find(d_tree, this->comparator(), lvalue);
     if (d_tree.sentinel() == node) {
-        ALLOCATOR allocator = get_allocator();
-
         BloombergLP::bsls::ObjectBuffer<VALUE> temp;  // for default 'VALUE'
-        AllocatorTraits::construct(allocator, temp.address());
+        BloombergLP::bslma::ConstructionUtil::construct(
+                                     temp.address(),
+                                     BloombergLP::bslma::Default::allocator());
         BloombergLP::bslma::DestructorGuard<VALUE> guard(temp.address());
 
         return emplace_hint(iterator(node),
