b7418876fa74ca94e7469bab3a8e71cda2981eec
Hyman Rosen <hrosen4@bloomberg.net>
2016-08-30 15:08:12 -0400

    Detect type_traits traits in Clang++.
    Clang has a __has_feature(...) tester macro that can be used to detect whether
    the _is_trivially_... traits and aligned_union are present, so use them.

--- groups/bsl/bsl+bslhdrs/bsl_type_traits.h
+++ groups/bsl/bsl+bslhdrs/bsl_type_traits.h
@@ -64,15 +64,24 @@ BSLS_IDENT("$Id: $")
 
 #if defined(BSLS_PLATFORM_CMP_GNU) && BSLS_PLATFORM_CMP_VERSION < 50000
 #  undef BSL_TYPE_TRAITS_HAS_IS_TRIVIALLY_TRAITS
-#endif
-
-#if defined(BSLS_PLATFORM_CMP_GNU) && BSLS_PLATFORM_CMP_VERSION < 50000
 #  undef BSL_TYPE_TRAITS_HAS_ALIGNED_UNION
 #endif
 
-#if defined(BSLS_PLATFORM_CMP_CLANG) && defined(__APPLE_CC__) \
-    && __APPLE_CC__ <= 6000
+#if defined(BSLS_PLATFORM_CMP_CLANG)
+
+#  if !__has_feature(aligned_union)
 #    undef BSL_TYPE_TRAITS_HAS_ALIGNED_UNION
+#  endif
+
+#  if !__has_feature(is_trivially_constructible)      || \
+      !__has_feature(is_trivially_copy_constructible) || \
+      !__has_feature(is_trivially_move_constructible) || \
+      !__has_feature(is_trivially_assignable)         || \
+      !__has_feature(is_trivially_copy_assignable)    || \
+      !__has_feature(is_trivially_move_assignable)
+#    undef BSL_TYPE_TRAITS_HAS_IS_TRIVIALLY_TRAITS
+#  endif
+
 #endif
 
 namespace bsl
