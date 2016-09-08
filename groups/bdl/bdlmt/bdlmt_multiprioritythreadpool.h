407ac6d577c2a8abe3cd442a794c55e6b4c88e50
Bill Chapman <bchapman2@bloomberg.net>
2016-09-08 11:57:29 -0400

    bdlcc_multipriorityqueue: fix memory leak in 'pushBack'.

--- groups/bdl/bdlmt/bdlmt_multiprioritythreadpool.h
+++ groups/bdl/bdlmt/bdlmt_multiprioritythreadpool.h
@@ -521,6 +521,7 @@ class MultipriorityThreadPool {
                                           // broadcast when suspended threads
                                           // are to resume
 
+  private:
     // NOT IMPLEMENTED
     MultipriorityThreadPool(const MultipriorityThreadPool&);
     MultipriorityThreadPool& operator=(const MultipriorityThreadPool&);
