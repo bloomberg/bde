16b9bb8b088d4a0b82e0920c473026c241ca5cca
(Henry) Mike Verschell <herschell@bloomberg.net>
2016-10-04 16:42:25 -0400

    Fix bdld_manageddatum.t test case relying on RVO

--- groups/bdl/bdld/bdld_manageddatum.t.cpp
+++ groups/bdl/bdld/bdld_manageddatum.t.cpp
@@ -1094,86 +1094,92 @@ int main(int argc, char *argv[])
             bdlt::Date udt;
             bdlt::Date udt1;
 
-            const Obj datumArray[] = {
-                Obj(),
-                Obj(Datum::createInteger(0), &ta),
-                Obj(Datum::createInteger(1), &ta),
-                Obj(Datum::createBoolean(true), &ta),
-                Obj(Datum::createBoolean(false), &ta),
-                Obj(Datum::createError(0), &ta),
-                Obj(Datum::createError(1, "some error", &ta), &ta),
-                Obj(Datum::createDate(bdlt::Date(2010, 1, 5)), &ta),
-                Obj(Datum::createDate(bdlt::Date(1, 1, 1)), &ta),
-                Obj(Datum::createTime(bdlt::Time(16, 45, 32, 12)), &ta),
-                Obj(Datum::createTime(bdlt::Time(1, 1, 1, 1)), &ta),
-                Obj(Datum::createDatetime(
-                        bdlt::Datetime(2010, 1, 5, 16, 45, 32, 12), &ta), &ta),
-                Obj(Datum::createDatetime(
-                            bdlt::Datetime(1, 1, 1, 1, 1, 1, 1), &ta), &ta),
-                Obj(Datum::createDatetimeInterval(
-                        bdlt::DatetimeInterval(34, 16, 45, 32, 12), &ta), &ta),
-                Obj(Datum::createDatetimeInterval(
-                            bdlt::DatetimeInterval(1, 1, 1, 1, 1), &ta), &ta),
-                Obj(Datum::createInteger64(9223372036854775807LL, &ta), &ta),
-                Obj(Datum::createInteger64(1229782938247303441LL, &ta), &ta),
-                Obj(Datum::copyString("A long string", &ta), &ta),
-                Obj(Datum::copyString("A very long string", &ta), &ta),
-                Obj(Datum::copyString("abc", &ta), &ta),
-                Obj(Datum::copyString("Abc", &ta), &ta),
-                Obj(Datum::createDouble(1.0), &ta),
-                Obj(Datum::createDouble(1.2345), &ta),
-                Obj(Datum::createDouble(-1.2346), &ta),
-                Obj(Datum::createDouble(k_DBL_MIN2), &ta),
-                Obj(Datum::createDouble(k_DBL_MAX2), &ta),
-                Obj(Datum::createDouble(k_DBL_INF), &ta),
-                Obj(Datum::createDouble(k_DBL_NEGINF), &ta),
+            struct {
+                int         d_line;
+                bdld::Datum d_datum;
+            } DATA[] = {
+            { L_, Datum::createNull() },
+            { L_, Datum::createInteger(0) },
+            { L_, Datum::createInteger(1) },
+            { L_, Datum::createBoolean(true) },
+            { L_, Datum::createBoolean(false) },
+            { L_, Datum::createError(0) },
+            { L_, Datum::createError(1, "some error", &ta) },
+            { L_, Datum::createDate(bdlt::Date(2010, 1, 5)) },
+            { L_, Datum::createDate(bdlt::Date(1, 1, 1)) },
+            { L_, Datum::createTime(bdlt::Time(16, 45, 32, 12)) },
+            { L_, Datum::createTime(bdlt::Time(1, 1, 1, 1)) },
+            { L_, Datum::createDatetime(
+                bdlt::Datetime(2010, 1, 5, 16, 45, 32, 12), &ta) },
+            { L_, Datum::createDatetime(
+                   bdlt::Datetime(1, 1, 1, 1, 1, 1, 1), &ta) },
+            { L_, Datum::createDatetimeInterval(
+                        bdlt::DatetimeInterval(34, 16, 45, 32, 12), &ta) },
+            { L_, Datum::createDatetimeInterval(
+                            bdlt::DatetimeInterval(1, 1, 1, 1, 1), &ta) },
+            { L_, Datum::createInteger64(9223372036854775807LL, &ta) },
+            { L_, Datum::createInteger64(1229782938247303441LL, &ta) },
+            { L_, Datum::copyString("A long string", &ta) },
+            { L_, Datum::copyString("A very long string", &ta) },
+            { L_, Datum::copyString("abc", &ta) },
+            { L_, Datum::copyString("Abc", &ta) },
+            { L_, Datum::createDouble(1.0) },
+            { L_, Datum::createDouble(1.2345) },
+            { L_, Datum::createDouble(-1.2346) },
+            { L_, Datum::createDouble(k_DBL_MIN2) },
+            { L_, Datum::createDouble(k_DBL_MAX2) },
+            { L_, Datum::createDouble(k_DBL_INF) },
+            { L_, Datum::createDouble(k_DBL_NEGINF) },
 
-                // This test will take care of 0.0 as -0.0 == 0.0.
+            // This test will take care of 0.0 as -0.0 == 0.0.
 
-                Obj(Datum::createDouble(k_DBL_NEGZERO), &ta),
-                Obj(Datum::createUdt(&udt, UDT_TYPE), &ta),
-                Obj(Datum::createUdt(&udt, 1), &ta),
-                Obj(Datum::createUdt(&udt1, UDT_TYPE), &ta),
+            { L_, Datum::createDouble(k_DBL_NEGZERO) },
+            { L_, Datum::createUdt(&udt, UDT_TYPE) },
+            { L_, Datum::createUdt(&udt, 1) },
+            { L_, Datum::createUdt(&udt1, UDT_TYPE) },
 
-                // Treat NaNs specially as value == value is false for NaN.
+            // Treat NaNs specially as value == value is false for NaN.
 
-                Obj(Datum::createDouble(k_DBL_QNAN2), &ta),
-                Obj(Datum::createDouble(k_DBL_SNAN), &ta),
-                Obj(createArray(&ta), &ta),
-                Obj(createEmptyArray(), &ta),
-                Obj(createMap(&ta), &ta),
-                Obj(createEmptyMap(), &ta)
+            { L_, Datum::createDouble(k_DBL_QNAN2) },
+            { L_, Datum::createDouble(k_DBL_SNAN) },
+            { L_, createArray(&ta) },
+            { L_, createEmptyArray() },
+            { L_, createMap(&ta) },
+            { L_, createEmptyMap() }
             };
-
-            const int datumArraySize = static_cast<int>(
-                                     sizeof(datumArray) / sizeof(*datumArray));
+            const int NUM_DATA = static_cast<int>(
+                sizeof(DATA) / sizeof(*DATA));
 
             // Comparing ManagedDatum objects.
 
-            for (int i = 0; i < datumArraySize; ++i) {
-                for (int j = 0; j < datumArraySize; ++j) {
-                    if (i == j) {
-                        if (datumArray[i]->isDouble() &&
-                                datumArray[i]->theDouble() !=
-                                    datumArray[i]->theDouble()) {
-                            // Treat NaNs specially.
-
-                            ASSERTV(i, j, datumArray[i] != datumArray[j]);
-                            ASSERTV(i, j, datumArray[j] != datumArray[i]);
-
-                        }
-                        else {
-                            ASSERTV(i, j, datumArray[i] == datumArray[j]);
-                            ASSERTV(i, j, datumArray[j] == datumArray[i]);
-                        }
+            for (int i = 0; i < NUM_DATA; ++i) {
+                for (int j = 0; j < NUM_DATA; ++j) {
+                    const int LINE_I = DATA[i].d_line;
+                    const int LINE_J = DATA[j].d_line;
+                    Obj x(DATA[i].d_datum, &ta); const Obj& X = x;
+                    Obj y(DATA[j].d_datum, &ta); const Obj& Y = y;
+                    if (i != j ||
+                        (X->isDouble() && X->theDouble() != X->theDouble())) {
+                        // Treat NaNs specially.
+                        ASSERTV(LINE_I, LINE_J, X, Y, !(X == Y));
+                        ASSERTV(LINE_I, LINE_J, X, Y, X != Y);
                     }
                     else {
-                        ASSERTV(i, j, datumArray[i] != datumArray[j]);
-                        ASSERTV(i, j, datumArray[j] != datumArray[i]);
+                        ASSERTV(LINE_I, LINE_J, X, Y, (X == Y));
+                        ASSERTV(LINE_I, LINE_J, X, Y, !(X != Y));
                     }
+                    x.release();
+                    y.release();
                 }
+            }            
+
+            for (int i = 0; i < NUM_DATA; ++i) {
+                // Relase the memory allocated in 'DATA'.
+
+                Datum::destroy(DATA[i].d_datum, &ta);
             }
         }
+
         if (verbose) cout << "\nTesting operators format." << endl;
         {
             typedef bool (*operatorPtr)(const Obj&, const Obj&);
