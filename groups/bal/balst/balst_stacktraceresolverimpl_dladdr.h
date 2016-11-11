d3a57a8a60b4cb2cb6338bd8e52d7c9c4c8d2ed6
Bill Chapman <bchapman2@bloomberg.net>
2016-11-11 13:40:20 -0500

    Adopt commit e989766 from master: Merge pull request #731 from bchapman/balst_stacktraceresolverimpl_dladdr-improve-demangling-drqs-89143156
    commit e9897661568b63fdc2a4f84df09736a257108f55
    Merge: 7f0e8e0 b7698ab
    Author: Henry Verschell <hverschell@bloomberg.net>
    Date:   Fri Nov 11 13:40:20 2016 -0500
    
        Merge pull request #731 from bchapman/balst_stacktraceresolverimpl_dladdr-improve-demangling-drqs-89143156
    
        balst_stacktraceresolverimpl_dladdr -- improved demangling, no symbol…
    
    commit b7698ab9488f02c3e78b6551b70a10aa984109d0
    Author: Bill Chapman <bchapman2@bloomberg.net>
    Date:   Fri Sep 16 15:39:47 2016 -0400
    
        balst_stacktraceresolverimpl_dladdr -- improved demangling, no symbol size limit

--- groups/bal/balst/balst_stacktraceresolverimpl_dladdr.h
+++ groups/bal/balst/balst_stacktraceresolverimpl_dladdr.h
@@ -64,10 +64,6 @@ BSLS_IDENT("$Id: $")
 #include <balst_stacktraceframe.h>
 #endif
 
-#ifndef INCLUDED_BDLMA_HEAPBYPASSALLOCATOR
-#include <bdlma_heapbypassallocator.h>
-#endif
-
 #ifndef INCLUDED_BSLS_TYPES
 #include <bsls_types.h>
 #endif
@@ -105,14 +101,7 @@ class StackTraceResolverImpl<ObjectFileFormat::Dladdr> {
                                             // as many other fields of them as
                                             // possible.
 
-    char              *d_demangleBuf_p;     // scratch space for demangling,
-                                            // length is 'DEMANGLING_BUF_LEN'
-                                            // in the imp file.
-
     bool               d_demangleFlag;      // whether we demangle names
-
-    bdlma::HeapBypassAllocator
-                       d_hbpAlloc;          // heap bypass allocator -- owned
 
   private:
     // NOT IMPLEMENTED
