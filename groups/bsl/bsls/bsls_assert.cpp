ff4fc78a6b3c2d4371fb1a72e0a415771ac7375d
Frank Birbacher <fbirbacher@bloomberg.net>
2017-03-23 19:19:19 +0000

    Use [[noreturn]] for bsls assert if supported.
    Signed-off-by: Frank Birbacher <fbirbacher@bloomberg.net>

--- groups/bsl/bsls/bsls_assert.cpp
+++ groups/bsl/bsls/bsls_assert.cpp
@@ -39,11 +39,9 @@ extern "C" {
 #error BSLS_ASSERT_NORETURN must be a macro scoped locally to this file
 #endif
 
-// Note that a portable syntax for 'noreturn' will be available once we have
-// access to conforming C++0x compilers.
-//# define BSLS_ASSERT_NORETURN [[noreturn]]
-
-#ifdef BSLS_PLATFORM_CMP_MSVC
+#if defined(BSLS_COMPILERFEATURES_SUPPORT_ATTRIBUTE_NORETURN)
+#   define BSLS_ASSERT_NORETURN [[noreturn]]
+#elif defined(BSLS_PLATFORM_CMP_MSVC)
 #   define BSLS_ASSERT_NORETURN __declspec(noreturn)
 #else
 #   define BSLS_ASSERT_NORETURN
