2d47472d890ce63bb3477931ca6fe97d57d21c62
Jeffrey Mendelsohn <JMENDELSOHN4@bloomberg.net>
2017-04-26 13:09:50 -0400

    fix datetime and time test compilation errors

--- groups/bdl/bdlt/bdlt_datetime.t.cpp
+++ groups/bdl/bdlt/bdlt_datetime.t.cpp
@@ -297,10 +297,9 @@ const int DEFAULT_NUM_DATA =
 
 static int s_countingLogMessageHandlerCount = 0;
 
-static void countingLogMessageHandler(bsls::LogSeverity::Enum,
-                                      const char              *,
+static void countingLogMessageHandler(const char *,
                                       const int,
-                                      const char              *)
+                                      const char *)
     // Increment 's_countingLogMessageHandlerCount'.
 {
     ++s_countingLogMessageHandlerCount;
