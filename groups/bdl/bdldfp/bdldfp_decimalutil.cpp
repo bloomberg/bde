beb834c6b3fcc1d2a90bd086599147da6d868ad6
Denis Sokolov <dsokolov7@bloomberg.net>
2017-05-26 20:41:03 +0400

    Initial draft of 'hashAppend' for Decimal32.

--- groups/bdl/bdldfp/bdldfp_decimalutil.cpp
+++ groups/bdl/bdldfp/bdldfp_decimalutil.cpp
@@ -287,99 +287,19 @@ BDLDFP_DISABLE_COMPILE; // Unsupported platform
 }
                         // classification functions
 
-#ifdef BDLDFP_DECIMALPLATFORM_DECNUMBER
-static int canonicalizeDecimalValueClassification(int classification)
-    // Return a standard mandated constant indicating the kind of floating
-    // point value specified by 'classification'.  The behavior is undefined
-    // unless 'classification' is a valid classification code for the
-    // underlying implementation.  Note that 'classification' is of an
-    // implementation defined type, and corresponds to specific underlying
-    // library constants.
-{
-    enum decClass cl = static_cast<decClass>(classification);
-    switch (cl) {
-    case DEC_CLASS_SNAN:
-    case DEC_CLASS_QNAN:          return FP_NAN;                      // RETURN
-    case DEC_CLASS_NEG_INF:
-    case DEC_CLASS_POS_INF:       return FP_INFINITE;                 // RETURN
-    case DEC_CLASS_NEG_ZERO:
-    case DEC_CLASS_POS_ZERO:      return FP_ZERO;                     // RETURN
-    case DEC_CLASS_NEG_NORMAL:
-    case DEC_CLASS_POS_NORMAL:    return FP_NORMAL;                   // RETURN
-    case DEC_CLASS_NEG_SUBNORMAL:
-    case DEC_CLASS_POS_SUBNORMAL: return FP_SUBNORMAL;                // RETURN
-    }
-    BSLS_ASSERT(!"Unknown decClass");
-    return -1;
-}
-#elif defined(BDLDFP_DECIMALPLATFORM_INTELDFP)
-static int canonicalizeDecimalValueClassification(int classification)
-    // Return a standard mandated constant indicating the kind of floating
-    // point value specified by 'classification'.  The behavior is undefined
-    // unless 'classification' is a valid classification code for the
-    // underlying implementation.  Note that 'classification' is of an
-    // implementation defined type, and corresponds to specific underlying
-    // library constants.
-{
-    enum class_types cl = static_cast<class_types>(classification);
-    switch (cl) {
-    case signalingNaN:
-    case quietNaN:          return FP_NAN;                      // RETURN
-    case negativeInfinity:
-    case positiveInfinity:  return FP_INFINITE;                 // RETURN
-    case negativeZero:
-    case positiveZero:      return FP_ZERO;                     // RETURN
-    case negativeNormal:
-    case positiveNormal:    return FP_NORMAL;                   // RETURN
-    case negativeSubnormal:
-    case positiveSubnormal: return FP_SUBNORMAL;                // RETURN
-    }
-    BSLS_ASSERT(!"Unknown decClass");
-    return -1;
-}
-#endif
-
 int DecimalUtil::classify(Decimal32 x)
 {
-#ifdef BDLDFP_DECIMALPLATFORM_C99_TR
-    return fpclassify(x.value());
-#elif defined(BDLDFP_DECIMALPLATFORM_DECNUMBER)
-    enum decClass cl = decSingleClass(x.data());
-    return canonicalizeDecimalValueClassification(cl);
-#elif defined(BDLDFP_DECIMALPLATFORM_INTELDFP)
-    return canonicalizeDecimalValueClassification(
-                                               __bid32_class(x.data()->d_raw));
-#else
-BDLDFP_DISABLE_COMPILE; // Unsupported platform
-#endif
+    return DecimalImpUtil::classify(x.value());
 }
+
 int DecimalUtil::classify(Decimal64 x)
 {
-#ifdef BDLDFP_DECIMALPLATFORM_C99_TR
-    return fpclassify(x.value());
-#elif defined(BDLDFP_DECIMALPLATFORM_DECNUMBER)
-    enum decClass cl = decDoubleClass(x.data());
-    return canonicalizeDecimalValueClassification(cl);
-#elif defined(BDLDFP_DECIMALPLATFORM_INTELDFP)
-    return canonicalizeDecimalValueClassification(
-                                               __bid64_class(x.data()->d_raw));
-#else
-BDLDFP_DISABLE_COMPILE; // Unsupported platform
-#endif
+    return DecimalImpUtil::classify(x.value());
 }
+
 int DecimalUtil::classify(Decimal128 x)
 {
-#ifdef BDLDFP_DECIMALPLATFORM_C99_TR
-    return fpclassify(x.value());
-#elif defined(BDLDFP_DECIMALPLATFORM_DECNUMBER)
-    enum decClass cl = decQuadClass(x.data());
-    return canonicalizeDecimalValueClassification(cl);
-#elif defined(BDLDFP_DECIMALPLATFORM_INTELDFP)
-    return canonicalizeDecimalValueClassification(
-                                              __bid128_class(x.data()->d_raw));
-#else
-BDLDFP_DISABLE_COMPILE; // Unsupported platform
-#endif
+return DecimalImpUtil::classify(x.value());
 }
 
 bool DecimalUtil::isFinite(Decimal32 x)
@@ -876,58 +796,10 @@ int DecimalUtil::decompose(int          *sign,
                            int          *exponent,
                            Decimal32     value)
 {
-    BSLS_ASSERT(sign);
-    BSLS_ASSERT(significand);
-    BSLS_ASSERT(exponent);
-
-    enum {
-        k_SIGN_MASK             = 0x80000000ul,
-        k_SPECIAL_ENCODING_MASK = 0x60000000ul,
-        k_INFINITY_MASK         = 0x78000000ul,
-        k_SMALL_COEFF_MASK      = 0x001ffffful,
-        k_LARGE_COEFF_MASK      = 0x007ffffful,
-        k_LARGE_COEFF_HIGH_BIT  = 0x00800000ul,
-        k_EXPONENT_MASK         = 0xff,
-        k_EXPONENT_SHIFT_LARGE  = 21,
-        k_EXPONENT_SHIFT_SMALL  = 23,
-        k_DECIMAL_EXPONENT_BIAS = 101,
-    };
-
-#ifdef BDLDFP_DECIMALPLATFORM_INTELDFP
-    unsigned int x = value.data()->d_raw;
-#else
-    unsigned int x = *value.data()->words;
-#endif
-
-    unsigned int tmp;
-    int cl = classify(value);
-
-    *sign = (x & k_SIGN_MASK) ? -1 : 1;
-
-    if ((x & k_SPECIAL_ENCODING_MASK) == k_SPECIAL_ENCODING_MASK) {
-        // special encodings
-        if ((x & k_INFINITY_MASK) == k_INFINITY_MASK) {
-            // NaN or Infinity
-            *significand = (x & k_SMALL_COEFF_MASK) | k_LARGE_COEFF_HIGH_BIT;
-            tmp = x >> k_EXPONENT_SHIFT_LARGE;
-            *exponent = tmp & k_EXPONENT_MASK;
-            return cl;
-        }
-        // get significand
-        *significand = (x & k_SMALL_COEFF_MASK) | k_LARGE_COEFF_HIGH_BIT;
-
-        // get exponent
-        tmp = x >> k_EXPONENT_SHIFT_LARGE;
-        *exponent = (tmp & k_EXPONENT_MASK) - k_DECIMAL_EXPONENT_BIAS;
-        return cl;
-    }
-    // get exponent
-    tmp = x >> k_EXPONENT_SHIFT_SMALL;
-    *exponent = (tmp & k_EXPONENT_MASK) - k_DECIMAL_EXPONENT_BIAS;
-    // get coefficient
-    *significand = (x & k_LARGE_COEFF_MASK);
-
-    return cl;
+    return  DecimalImpUtil::decompose(sign,
+                                      significand,
+                                      exponent,
+                                      value.value());
 }
 
 int DecimalUtil::decompose(int                 *sign,
@@ -935,56 +807,10 @@ int DecimalUtil::decompose(int                 *sign,
                            int                 *exponent,
                            Decimal64            value)
 {
-    BSLS_ASSERT(sign);
-    BSLS_ASSERT(significand);
-    BSLS_ASSERT(exponent);
-
-    const bsls::Types::Uint64 k_SIGN_MASK             = 0x8000000000000000ull;
-    const bsls::Types::Uint64 k_SPECIAL_ENCODING_MASK = 0x6000000000000000ull;
-    const bsls::Types::Uint64 k_INFINITY_MASK         = 0x7800000000000000ull;
-    const bsls::Types::Uint64 k_SMALL_COEFF_MASK      = 0x0007ffffffffffffull;
-    const bsls::Types::Uint64 k_LARGE_COEFF_MASK      = 0x001fffffffffffffull;
-    const bsls::Types::Uint64 k_LARGE_COEFF_HIGH_BIT  = 0x0020000000000000ull;
-    const bsls::Types::Uint64 k_EXPONENT_MASK         = 0x3ff;
-    const int k_EXPONENT_SHIFT_LARGE                  = 51;
-    const int k_EXPONENT_SHIFT_SMALL                  = 53;
-    const int k_DECIMAL_EXPONENT_BIAS                 = 398;
-
-#ifdef BDLDFP_DECIMALPLATFORM_INTELDFP
-    bsls::Types::Uint64 x = value.data()->d_raw;
-#else
-    bsls::Types::Uint64 x = *value.data()->longs;
-#endif
-
-    bsls::Types::Uint64 tmp;
-    int cl = classify(value);
-
-    *sign = (x & k_SIGN_MASK) ? -1 : 1;
-
-    if ((x & k_SPECIAL_ENCODING_MASK) == k_SPECIAL_ENCODING_MASK) {
-        // special encodings
-        if ((x & k_INFINITY_MASK) == k_INFINITY_MASK) {
-            // NaN or Infinity
-            *significand = (x & k_SMALL_COEFF_MASK) | k_LARGE_COEFF_HIGH_BIT;
-            tmp = x >> k_EXPONENT_SHIFT_LARGE;
-            *exponent = static_cast<int>(tmp & k_EXPONENT_MASK);
-            return cl;
-        }
-        *significand = (x & k_SMALL_COEFF_MASK) | k_LARGE_COEFF_HIGH_BIT;
-        // get exponent
-        tmp = x >> k_EXPONENT_SHIFT_LARGE;
-        *exponent = static_cast<int>(tmp & k_EXPONENT_MASK)
-                    - k_DECIMAL_EXPONENT_BIAS;
-        return cl;
-    }
-    // exponent
-    tmp = x >> k_EXPONENT_SHIFT_SMALL;
-    *exponent = static_cast<int>(tmp & k_EXPONENT_MASK)
-                - k_DECIMAL_EXPONENT_BIAS;
-    // coefficient
-    *significand = x & k_LARGE_COEFF_MASK;
-
-    return cl;
+    return  DecimalImpUtil::decompose(sign,
+                                      significand,
+                                      exponent,
+                                      value.value());
 }
 
 int DecimalUtil::decompose(int                 *sign,
@@ -992,61 +818,11 @@ int DecimalUtil::decompose(int                 *sign,
                            int                 *exponent,
                            Decimal128           value)
 {
-    const bsls::Types::Uint64 k_SIGN_MASK             = 0x8000000000000000ull;
-    const bsls::Types::Uint64 k_SPECIAL_ENCODING_MASK = 0x6000000000000000ull;
-    const bsls::Types::Uint64 k_SMALL_COEFF_MASK      = 0x00007fffffffffffull;
-    const bsls::Types::Uint64 k_LARGE_COEFF_MASK      = 0x0001ffffffffffffull;
-    const bsls::Types::Uint64 k_LARGE_COEFF_HIGH_BIT  = 0x0020000000000000ull;
-    const bsls::Types::Uint64 k_EXPONENT_MASK         = 0x3fff;
-    const int k_EXPONENT_SHIFT_LARGE                  = 47;
-    const int k_EXPONENT_SHIFT_SMALL                  = 49;
-    const int k_DECIMAL_EXPONENT_BIAS                 = 6176;
+    return  DecimalImpUtil::decompose(sign,
+                                      significand,
+                                      exponent,
+                                      value.value());
 
-    BSLS_ASSERT(sign);
-    BSLS_ASSERT(significand);
-    BSLS_ASSERT(exponent);
-
-#ifdef BDLDFP_DECIMALPLATFORM_INTELDFP
-    #ifdef BSLS_PLATFORM_IS_BIG_ENDIAN
-    bsls::Types::Uint64 xH = value.data()->d_raw.w[0];
-    bsls::Types::Uint64 xL = value.data()->d_raw.w[1];
-    #elif defined(BSLS_PLATFORM_IS_LITTLE_ENDIAN)
-    bsls::Types::Uint64 xL = value.data()->d_raw.w[0];
-    bsls::Types::Uint64 xH = value.data()->d_raw.w[1];
-    #endif
-#else
-    #ifdef BSLS_PLATFORM_IS_BIG_ENDIAN
-    bsls::Types::Uint64 xH = *value.data()->longs[0];
-    bsls::Types::Uint64 xL = *value.data()->longs[1];
-    #elif defined(BSLS_PLATFORM_IS_LITTLE_ENDIAN)
-    bsls::Types::Uint64 xL = *value.data()->longs[0];
-    bsls::Types::Uint64 xH = *value.data()->longs[1];
-    #endif
-#endif
-
-    bsls::Types::Uint64 tmp;
-    int cl = classify(value);
-
-   *sign = (xH & k_SIGN_MASK) ? -1 : 1;
-
-    if ((xH & k_SPECIAL_ENCODING_MASK) == k_SPECIAL_ENCODING_MASK) {
-        // special encodings
-        significand->setHigh((xH & k_SMALL_COEFF_MASK)
-                             | k_LARGE_COEFF_HIGH_BIT);
-        significand->setLow(xL);
-        tmp = xH >> k_EXPONENT_SHIFT_LARGE;
-        *exponent = static_cast<int>(tmp & k_EXPONENT_MASK);
-        return cl; // NaN or Infinity
-    }
-
-    tmp = xH >> k_EXPONENT_SHIFT_SMALL;
-    *exponent = static_cast<int>(tmp & k_EXPONENT_MASK)
-                - k_DECIMAL_EXPONENT_BIAS;
-    // coefficient
-    significand->setHigh(xH & k_LARGE_COEFF_MASK);
-    significand->setLow(xL);
-
-    return cl;
 }
 
 }  // close package namespace
