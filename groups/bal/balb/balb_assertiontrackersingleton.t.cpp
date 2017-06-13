2f40d7b69c703ccfa2f38bd1f358ed0d5ff0a594
Hyman Rosen <hrosen4@bloomberg.net>
2017-06-13 16:03:57 -0400

    Add some testing.

--- groups/bal/balb/balb_assertiontrackersingleton.t.cpp
+++ groups/bal/balb/balb_assertiontrackersingleton.t.cpp
@@ -3,8 +3,12 @@
 #include <balb_assertiontrackersingleton.h>
 #include <balb_assertiontracker.h>
 
+#include <bdlf_bind.h>
+#include <bdlf_placeholder.h>
+
 #include <bslim_testutil.h>
 
+#include <bslma_defaultallocatorguard.h>
 #include <bslma_testallocator.h>
 
 #include <bsl_cstdlib.h>
@@ -28,7 +32,7 @@ using namespace bsl;  // automatically added by script
 // ACCESSORS
 // ----------------------------------------------------------------------------
 // [ 1] BREATHING TEST
-// USAGE EXAMPLE
+// [ 2] USAGE EXAMPLE
 
 // ============================================================================
 //                     STANDARD BDE ASSERT TEST FUNCTION
@@ -80,6 +84,57 @@ void aSsErT(bool condition, const char *message, int line)
 
 typedef balb::AssertionTrackerSingleton<balb::AssertionTracker> Obj;
 
+///Usage
+///-----
+// This section illustrates intended use of this component.
+//
+///Example 1: Count Number of Assertions Triggered
+///- - - - - - - - - - - - - - - - - - - - - - - -
+// We wish to count how many times assertions trigger, doing no other handling
+// and allowing the program to continue running (note that this is normally
+// forbidden in production).  We can use 'AssertionTrackerSingleton' to set up
+// an assertion handler to do this.
+//
+// First, we create a class to do the counting.
+//..
+    class AssertionCounter {
+        // PRIVATE DATA
+        int d_assertion_counter;  // Number of assertions seen.
+
+      public:
+        // PUBLIC CREATORS
+        explicit AssertionCounter(bsls::Assert::Handler = 0, void * = 0);
+            // Create an 'AssertionCounter' object.  We ignore the fallback
+            // handler and optional allocator parameters.
+
+        // PUBLIC MANIPULATORS
+        void operator()(const char *, const char *, int);
+            // Function called when assertion failure occurs.  We ignore the
+            // text, file, and line parameters.
+
+        // PUBLIC ACCESSORS
+        int getAssertionCount() const;
+            // Return the number of assertions we have seen.
+    };
+//..
+// Then, we implement the member functions of the 'AssertionCounter' class.
+//..
+    AssertionCounter::AssertionCounter(bsls::Assert::Handler, void *)
+    : d_assertion_counter(0)
+    {
+    }
+
+    void AssertionCounter::operator()(const char *, const char *, int)
+    {
+        ++d_assertion_counter;
+    }
+
+    int AssertionCounter::getAssertionCount() const
+    {
+        return d_assertion_counter;
+    }
+//..
+
 // ============================================================================
 //                               MAIN PROGRAM
 // ----------------------------------------------------------------------------
@@ -93,19 +148,59 @@ int main(int argc, char *argv[])
 
     cout << "TEST " << __FILE__ << " CASE " << test << endl;
 
-    bslma::TestAllocator globalAllocator("global", veryVeryVeryVerbose);
-    bslma::Default::setGlobalAllocator(&globalAllocator);
-
     switch (test) { case 0:
+      case 2: {
+        // --------------------------------------------------------------------
+        // USAGE EXAMPLE
+        //   Extracted from component header file.
+        //
+        // Concerns:
+        //: 1 The usage example provided in the component header file compiles,
+        //:   links, and runs as shown.
+        //
+        // Plan:
+        //: 1 Incorporate usage example from header into test driver, remove
+        //:   leading comment characters, and replace 'assert' with 'ASSERT'.
+        //:   (C-1)
+        //
+        // Testing:
+        //   USAGE EXAMPLE
+        // --------------------------------------------------------------------
+
+        if (verbose) cout << "\nUSAGE EXAMPLE"
+                             "\n=============\n";
+// Next, we set up an instance of this class to be the installed assertion
+// using 'AssertionTrackerSingleton'.  Note that this needs to be done early in
+// 'main()' before any other handlers are installed.  If another handler has
+// already been installed, a null pointer will be returned, and we assert that
+// this does not happen.
+//..
+    AssertionCounter *ac_p =
+        balb::AssertionTrackerSingleton<AssertionCounter>::singleton();
+    ASSERT(ac_p);
+//..
+// Finally, we will trigger some assertions and verify that we are counting
+// them correctly.
+//..
+    BSLS_ASSERT(0 && "assertion 1");
+    BSLS_ASSERT(0 && "assertion 2");
+    ASSERT(ac_p->getAssertionCount() == 2);
+//..
+      } break;
       case 1: {
         // --------------------------------------------------------------------
         // BREATHING TEST
+        //   This case exercises (but does not fully test) basic functionality.
         //
         // Concerns:
-        //: 1 A concern.
+        //: 1 The class is sufficiently functional to install an assertion
+        //:   handler and process assertions.
         //
         // Plan:
-        //: 1 A plan.
+        //: 1 Create a singleton 'AssertionTracker' object, trigger some
+        //:   assertions, and verify that they are noticed by having the
+        //:   assertion handler write them to a string stream and then
+        //:   examining the contents of the stream.
         //
         // Testing:
         //   BREATHING TEST
@@ -115,14 +210,59 @@ int main(int argc, char *argv[])
             cout << "BREATHING TEST\n"
                     "==============\n";
         {
-            ASSERT(Obj::singleton());
+            bsl::ostringstream            os;
+            balb::AssertionTracker       *at_p = Obj::singleton();
+
+            ASSERT(at_p);
+            at_p->callback(
+                bdlf::BindUtil::bind(&balb::AssertionTracker::reportAssertion,
+                                     &os,
+                                     bdlf::PlaceHolders::_1,
+                                     bdlf::PlaceHolders::_2,
+                                     bdlf::PlaceHolders::_3,
+                                     bdlf::PlaceHolders::_4,
+                                     bdlf::PlaceHolders::_5));
+            os << "\n";
             for (int i = 0; i < 10; ++i) {
                 BSLS_ASSERT(0 && "assert 1");
                 for (int j = 0; j < 10; ++j) {
                     BSLS_ASSERT(0 && "assert 2");
                 }
             }
-            Obj::singleton()->iterateAll();
+
+            bsl::string s = os.str();
+
+            bsl::string::size_type p0 = s.find(__FILE__);
+            ASSERTV(s, s.npos != p0);
+
+            bsl::string::size_type p1 = s.find("\n1 ");
+            ASSERTV(s, s.npos != p1);
+            bsl::string::size_type p2 = s.find("0 && \"assert 1\"");
+            ASSERTV(s, s.npos != p2);
+            ASSERTV(s, p1 < p2);
+
+            bsl::string::size_type p3 = s.find("\n1 ");
+            ASSERTV(s, s.npos != p3);
+            bsl::string::size_type p4 = s.find("0 && \"assert 2\"");
+            ASSERTV(s, s.npos != p4);
+            ASSERTV(s, p3 < p4);
+
+            os.str("");
+            os << "\n";
+            at_p->iterateAll();
+            s = os.str();
+
+            bsl::string::size_type p5 = s.find("\n10 ");
+            ASSERTV(s, s.npos != p5);
+            bsl::string::size_type p6 = s.find("0 && \"assert 1\"");
+            ASSERTV(s, s.npos != p6);
+            ASSERTV(s, p5 < p6);
+
+            bsl::string::size_type p7 = s.find("\n100 ");
+            ASSERTV(s, s.npos != p7);
+            bsl::string::size_type p8 = s.find("0 && \"assert 2\"");
+            ASSERTV(s, s.npos != p8);
+            ASSERTV(s, p7 < p8);
         }
       } break;
       default: {
@@ -130,9 +270,6 @@ int main(int argc, char *argv[])
         testStatus = -1;
       }
     }
-
-    LOOP_ASSERT(globalAllocator.numBlocksTotal(),
-                0 == globalAllocator.numBlocksTotal());
 
     if (testStatus > 0) {
         cerr << "Error, non-zero test status = " << testStatus << "." << endl;
