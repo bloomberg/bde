073e14c5d968401a81b05537260eb70f4707e3c7
Hyman Rosen <hrosen4@bloomberg.net>
2017-06-14 16:16:29 -0400

    More test driver additions.

--- groups/bal/balb/balb_assertiontracker.t.cpp
+++ groups/bal/balb/balb_assertiontracker.t.cpp
@@ -36,7 +36,25 @@ using namespace bsl;  // automatically added by script
 // ACCESSORS
 // ----------------------------------------------------------------------------
 // [ 1] BREATHING TEST
-// USAGE EXAMPLE
+// [ 2] AssertionTracker(bsls::Assert::Handler, bslma::Allocator *);
+// [ 2] bslma::Allocator *allocator() const;
+// [ 2] void callback(Callback cb);
+// [ 2] void operator()(const char *text, const char *file, int line);
+// [ 2] void maxAssertions(int value);
+// [ 2] void maxLocations(int value);
+// [ 2] void maxStackTracesPerLocation(int value);
+// [ 2] void onEachAssertion(bool value);
+// [ 2] void onNewLocation(bool value);
+// [ 2] void onNewStackTrace(bool value);
+// [ 2] Callback callback() const;
+// [ 2] int maxAssertions() const;
+// [ 2] int maxLocations() const;
+// [ 2] int maxStackTracesPerLocation() const;
+// [ 2] bool onEachAssertion() const;
+// [ 2] bool onNewLocation() const;
+// [ 2] bool onNewStackTrace() const;
+// [ 3] void reportAssertion(...);
+// [ 3] void iterateAll() const;
 
 // ============================================================================
 //                     STANDARD BDE ASSERT TEST FUNCTION
@@ -88,6 +106,38 @@ void aSsErT(bool condition, const char *message, int line)
 
 typedef balb::AssertionTracker Obj;
 
+int defaultAssertionCount;
+void defaultHandler(const char *, const char *, int)
+    // Function to be installed as the default handler.
+{
+    ++defaultAssertionCount;
+}
+
+int handledAssertionCount;
+void reporter(int, const char *, const char *, int, const bsl::vector<void *>&)
+    // Function to be installed as the assertion tracker reporter.
+{
+    ++handledAssertionCount;
+}
+
+void trigger1(Obj& t, const char *a, const char *f, int l)
+    // Invoke the specified 't' with the specified 'a', 'f', and 'l';
+{
+    t(a, f, l);
+}
+
+void trigger2(Obj& t, const char *a, const char *f, int l)
+    // Invoke 'trigger1' with the specified 't', 'a', 'f', and 'l';
+{
+    trigger1(t, a, f, l);
+}
+
+void trigger3(Obj& t, const char *a, const char *f, int l)
+    // Invoke 'trigger1' with the specified 't', 'a', 'f', and 'l';
+{
+    trigger1(t, a, f, l);
+}
+
 // ============================================================================
 //                               MAIN PROGRAM
 // ----------------------------------------------------------------------------
@@ -104,23 +154,272 @@ int main(int argc, char *argv[])
     bslma::TestAllocator ta("test", veryVeryVeryVerbose);
 
     switch (test) { case 0:
+      case 3: {
+        // --------------------------------------------------------------------
+        // REPORTER TEST
+        //   This case tests the default reporting function of the assertion
+        //   tracker and its 'iterateAll' method.
+        //
+        // Concerns:
+        //: 1 Assertions are correctly printed to the stream passed to the
+        //:   reporting function.
+        //:
+        //: 2 The 'iterateAll' method sends correct counts.
+        //
+        // Plan:
+        //: 1 Use an 'ostringstream' as the reporting stream and examine its
+        //:   contents for correct usage.
+        //:
+        //: 2 Call 'iterateAll' and examine the contents of the stream.
+        //
+        // Testing:
+        //   void reportAssertion(...);
+        //   void iterateAll() const;
+        // --------------------------------------------------------------------
+        if (verbose) cout << "\nREPORTER TEST"
+                             "\n=============\n";
+
+        bsl::ostringstream os;
+
+        Obj Z(bsls::Assert::failureHandler(), &ta);
+
+        Z.callback(bdlf::BindUtil::bind(&Obj::reportAssertion,
+                                        &os,
+                                        bdlf::PlaceHolders::_1,
+                                        bdlf::PlaceHolders::_2,
+                                        bdlf::PlaceHolders::_3,
+                                        bdlf::PlaceHolders::_4,
+                                        bdlf::PlaceHolders::_5));
+
+
+        os << "\n";
+        for (int i = 0; i < 10; ++i) {
+            Z("0 && \"assert 1\"", __FILE__, __LINE__);
+            for (int j = 0; j < 10; ++j) {
+                Z("0 && \"assert 2\"", __FILE__, __LINE__);
+            }
+        }
+
+        bsl::string::size_type p1, p2;
+
+        bsl::string s = os.str();
+
+        p1 = s.find(__FILE__);
+        ASSERTV(s, s.npos != p1);
+
+        p1 = s.find("\n1 ");
+        ASSERTV(s, s.npos != p1);
+        p2 = s.find("0 && \"assert 1\"", p1);
+        ASSERTV(s, s.npos != p2);
+
+        p1 = s.find("\n1 ", p2);
+        ASSERTV(s, s.npos != p1);
+        p2 = s.find("0 && \"assert 2\"", p1);
+        ASSERTV(s, s.npos != p2);
+
+        os.str("");
+        os << "\n";
+        Z.iterateAll();
+        s = os.str();
+
+        p1 = s.find("\n10 ");
+        ASSERTV(s, s.npos != p1);
+        p2 = s.find("0 && \"assert 1\"", p1);
+        ASSERTV(s, s.npos != p2);
+
+        p1 = s.find("\n100 ");
+        ASSERTV(s, s.npos != p1);
+        p2 = s.find("0 && \"assert 2\"", p1);
+        ASSERTV(s, s.npos != p2);
+      } break;
+      case 2: {
+        // --------------------------------------------------------------------
+        // INVOCATION TEST
+        //   This case tests that the assertion tracker invokes its callback
+        //   and fallback handlers the correct number of times, including when
+        //   various limits are exceeded, and obeys the parameters describing
+        //   when the handlers are called.
+        //
+        // Concerns:
+        //: 1 The tracker will invoke the fallback handler when a limit on the
+        //:   number of assertions is set and the limit is exceeded.
+        //:
+        //: 2 The tracker will invoke the callback for each assertion not over
+        //:   the limit when that is enabled.
+        //:
+        //: 3 The tracker will invoke the fallback handler when a limit on the
+        //:   number of locations is set and the limit is exceeded.
+        //:
+        //: 4 The tracker will invoke the callback for each new assertion
+        //:   location not over the limit when that is enabled.
+        //:
+        //: 5 The tracker will invoke the fallback handler when a limit on the
+        //:   number of stack traces per location is set and the limit is
+        //:   exceeded.
+        //:
+        //: 6 The tracker will invoke the callback for each new assertion stack
+        //:   not over the limit when that is enabled.
+        //:
+        //: 7 The value-semantic aspects of the tracker work; the attributes
+        //:   that represent the callback, reporting, and limits can be set and
+        //:   retrieved correctly.
+        //
+        // Plan:
+        //: 1 Create a table-based test where each table entry describes the
+        //:   reporting modes that are enabled, the limits to be observed, an
+        //:   array with counts specifying how many times each of 3 assertions
+        //:   should be triggered from 3 different stack traces, and finally
+        //:   the number of times each we expect the callback and the fallback
+        //:   handler to be invoked.  Set up callback and fallback handlers
+        //:   that count calls to themselves, set up the assertion tracker and
+        //:   trigger1 the assertions based on the counts, and verify that the
+        //:   number of calls match.  (C-1..6)
+        //:
+        //: 2 When setting attributes on the tracker, read them back and verify
+        //:   that they are returned as they were set.  (C-7)
+        //
+        // Testing:
+        //   AssertionTracker(bsls::Assert::Handler, bslma::Allocator *);
+        //   bslma::Allocator *allocator() const;
+        //   void callback(Callback cb);
+        //   void operator()(const char *text, const char *file, int line);
+        //   void maxAssertions(int value);
+        //   void maxLocations(int value);
+        //   void maxStackTracesPerLocation(int value);
+        //   void onEachAssertion(bool value);
+        //   void onNewLocation(bool value);
+        //   void onNewStackTrace(bool value);
+        //   Callback callback() const;
+        //   int maxAssertions() const;
+        //   int maxLocations() const;
+        //   int maxStackTracesPerLocation() const;
+        //   bool onEachAssertion() const;
+        //   bool onNewLocation() const;
+        //   bool onNewStackTrace() const;
+        // --------------------------------------------------------------------
+
+        if (verbose) cout << "\nINVOCATION TEST"
+                             "\n===============\n";
+        {
+            static const struct {
+                int  d_line;                       // line number
+                bool d_onEachAssertion;            // report each assertion
+                bool d_onNewLocation;              // report each new location
+                bool d_onNewStackTrace;            // report each new stack
+                int  d_maxAssertions;              // max assertions handled
+                int  d_maxLocations;               // max locations handled
+                int  d_maxStackTracesPerLocation;  // max stacks handled
+                int  d_assertion_location[3][3];   // assertions to trigger1
+                int  d_expected_handled;
+                int  d_expected_default;
+            } DATA[] = {
+                {L_, 1, 0, 0, -1, -1, -1, {1, 1, 1, 1, 1, 1, 1, 1, 1},  9,  0},
+                {L_, 0, 1, 0, -1, -1, -1, {2, 2, 2, 2, 2, 2, 2, 2, 2},  3,  0},
+                {L_, 0, 0, 1, -1, -1, -1, {2, 2, 2, 2, 2, 2, 2, 2, 2},  9,  0},
+                {L_, 1, 1, 1,  0,  0,  0, {1, 1, 1, 1, 1, 1, 1, 1, 1},  0,  9},
+            };
+            const int NUM_DATA = sizeof DATA / sizeof *DATA;
+            for (int ti = 0; ti < NUM_DATA; ++ti) {
+                const int  LINE      = DATA[ti].d_line;
+                const bool EACH      = DATA[ti].d_onEachAssertion;
+                const bool LOC       = DATA[ti].d_onNewLocation;
+                const bool STACK     = DATA[ti].d_onNewStackTrace;
+                const int  MAX_A     = DATA[ti].d_maxAssertions;
+                const int  MAX_L     = DATA[ti].d_maxLocations;
+                const int  MAX_S     = DATA[ti].d_maxStackTracesPerLocation;
+                const int  EXP_H     = DATA[ti].d_expected_handled;
+                const int  EXP_D     = DATA[ti].d_expected_default;
+                const int(&AL)[3][3] = DATA[ti].d_assertion_location;
+
+                if (veryVerbose) {
+                    cout << LINE << " "
+                         << &"!Each "    [EACH]
+                         << &"!NewLoc "  [LOC]
+                         << &"!NewStack "[STACK]
+                         << "MAXA " << MAX_A << " "
+                         << "MAXL " << MAX_L << " "
+                         << "MAXS " << MAX_S << "\n"
+                         << "CALLS"
+                         << " " << AL[0][0]
+                         << " " << AL[0][1]
+                         << " " << AL[0][2]
+                         << " " << AL[1][0]
+                         << " " << AL[1][1]
+                         << " " << AL[1][2]
+                         << " " << AL[2][0]
+                         << " " << AL[2][1]
+                         << " " << AL[2][2]
+                         << "\n"
+                         << "EXPECTED HANDLED " << EXP_H << " "
+                         << "EXPECTED DEFAULT " << EXP_D << "\n";
+                }
+
+                Obj Z(defaultHandler, &ta);
+                ASSERT(&ta == Z.allocator());
+                Z.callback(reporter);
+                handledAssertionCount = 0;
+                defaultAssertionCount = 0;
+
+                Z.callback()(0, 0, 0, 0, bsl::vector<void *>());
+                ASSERT(1 == handledAssertionCount);
+                handledAssertionCount = 0;
+
+                Z.onEachAssertion(EACH);
+                ASSERT(EACH == Z.onEachAssertion());
+                Z.onNewLocation(LOC);
+                ASSERT(LOC == Z.onNewLocation());
+                Z.onNewStackTrace(STACK);
+                ASSERT(STACK == Z.onNewStackTrace());
+                Z.maxAssertions(MAX_A);
+                ASSERT(MAX_A == Z.maxAssertions());
+                Z.maxLocations(MAX_L);
+                ASSERT(MAX_L == Z.maxLocations());
+                Z.maxStackTracesPerLocation(MAX_S);
+                ASSERT(MAX_S == Z.maxStackTracesPerLocation());
+
+                for (int i = 0; i < AL[0][0]; ++i)
+                    trigger1(Z, "assertion 0", __FILE__ "1", LINE);
+                for (int i = 0; i < AL[0][1]; ++i)
+                    trigger2(Z, "assertion 0", __FILE__ "1", LINE);
+                for (int i = 0; i < AL[0][2]; ++i)
+                    trigger3(Z, "assertion 0", __FILE__ "1", LINE);
+                for (int i = 0; i < AL[1][0]; ++i)
+                    trigger1(Z, "assertion 1", __FILE__ "2", LINE);
+                for (int i = 0; i < AL[1][1]; ++i)
+                    trigger2(Z, "assertion 1", __FILE__ "2", LINE);
+                for (int i = 0; i < AL[1][2]; ++i)
+                    trigger3(Z, "assertion 1", __FILE__ "2", LINE);
+                for (int i = 0; i < AL[2][0]; ++i)
+                    trigger1(Z, "assertion 2", __FILE__ "3", LINE);
+                for (int i = 0; i < AL[2][1]; ++i)
+                    trigger2(Z, "assertion 2", __FILE__ "3", LINE);
+                for (int i = 0; i < AL[2][2]; ++i)
+                    trigger3(Z, "assertion 2", __FILE__ "3", LINE);
+
+                ASSERTV(handledAssertionCount, EXP_H == handledAssertionCount);
+                ASSERTV(defaultAssertionCount, EXP_D == defaultAssertionCount);
+            }
+        }
+      } break;
       case 1: {
         // --------------------------------------------------------------------
         // BREATHING TEST
+        //   This case exercises (but does not fully test) basic functionality.
         //
         // Concerns:
-        //: 1 A concern.
+        //: 1 The class can be invoked with the assertion failure triple, and
+        //:   it can iterate over the accumulated assertions.
         //
         // Plan:
-        //: 1 A plan.
+        //: 1 Create an assertion tracker object and invoke a few of its
+        //:   methods.
         //
         // Testing:
         //   BREATHING TEST
         // --------------------------------------------------------------------
 
-        if (verbose)
-            cout << "BREATHING TEST\n"
-                    "==============\n";
+        if (verbose) cout << "\nBREATHING TEST"
+                             "\n==============\n";
         {
             Obj Z(bsls::Assert::failureHandler(), &ta);
             for (int i = 0; i < 10; ++i) {
