209c912227992f25457ca842374a48ed0f0259e7
George Laptev <George.Laptev@teknavo.com>
2017-07-11 04:53:41 -0400

    minor change

--- groups/bdl/bdldfp/bdldfp_decimalformatutil.h
+++ groups/bdl/bdldfp/bdldfp_decimalformatutil.h
@@ -119,7 +119,6 @@ class DecimalFormatConfig {
     }
 };
 
-
                         // =======================
                         // class DecimalFormatUtil
                         // =======================
