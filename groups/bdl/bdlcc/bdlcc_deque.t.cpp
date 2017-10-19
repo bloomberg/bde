7386a285bc60d1c1549cf204f85ccb59931f9ab3
Ofer Imanuel <oimanuel@bloomberg.net>
2017-10-19 13:06:52 -0400

    copyright 2014==>2017

--- groups/bdl/bdlcc/bdlcc_deque.t.cpp
+++ groups/bdl/bdlcc/bdlcc_deque.t.cpp
@@ -6311,7 +6311,7 @@ int main(int argc, char *argv[])
 
 // ----------------------------------------------------------------------------
 // NOTICE:
-//      Copyright (C) Bloomberg L.P., 2014
+//      Copyright (C) Bloomberg L.P., 2017
 //      All Rights Reserved.
 //      Property of Bloomberg L.P. (BLP)
 //      This software is made available solely pursuant to the
