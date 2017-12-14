05f8f12a35447b023cb660ecf775aee8498e7837
Oleg Subbotin <osubbotin@bloomberg.net>
2017-12-14 16:46:54 +0000

    Adopt commit 28e3e3c7a from master: Merge pull request #1578 from osubbotin/review/cmake-internal-release
    commit 28e3e3c7a549ab0b632e46912b4381ac9ecfae2d
    Merge: 371089dab 84f448dce
    Author: Oleg Subbotin <osubbotin@bloomberg.net>
    Date:   Thu Dec 14 16:46:54 2017 +0000
    
        Merge pull request #1578 from osubbotin/review/cmake-internal-release
    
        Cmake build system. Initial internal release.
    
    commit 84f448dcec99b84a3d792d53a21cfeac475ad43f
    Author: Oleg Subbotin <osubbotin@bloomberg.net>
    Date:   Fri Oct 20 13:59:38 2017 -0400
    
        Cmake build system. Initial internal release.

---
+++ groups/bsl/bsl+stdhdrs/package/bsl+stdhdrs.cmake
@@ -0,0 +1,39 @@
+function(process_package outName list_file uorName)
+    get_filename_component(packageName ${list_file} NAME_WE)
+    get_filename_component(list_dir ${list_file} DIRECTORY)
+    get_filename_component(root_dir ${list_dir} DIRECTORY)
+
+    bde_add_info_target(${packageName})
+    set(${outName} ${packageName} PARENT_SCOPE)
+
+    # Sources and headers
+    bde_utils_add_meta_file("${list_dir}/${packageName}.pub" headers_list TRACK)
+    bde_list_template_substitute(headers "%" "${root_dir}/%" ${headers_list})
+    bde_info_target_set_property(${packageName} HEADERS "${headers}")
+
+    # Dependencies
+    bde_utils_add_meta_file("${list_dir}/${packageName}.dep" dependencies TRACK)
+    bde_info_target_set_property(${packageName} DEPENDS "${dependencies}")
+
+    # Include directories
+    bde_add_interface_target(${packageName})
+    bde_info_target_set_property(${packageName} INTERFACE_TARGET ${packageName})
+    bde_interface_target_include_directories(
+        ${packageName}
+        PUBLIC
+            $<BUILD_INTERFACE:${root_dir}>
+            $<INSTALL_INTERFACE:"include/stlport">
+    )
+
+    # Custom install of all headers in 'stlport'.
+    # Note that this code correctly handles install of subdirs.
+    foreach (file ${headers_list})
+        get_filename_component(dir ${file} DIRECTORY)
+        string(CONCAT fullFile ${root_dir} "/" ${file})
+        install(
+            FILES ${fullFile}
+            DESTINATION "include/stlport/${dir}"
+            COMPONENT "${uorName}-headers"
+        )
+    endforeach()
+endfunction()
