664722b65c52b519abdaf7503e12ebbd9d1feb5a
Hyman Rosen <hrosen4@bloomberg.net>
2018-01-18 15:29:08 -0500

    Adopt commit 02e8754af from master: Merge pull request #1648 from hrosen4/signed-shift_drqs-114474016
    commit 02e8754afa72c384b4708a31c4cd598e966d337e
    Merge: 2b7aa9f99 2268e4743
    Author: Henry Verschell <hverschell@bloomberg.net>
    Date:   Thu Jan 18 15:29:08 2018 -0500
    
        Merge pull request #1648 from hrosen4/signed-shift_drqs-114474016
    
        Signed integer left shift considered harmful.
    
    commit 2268e4743ce406a0404f9d94116cd8933395c9e0
    Author: Hyman Rosen <hrosen4@bloomberg.net>
    Date:   Thu Jan 18 11:45:57 2018 -0500
    
        Signed integer left shift considered harmful.

--- groups/bdl/bdlde/bdlde_base64encoder.h
+++ groups/bdl/bdlde/bdlde_base64encoder.h
@@ -428,13 +428,13 @@ class Base64Encoder {
                                                       // length
 
     // INSTANCE DATA
-    signed char d_state;  // state as per above enum
+    signed char d_state;          // state as per above enum
 
-    int d_maxLineLength;  // maximum length of output line
-    int d_lineLength;     // current length of output line
-    int d_outputLength;   // total number of output characters
-    int d_stack;          // storage of non-emitted input
-    int d_bitsInStack;    // number of bits in 'd_stack'
+    int         d_maxLineLength;  // maximum length of output line
+    int         d_lineLength;     // current length of output line
+    int         d_outputLength;   // total number of output characters
+    unsigned    d_stack;          // storage of non-emitted input
+    int         d_bitsInStack;    // number of bits in 'd_stack'
 
   private:
     // NOT IMPLEMENTED
