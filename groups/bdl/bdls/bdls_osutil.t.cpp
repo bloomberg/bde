3766702c25bf7bf49085b68e63a579520c2cf08d
Alisdair Meredith <ameredith1@bloomberg.net>
2018-01-24 15:22:54 -0500

    Less strict validation for matching an OS string on Windows
    The string constructed to represent the Service Pack release
    on Windows will often not match the string directly reported
    by Windows itself, as the BDE string will report '1.0' rather
    than just '1'.  Modify the test to look for simply the same
    leading string, and assume that trailing text does not matter.
    This is for display only, and precise values matter less.
    Note that this test may be expected to fail if run on a Windows
    OS on a non-English speaking locale.
    
    Fix up a number of issues for bde_verify, and general test
    driver best practice.

--- groups/bdl/bdls/bdls_osutil.t.cpp
+++ groups/bdl/bdls/bdls_osutil.t.cpp
@@ -7,27 +7,25 @@
 // should not be used as an example for new development.
 // ----------------------------------------------------------------------------
 
-
 #include <bdls_osutil.h>
 
 #include <bslim_testutil.h>
 
-#include <bsls_platform.h>
-
 #include <bslma_default.h>
-#include <bslma_defaultallocatorguard.h>
 #include <bslma_testallocator.h>
 
 #include <bsls_assert.h>
 #include <bsls_asserttest.h>
+#include <bsls_platform.h>
 
 #include <bsl_iostream.h>
 #include <bsl_sstream.h>
+#include <bsl_string.h>
 
 #ifdef BSLS_PLATFORM_OS_WINDOWS
 #include <windows.h>
 #include <process.h>
-#include <cstring>
+#include <bsl_cstring.h>
 #else
 #include <unistd.h>
 #include <sys/utsname.h>
@@ -43,9 +41,10 @@ using namespace bsl;
 //                              --------
 //
 //-----------------------------------------------------------------------------
-// [ ]
+// [2] int getOsInfo(bsl::string *os, bsl::string *v, bsl::string *ptch);
 //-----------------------------------------------------------------------------
 // [1] BREATHING TEST
+// [3] USAGE EXAMPLE
 
 // ============================================================================
 //                     STANDARD BDE ASSERT TEST FUNCTION
@@ -116,11 +115,28 @@ int main(int argc, char *argv[])
 {
     int                 test = argc > 1 ? atoi(argv[1]) : 0;
     bool             verbose = argc > 2;
-//  bool         veryVerbose = argc > 3;
-//  bool     veryVeryVerbose = argc > 4;
-//  bool veryVeryVeryVerbose = argc > 5;
+    bool         veryVerbose = argc > 3;
+    bool     veryVeryVerbose = argc > 4;
+    bool veryVeryVeryVerbose = argc > 5;
+
+    (void)     veryVerbose;  // not currently used
+    (void) veryVeryVerbose;  // not currently used
 
     cout << "TEST " << __FILE__ << " CASE " << test << endl;
+
+    // CONCERN: In no case does memory come from the global allocator.
+
+    bslma::TestAllocator globalAllocator("global", veryVeryVeryVerbose);
+    bslma::Default::setGlobalAllocator(&globalAllocator);
+
+    // CONCERN: The default allocator has not been locked, and in no case does
+    // memory come from the global allocator.
+
+    bslma::TestAllocator defaultAllocator("default", veryVeryVeryVerbose);
+    ASSERT(0 == bslma::Default::setDefaultAllocator(&defaultAllocator));
+
+    // CONCERN: The default allocator has not been locked, and there are no
+    // leaks through the default allocator when the test driver exits.
 
     switch (test) { case 0:
       case 3: {
@@ -144,68 +160,77 @@ int main(int argc, char *argv[])
                           << "USAGE EXAMPLE" << endl
                           << "=============" << endl;
 
-        // The following example demonstrates using 'getOsInfo' to obtain
-        // information about the operating system at runtime and writing it to
-        // the console.
-
-        // First, we create strings for the operating system name ('osName'),
-        // version ('osVersion'), and patch ('osPatch'), and then call
-        // 'getOsInfo' to load these strings with values for the operating
-        // system the task is executing in:
-
-        bsl::string name;
-        bsl::string version;
-        bsl::string patch;
-        int rc = bdls::OsUtil::getOsInfo(&name, &version, &patch);
-
-        // Then, we use the standard streams to write the operating system
-        // version information to the console, or an error message of failure:
-
-        if (0 == rc) {
-            cout << "OS Name: " << name << "\n"
-                      << "Version: " << version << "\n"
-                      << "Patch:   " << patch << "\n";
-        } else {
-            cout << "Cannot determine OS name and version\n";
-        }
-
-        // Finally, the resulting console output on the Red Hat Enterprise
-        // Linux Server 5.5 would be
-        //..
-        // OS Name: Linux
-        // Version: 2.6.18-194.32.1.el5
-        // Patch:   #1 SMP Mon Dec 20 10:52:42 EST 2010
-        //..
-
+// The following example demonstrates using 'getOsInfo' to obtain information
+// about the operating system at runtime and writing it to the console.
+//
+// First, we create strings for the operating system name ('osName'), version
+// ('osVersion'), and patch ('osPatch'), and then call 'getOsInfo' to load
+// these strings with values for the operating system the task is executing in:
+//..
+    bsl::string name;
+    bsl::string version;
+    bsl::string patch;
+//..
+// Then, we use the standard streams to write the operating system version
+// information to the console, or an error message of failure:
+//..
+    const int rc = bdls::OsUtil::getOsInfo(&name, &version, &patch);
+    if (rc == 0) {
+        bsl::cout << "OS Name: " << name << "\n"
+                  << "Version: " << version << "\n"
+                  << "Patch:   " << patch << "\n";
+    } else {
+        bsl::cout << "Cannot determine OS name and version\n";
+    }
+//..
+// Finally, the resulting console output on the Red Hat Enterprise Linux Server
+// 5.5 would be
+//..
+//  OS Name: Linux
+//  Version: 2.6.18-194.32.1.el5
+//  Patch:   #1 SMP Mon Dec 20 10:52:42 EST 2010
+//..
+// On Windows 7 SP1, the display would be
+//..
+//  OS Name: Windows
+//  Version: 6.1
+//  Patch:   Service Pack 1
+//..
       } break;
       case 2: {
         // --------------------------------------------------------------------
-        // OS IDENTIFICATION
+        // TESTING 'getOsInfo'
         //   Ensure that the operating system is identified correctly.
         //
         // Concerns:
         //: 1 The operating system is identified as defined in the contract.
+        //: 2 The result is always zero, as there is no clear way to force an
+        //:   OS to fail to report the expected data.
         //
         // Plan:
-        //: 1 Verify OS Name is 'Windows' when built on MS Windows, and the
+        //: 1 Verify the function returns zero
+        //: 2 Verify OS Name is 'Windows' when built on MS Windows, and the
         //:   loaded version and patch match values returned by GetVersionEx().
-        //: 2 On Posix systems verify that loaded 'name', 'version' and 'patch'
+        //: 3 On Posix systems verify that loaded 'name', 'version' and 'patch'
         //:   match sysname, release and version, respectively, as returned by
         //:   uname(2).
+        //: 4 Negative testing: verify that passing a null pointer for any
+        //:   argument is detected by a 'BSLS_ASSERT'.
         //
         // Testing:
-        //   getOsInfo
+        //   int getOsInfo(bsl::string *os, bsl::string *v, bsl::string *ptch);
         // --------------------------------------------------------------------
 
         if (verbose) cout << endl
-                          << "OS IDENTIFICATION TEST" << endl
-                          << "======================" << endl;
+                          << "TESTING 'getOsInfo'" << endl
+                          << "===================" << endl;
         bsl::string name;
         bsl::string version;
         bsl::string patch;
         ASSERT(0 == Obj::getOsInfo(&name, &version, &patch));
+
 #ifdef BSLS_PLATFORM_OS_WINDOWS
-        ASSERT("Windows" == name);
+        ASSERTV(name, "Windows" == name);
 
         OSVERSIONINFOEX osvi;
         memset(&osvi, 0, sizeof(osvi));
@@ -216,16 +241,28 @@ int main(int argc, char *argv[])
         expectedVersion << osvi.dwMajorVersion
                         << '.'
                         << osvi.dwMinorVersion;
-        ASSERT(version == expectedVersion.str());
+        ASSERTV(version,   expectedVersion.str(),
+                version == expectedVersion.str());
 
-        ASSERT(patch == osvi.szCSDVersion);
+        ASSERTV(patch, osvi.szCSDVersion, 0 == patch.find(osvi.szCSDVersion));
 #else
         struct utsname unameInfo;
         ASSERT(uname(&unameInfo) >= 0);
-        ASSERT(name == unameInfo.sysname);
-        ASSERT(version == unameInfo.release);
-        ASSERT(patch == unameInfo.version);
+        ASSERTV(name, unameInfo.sysname, name == unameInfo.sysname);
+        ASSERTV(version, unameInfo.release, version == unameInfo.release);
+        ASSERTV(patch, unameInfo.version, patch == unameInfo.version);
 #endif
+
+        if (verbose) cout << "Negative testing" << endl;
+        {
+            bsls::AssertTestHandlerGuard hg;
+
+            ASSERT_FAIL(Obj::getOsInfo(    0, &version, &patch));
+            ASSERT_FAIL(Obj::getOsInfo(&name,        0, &patch));
+            ASSERT_FAIL(Obj::getOsInfo(&name, &version,      0));
+            ASSERT_FAIL(Obj::getOsInfo(    0,        0,      0));
+            ASSERT_PASS(Obj::getOsInfo(&name, &version, &patch));
+        }
       } break;
       case 1: {
         // --------------------------------------------------------------------
@@ -257,6 +294,11 @@ int main(int argc, char *argv[])
         testStatus = -1;
       }
     }
+
+    // CONCERN: In no case does memory come from the global allocator.
+
+    LOOP_ASSERT(globalAllocator.numBlocksTotal(),
+                0 == globalAllocator.numBlocksTotal());
 
     if (testStatus > 0) {
         cerr << "Error, non-zero test status = " << testStatus << "." << endl;
