e7499591b90c02a12d8f53bd10a3eb5d82826599
Rostislav Khlebnikov <rkhlebnikov@bloomberg.net>
2018-02-22 17:38:47 +0000

    bigobj

---
+++ groups/bdl/bdlf/package/bdlf.cmake
@@ -0,0 +1,17 @@
+include(bde_interface_target)
+include(bde_struct)
+
+function(process_package retPackage)
+    default_process_package(package ${ARGN})
+
+    bde_struct_get_field(testInterface ${package} TEST_INTERFACE_TARGET)
+    bde_interface_target_compile_options(
+        ${testInterface}
+        PRIVATE
+            $<$<CXX_COMPILER_ID:MSVC>:
+                /bigobj
+            >
+    )
+
+    bde_return(${package})
+endfunction()
