a055db609f5b2db373e229fb5476a4c45ff9e4c6
Hyman Rosen <hrosen4@bloomberg.net>
2018-03-19 19:38:17 -0400

    Add the files...

---
+++ groups/bdl/bdlb/bdlb_transformiterator.t.cpp
@@ -0,0 +1,175 @@
+// bdlb_transformiterator.t.cpp                                       -*-C++-*-
+#include <bdlb_transformiterator.h>
+
+#include <bslim_testutil.h>
+
+#include <bsls_asserttest.h>
+
+#include <bsl_algorithm.h>
+#include <bsl_iostream.h>
+#include <bsl_cmath.h>
+#include <bsl_numeric.h>
+
+using namespace BloombergLP;
+using namespace bsl;
+
+// ============================================================================
+//                             TEST PLAN
+// ----------------------------------------------------------------------------
+//                              Overview
+//                              --------
+//
+// ----------------------------------------------------------------------------
+// CREATORS
+//
+// MANIPULATORS
+//
+// ACCESSORS
+//
+// ----------------------------------------------------------------------------
+// [ 1] BREATHING TEST
+// [ 2] USAGE EXAMPLE
+
+// ============================================================================
+//                     STANDARD BDE ASSERT TEST FUNCTION
+// ----------------------------------------------------------------------------
+
+namespace {
+
+int testStatus = 0;
+
+void aSsErT(bool condition, const char *message, int line)
+{
+    if (condition) {
+        cout << "Error " __FILE__ "(" << line << "): " << message
+             << "    (failed)" << endl;
+
+        if (0 <= testStatus && testStatus <= 100) {
+            ++testStatus;
+        }
+    }
+}
+
+}  // close unnamed namespace
+
+// ============================================================================
+//               STANDARD BDE TEST DRIVER MACRO ABBREVIATIONS
+// ----------------------------------------------------------------------------
+
+#define ASSERT       BSLIM_TESTUTIL_ASSERT
+#define ASSERTV      BSLIM_TESTUTIL_ASSERTV
+
+#define LOOP_ASSERT  BSLIM_TESTUTIL_LOOP_ASSERT
+#define LOOP0_ASSERT BSLIM_TESTUTIL_LOOP0_ASSERT
+#define LOOP1_ASSERT BSLIM_TESTUTIL_LOOP1_ASSERT
+#define LOOP2_ASSERT BSLIM_TESTUTIL_LOOP2_ASSERT
+#define LOOP3_ASSERT BSLIM_TESTUTIL_LOOP3_ASSERT
+#define LOOP4_ASSERT BSLIM_TESTUTIL_LOOP4_ASSERT
+#define LOOP5_ASSERT BSLIM_TESTUTIL_LOOP5_ASSERT
+#define LOOP6_ASSERT BSLIM_TESTUTIL_LOOP6_ASSERT
+
+#define Q            BSLIM_TESTUTIL_Q   // Quote identifier literally.
+#define P            BSLIM_TESTUTIL_P   // Print identifier and value.
+#define P_           BSLIM_TESTUTIL_P_  // P(X) without '\n'.
+#define T_           BSLIM_TESTUTIL_T_  // Print a tab (w/o newline).
+#define L_           BSLIM_TESTUTIL_L_  // current Line number
+
+// ============================================================================
+//                  NEGATIVE-TEST MACRO ABBREVIATIONS
+// ----------------------------------------------------------------------------
+
+#define ASSERT_SAFE_PASS(EXPR) BSLS_ASSERTTEST_ASSERT_SAFE_PASS(EXPR)
+#define ASSERT_SAFE_FAIL(EXPR) BSLS_ASSERTTEST_ASSERT_SAFE_FAIL(EXPR)
+#define ASSERT_PASS(EXPR)      BSLS_ASSERTTEST_ASSERT_PASS(EXPR)
+#define ASSERT_FAIL(EXPR)      BSLS_ASSERTTEST_ASSERT_FAIL(EXPR)
+#define ASSERT_OPT_PASS(EXPR)  BSLS_ASSERTTEST_ASSERT_OPT_PASS(EXPR)
+#define ASSERT_OPT_FAIL(EXPR)  BSLS_ASSERTTEST_ASSERT_OPT_FAIL(EXPR)
+
+#define ASSERT_SAFE_PASS_RAW(EXPR) BSLS_ASSERTTEST_ASSERT_SAFE_PASS_RAW(EXPR)
+#define ASSERT_SAFE_FAIL_RAW(EXPR) BSLS_ASSERTTEST_ASSERT_SAFE_FAIL_RAW(EXPR)
+#define ASSERT_PASS_RAW(EXPR)      BSLS_ASSERTTEST_ASSERT_PASS_RAW(EXPR)
+#define ASSERT_FAIL_RAW(EXPR)      BSLS_ASSERTTEST_ASSERT_FAIL_RAW(EXPR)
+#define ASSERT_OPT_PASS_RAW(EXPR)  BSLS_ASSERTTEST_ASSERT_OPT_PASS_RAW(EXPR)
+#define ASSERT_OPT_FAIL_RAW(EXPR)  BSLS_ASSERTTEST_ASSERT_OPT_FAIL_RAW(EXPR)
+
+//=============================================================================
+//                                USAGE EXAMPLE
+//-----------------------------------------------------------------------------
+
+///Example 1:
+/// - - - - -
+//..
+//..
+
+
+//=============================================================================
+//                                 MAIN PROGRAM
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    int test            = argc > 1 ? atoi(argv[1]) : 0;
+    int verbose         = argc > 2;  (void)verbose;
+    int veryVerbose     = argc > 3;  (void)veryVerbose;
+    int veryVeryVerbose = argc > 4;  (void)veryVeryVerbose;
+
+    cout << "TEST " << __FILE__ << " CASE " << test << endl;
+
+    switch (test) { case 0:
+      case 2: {
+        // --------------------------------------------------------------------
+        // USAGE EXAMPLE
+        //
+        // Concerns:
+        //: 1 Yada, yada, yada.
+        //
+        // Plan:
+        //: 1 Something, something, something  (C-1).
+        //
+        // Testing:
+        //   USAGE EXAMPLE
+        // --------------------------------------------------------------------
+        if (verbose) cout << "\nUSAGE EXAMPLE"
+                             "\n=============\n";
+        ASSERT(2 == 2);
+      } break;
+      case 1: {
+        // --------------------------------------------------------------------
+        // BREATHING TEST
+        //   This case exercises (but does not fully test) basic functionality.
+        //
+        // Concerns:
+        //: 1 The class is functional enough to enable comprehensive
+        //:   testing in subsequent cases
+        //
+        // Plan:
+        //: 1 Create an object, using the ctor with functional object.
+        //:
+        //: 2 Assign value to dereferenced iterator.
+        //:
+        //: 3 Check that functional object was invoked with correct value.
+        //
+        // Testing:
+        //   BREATHING TEST
+        // --------------------------------------------------------------------
+
+        if (verbose) cout << "\nBREATHING TEST"
+                             "\n==============\n";
+
+        double d[] = { 1, 4, 9, 16, 25, 36 };
+        typedef bdlb::TransformIterator<double(*)(double), double *> Ti;
+
+        ASSERT(21 == bsl::accumulate(Ti(d + 0, sqrt), Ti(d + 6, sqrt), 0.0));
+      } break;
+      default: {
+         cerr << "WARNING: CASE '" << test << "' NOT FOUND." << endl;
+         testStatus = -1;
+      }
+    }
+
+    if (testStatus > 0) {
+        cerr << "Error, non-zero test status = " << testStatus << "." << endl;
+    }
+
+    return testStatus;
+}
