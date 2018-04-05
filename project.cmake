88a8a8ac5760b38fded69ee180def43e2a66fb30
Rostislav Khlebnikov <rkhlebnikov@bloomberg.net>
2018-04-05 14:25:14 +0100

    removed "project" interface

--- project.cmake
+++ project.cmake
@@ -8,25 +8,23 @@ include(layers/ufid)
 include(layers/install_pkg_config)
 include(layers/install_cmake_config)
 
-bde_prefixed_override(bdeproj project_setup_interface)
-function(bdeproj_project_setup_interface proj)
-    bde_assert_no_extra_args()
+function(bdeproj_uor_setup_interface uor)
+    uor_setup_interface_base("" ${ARGV})
 
-    project_setup_interface_base("" ${ARGV})
-
-    bde_struct_get_field(interfaceTarget ${proj} INTERFACE_TARGET)
+    bde_struct_get_field(interfaceTarget ${uor} INTERFACE_TARGET)
     bde_interface_target_compile_definitions(
         ${interfaceTarget}
         PUBLIC
             $<$<NOT:$<BOOL:$<TARGET_PROPERTY:SUPPRESS_BSL_OVERRIDES_STD>>>:BSL_OVERRIDES_STD>
     )
-
-    bde_return(${proj})
 endfunction()
 
 bde_prefixed_override(bdeproj project_process_uors)
 function(bdeproj_project_process_uors proj listDir)
     bde_assert_no_extra_args()
+
+    bde_record_overrides()
+    bde_prefixed_override(bdeproj uor_setup_interface)
 
     bde_project_process_package_groups(
         ${proj}
@@ -36,6 +34,8 @@ function(bdeproj_project_process_uors proj listDir)
         ${listDir}/groups/bsl
         ${listDir}/groups/btl
     )
+
+    bde_remove_recorded_overrides()
 
     bde_project_process_standalone_packages(
         ${proj}
