3634909569a1f246ea089237e21e2723bc62778c
Jeffrey Mendelsohn <JMENDELSOHN4@bloomberg.net>
2018-04-27 11:31:01 -0400

    .

--- groups/bdl/bdlc/bdlc_hashtable.t.cpp
+++ groups/bdl/bdlc/bdlc_hashtable.t.cpp
@@ -132,32 +132,13 @@ void aSsErT(bool condition, const char *message, int line)
 namespace {
 
 // ============================================================================
-//                   GLOBAL TYPEDEFS/CONSTANTS FOR TESTING
-// ----------------------------------------------------------------------------
-
-int PRIMES_1000[] = {
-      2,   3,   5,   7,  11,  13,  17,  19,  23,  29,  31,  37,  41,  43,  47,
-     53,  59,  61,  67,  71,  73,  79,  83,  89,  97, 101, 103, 107, 109, 113,
-    127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197,
-    199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
-    283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379,
-    383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
-    467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571,
-    577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
-    661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761,
-    769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
-    877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977,
-    983, 991, 997,
-};
-
-// ============================================================================
 //                       HELPER FUNCTIONS FOR TESTING
 // ----------------------------------------------------------------------------
 
 namespace TestCase3 {
 
 template <class TYPE>
-void testIsNullFootprint(TYPE *ignored)
+void testIsNullFootprint(TYPE *)
 {
     typedef TYPE                         Bucket;
     typedef bdlc::HashTableDefaultTraits Traits;
@@ -167,7 +148,7 @@ void testIsNullFootprint(TYPE *ignored)
     bsl::memset(d_bucket.buffer(), 0, sizeof(Bucket));
     ASSERT(Traits::isNull(d_bucket.object()));
 
-    for (int i = 0; i < sizeof(Bucket); ++i) {
+    for (unsigned int i = 0; i < sizeof(Bucket); ++i) {
         bsl::memset(d_bucket.buffer(), 0, sizeof(Bucket));
         d_bucket.buffer()[i] = 1;
         ASSERT(!Traits::isNull(d_bucket.object()));
@@ -175,7 +156,7 @@ void testIsNullFootprint(TYPE *ignored)
 }
 
 template <class TYPE>
-void testIsRemovedFootprint(TYPE *ignored)
+void testIsRemovedFootprint(TYPE *)
 {
     typedef TYPE                         Bucket;
     typedef bdlc::HashTableDefaultTraits Traits;
@@ -185,7 +166,7 @@ void testIsRemovedFootprint(TYPE *ignored)
     bsl::memset(d_bucket.buffer(), 0xFF, sizeof(Bucket));
     ASSERT(Traits::isRemoved(d_bucket.object()));
 
-    for (int i = 0; i < sizeof(Bucket); ++i) {
+    for (unsigned int i = 0; i < sizeof(Bucket); ++i) {
         bsl::memset(d_bucket.buffer(), 0xFF, sizeof(Bucket));
         d_bucket.buffer()[i] = 0;
         ASSERT(!Traits::isRemoved(d_bucket.object()));
@@ -212,31 +193,6 @@ bool isPrime(unsigned int n)
 }
 
 }  // close namespace TestCase2
-
-namespace TestCase5 {
-
-bool g_testHash1Called;
-unsigned int testHash1(int key)
-{
-    g_testHash1Called = true;
-    return 0;
-}
-
-bool g_testHash2Called;
-unsigned int testHash2(int key)
-{
-    g_testHash2Called = true;
-    return 0;
-}
-
-bool g_testHash3Called;
-unsigned int testHash3(int key)
-{
-    g_testHash3Called = true;
-    return 0;
-}
-
-}  // close namespace TestCase5
 
 // ============================================================================
 //                               USAGE EXAMPLE
