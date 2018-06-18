6f2bb2bce72a21807901763dcc50d12fb6498510
Mike Giroux <mgiroux@bloomberg.net>
2018-06-18 12:11:52 -0400

    Revert bsl version change - no rssuite for 3.10.0

--- groups/bsl/bslscm/bslscm_versiontag.h
+++ groups/bsl/bslscm/bslscm_versiontag.h
@@ -57,7 +57,7 @@ BSLS_IDENT("$Id: $")
 #define BSL_VERSION_MAJOR    3
     // Provide the major version number of the current (latest) BSL release.
 
-#define BSL_VERSION_MINOR    10
+#define BSL_VERSION_MINOR    7
     // Provide the minor version number of the current (latest) BSL release.
 
 #define BSL_MAKE_VERSION(major, minor) ((major) * 10000 \
