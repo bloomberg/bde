a9c517b483ac1eda4e536a2c30170af7a19bd8da
Clay Wilson <cwilson9@bloomberg.net>
2018-07-06 17:46:46 -0400

    update archaic Copyright notices

--- groups/bdl/bdlb/bdlb_scopeexit.h
+++ groups/bdl/bdlb/bdlb_scopeexit.h
@@ -1,7 +1,8 @@
 // bdlb_scopeexit.h                                                   -*-C++-*-
 #ifndef INCLUDED_BDLB_SCOPEEXIT
 #define INCLUDED_BDLB_SCOPEEXIT
-#  include <bsls_ident.h>
+
+#include <bsls_ident.h>
 BSLS_IDENT("$Id: $")
 
 //@PURPOSE: Provide a general-purpose guard object for scope-exit logic.
@@ -493,10 +494,17 @@ ScopeExitUtil::makeScopeExit(EXIT_FUNC&& function)
 #endif
 
 // ----------------------------------------------------------------------------
-// NOTICE:
-//      Copyright (C) Bloomberg L.P., 2018
-//      All Rights Reserved.
-//      Property of Bloomberg L.P. (BLP)
-//      This software is made available solely pursuant to the
-//      terms of a BLP license agreement which governs its use.
+// Copyright 2018 Bloomberg Finance L.P.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 // ----------------------------- END-OF-FILE ----------------------------------
