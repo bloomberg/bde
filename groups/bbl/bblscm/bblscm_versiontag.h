7cff10798c8d16411cacae0640b80c127d2cb9df
Mike Giroux <mgiroux@bloomberg.net>
2018-09-17 14:18:50 -0400

    Bump bal, bbl, and bdl versions to 3.13.0

--- groups/bbl/bblscm/bblscm_versiontag.h
+++ groups/bbl/bblscm/bblscm_versiontag.h
@@ -39,12 +39,12 @@ BSLS_IDENT("$Id: $")
 #include <bslscm_versiontag.h>
 
 
-// For the 3.12.x release series, 'bbl' is being versioned independently of
+// For the 3.13.x release series, 'bbl' is being versioned independently of
 // 'bsl'.
 #define BBL_VERSION_MAJOR     3
     // Provide the major version number of the current (latest) BBL release.
 
-#define BBL_VERSION_MINOR     12
+#define BBL_VERSION_MINOR     13
     // Provide the minor version number of the current (latest) BBL release.
 
 #define BBL_VERSION BSL_MAKE_VERSION(BBL_VERSION_MAJOR, \
