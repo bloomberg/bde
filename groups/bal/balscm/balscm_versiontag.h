58b56a7b9131b546d2a65c144015c4529880704a
Mike Giroux <mgiroux@bloomberg.net>
2018-10-31 12:33:31 -0400

    Bump version to 3.16.0

--- groups/bal/balscm/balscm_versiontag.h
+++ groups/bal/balscm/balscm_versiontag.h
@@ -56,7 +56,7 @@ BSLS_IDENT("$Id: $")
 #define BAL_VERSION_MAJOR 3
     // BAL release major version
 
-#define BAL_VERSION_MINOR 15
+#define BAL_VERSION_MINOR 16
     // BAL release major version
 
 #define BAL_VERSION BSL_MAKE_VERSION(BAL_VERSION_MAJOR, \
