85e14a3d8176f556a89c1fba1de394e8bde77dae
George Logothetis <glogothetis1@bloomberg.net>
2019-04-15 10:20:14 -0400

    Do not include rw7/rw/compiler.h for SS12.4 and later

--- groups/bsl/bsls/bsls_libraryfeatures.cpp
+++ groups/bsl/bsls/bsls_libraryfeatures.cpp
@@ -78,10 +78,12 @@ BSLS_IDENT("$Id$ $CSID$")
 #endif
 
 #elif defined(BSLS_LIBRARYFEATURES_STDCPP_LIBCSTD)
+#if BSLS_PLATFORM_CMP_VERSION <= 0x5120  // for SS12.3 and earlier
 #include <rw7/rw/compiler.h>
 #ifndef __RWCOMPILER_H__
 #error <rw/compiler.h> does not seem to #define __RWCOMPILER_H__.
 #endif
+#endif // BSLS_PLATFORM_CMP_VERSION
 
 #elif defined(BSLS_LIBRARYFEATURES_STDCPP_STLPORT)
 #include <config/stlcomp.h>
