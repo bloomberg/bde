f2a2803ccec8509f91936d8dd4c7fbca9c7c42a2
Steven Breitstein <sbreitstein@bloomberg.net>
2019-07-17 10:25:22 -0400

    drqs-142948762: bslstl_boyermoorehorspoolsearcher': Save work.

--- groups/bsl/bslstl/bslstl_boyermoorehorspoolsearcher.t.cpp
+++ groups/bsl/bslstl/bslstl_boyermoorehorspoolsearcher.t.cpp
@@ -5124,7 +5124,11 @@ int main(int argc, char *argv[])
                 P_(da.numBytesInUse())
             }
 
-            ASSERT(dam.isTotalUp()); 
+            if (0 == BSL::strlen(NEEDLE)) {
+                ASSERT(dam.isTotalSame());
+            } else {
+                ASSERT(dam.isTotalUp());
+            }
             dam.reset();
 
             const RndMechCi rndMechCi(needleRnd.begin(), needleRnd.end());
@@ -5140,7 +5144,6 @@ int main(int argc, char *argv[])
             } else {
                 ASSERT(dam.isTotalSame());
             }
-
             dam.reset();
 
             // Invoke each using both kinds of haystack
