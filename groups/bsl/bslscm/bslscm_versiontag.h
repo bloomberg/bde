b9d6e3d61ee2be7ec8ea9366da7cc6b4fd731b7d
Mike Giroux <mgiroux@bloomberg.net>
2020-03-13 11:29:53 -0400

    Bumped version to 3.51.0.0

--- groups/bsl/bslscm/bslscm_versiontag.h
+++ groups/bsl/bslscm/bslscm_versiontag.h
@@ -51,7 +51,7 @@ BSLS_IDENT("$Id: $")
 #define BSL_VERSION_MAJOR    3
     // Provide the major version number of the current (latest) BSL release.
 
-#define BSL_VERSION_MINOR    50
+#define BSL_VERSION_MINOR    51
     // Provide the minor version number of the current (latest) BSL release.
 
 #define BSL_MAKE_VERSION(major, minor) ((major) * 10000 \
