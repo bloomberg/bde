1fb1008148fbcb96240a7c4cce471db10e3f3db6
Oleg Subbotin <osubbotin@bloomberg.net>
2020-08-07 16:28:00 -0400

    Update bslstl_stringview.h (#2888)
    Comment out compilation error block.

--- groups/bsl/bslstl/bslstl_stringview.h
+++ groups/bsl/bslstl/bslstl_stringview.h
@@ -235,7 +235,8 @@ using native_std::operator>=;
 #define BSLSTL_STRING_VIEW_LINKER_CHECK_NAME bslstl_stringview_POST_cpp17_ABI
 
 #elif defined(BSLS_PLATFORM_OS_LINUX)
-#error "BDE requires at least C++17 for ABI compatibility on Linux systems."
+// Must be disabled on master to allow matrix and nightly builds.
+//#error "BDE requires at least C++17 for ABI compatibility on Linux systems."
 #endif  // BSLS_LIBRARYFEATURES_HAS_CPP17_BASELINE_LIBRARY
 #endif  // BDE_DISABLE_CPP17_ABI
 
