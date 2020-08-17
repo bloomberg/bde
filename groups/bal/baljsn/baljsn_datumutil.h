8f7acd544e8bbec5f390b049f995f5b38da8f5b4
Mike Giroux <mgiroux@bloomberg.net>
2020-08-17 11:37:39 -0400

    Applied latest round (and item missed from prev round) of MikeV fixes

--- groups/bal/baljsn/baljsn_datumutil.h
+++ groups/bal/baljsn/baljsn_datumutil.h
@@ -15,27 +15,28 @@ BSLS_IDENT("$Id$ $CSID$")
 // JSON string, and back.
 //
 ///Mapping Data Types between 'Datum' and JSON
-///-----------------------------------------
+///-------------------------------------------
 // While most scalar types supported by 'Datum' can be encoded into a JSON
 // string, only the subset of types represented natively in JSON -- number
 // (represented in C++ as a 'double'), 'string', 'bool', 'null', array, and map
 // types -- will be populated in a 'Datum' decoded from a JSON string.  If one
 // were to encode a 'Datum' containing a type not natively supported by JSON,
 // if that JSON string were decoded back into a 'Datum' object, the resulting
-// Datum would not be equal to the original value.  For example, a 'Datum'
+// 'Datum' would not be equal to the original value.  For example, a 'Datum'
 // containing an integer would be encoded into a JSON number, and then decoded
 // back into a 'Datum' using 'double' to represent that number.
 //
-// Clients wishing to ensure that encoding and then decoding results in a Datum
-// equal to the original value should use only 'Datum' types natively supported
-// in JSON (see {'Supported Types'} below), and ensure that duplicate keys are
-// not present in the source 'Datum' (duplicate keys in a 'Datum' map are
-// typically an error, but the interface does allow them to be created).  To
-// verify that encoded JSON fields can be decoded back into 'Datum' fields of
-// of equal value, clients can supply the 'strictTypes' option (see
-// {'DatumEncoderOptions'}) to 'encode'.  A call to 'encode' with 'strictTypes'
-// as 'true' will return a positive value if the encoded 'Datum' contained a
-// type not natively supported in JSON.
+// Clients wishing to ensure that encoding and then decoding results in a
+// 'Datum' equal to the original value should use only 'Datum' types natively
+// supported in JSON (see {'Supported Types'} below), and ensure that duplicate
+// keys are not present in the source 'Datum' (duplicate keys in a 'Datum' map
+// are typically an error, but the interface does allow them to be created).
+// Enabling the 'strictTypes' option verifies that the types in encoded JSON
+// fields can be decoded back into 'Datum' fields of equal value.  So, for
+// example, enabling 'strictTypes' will result in 'encode' producing a positive
+// return status if one of the encoded types is an 'int', because decoding the
+// resulting JSON will produce a 'double'.  The 'strictTypes' option does not,
+// however, verify that Datum maps contain unique keys.
 //
 // The order of key/value pairs in objects in textual JSON passed to 'decode'
 // is preserved in the decoded 'Datum'.  If multiple entries with the same
@@ -43,11 +44,11 @@ BSLS_IDENT("$Id$ $CSID$")
 //
 // The order of key/value pairs ('DatumMapEntry') in 'Datum' objects passed to
 // 'encode' will be preserved in the resulting 'JSON', and all keys/value pairs
-// will be present.  'strictTypes' checking does not alter the behavior for
-// duplicate keys in maps.  They are allowed whether 'strictTypes' is 'true' or
-// 'false'.  Note that a 'Datum' map contraining duplicate keys is typically an
-// error (the result of an incorrectly constructed 'Datum'), but the public
-// interface for 'Datum' does not disallow creating such a 'Datum' object.
+// will be present (including duplicate keys).  Duplicate keys will be rendered
+// in an encoded JSON, even if 'strictTypes' checking is enable.  Note that a
+// Datum map containing duplicate keys is typically an error (the result of a
+// incorrectly constructed Datum), but the public interface for Datum does not
+// disallow creating such a 'Datum' object.
 //
 ///Supported Types
 ///---------------
