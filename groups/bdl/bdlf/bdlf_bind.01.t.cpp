993ce06c57b294102d5399292aadd87666be800b
Mungo Gill <mgill83@bloomberg.net>
2021-01-21 17:29:58 +0000

    Reword function contract

--- groups/bdl/bdlf/bdlf_bind.01.t.cpp
+++ groups/bdl/bdlf/bdlf_bind.01.t.cpp
@@ -388,8 +388,8 @@ struct MyFunctionObjectWithNonConstVoidAndNonConstIntFunction {
     }
 
     void operator()(int increment)
-        // Function called by test run.  Increments value by 'increment' to
-        // facilitate assert test.
+        // Function called by test run.  Increments counter value by the
+        // specified 'increment' to facilitate assert test.
     {
         d_stateI += increment;
     }
