e48fb817195927806069ba75801c1139bd668853
Mike Giroux <mgiroux@bloomberg.net>
2022-08-10 13:55:53 -0400

    Bumped version to 3.106.0.0

--- groups/bdl/bdlscm/bdlscm_versiontag.h
+++ groups/bdl/bdlscm/bdlscm_versiontag.h
@@ -54,7 +54,7 @@ BSLS_IDENT("$Id: $")
 #define BDL_VERSION_MAJOR     3
     // Provide the major version number of the current (latest) BDL release.
 
-#define BDL_VERSION_MINOR     105
+#define BDL_VERSION_MINOR     106
     // Provide the minor version number of the current (latest) BDL release.
 
 #define BDL_VERSION BSL_MAKE_VERSION(BDL_VERSION_MAJOR, \
