3b872f6a8dfe0e6b92fcf1d0287a8ca0f36413ad
Oleg Subbotin <osubbotin@bloomberg.net>
2023-04-06 15:56:47 -0400

    Bumped version to 3.116.0.0

--- groups/bsl/bslscm/bslscm_versiontag.h
+++ groups/bsl/bslscm/bslscm_versiontag.h
@@ -62,7 +62,7 @@ BSLS_IDENT("$Id: $")
 #define BSL_VERSION_MAJOR    3
     // Provide the major version number of the current (latest) BSL release.
 
-#define BSL_VERSION_MINOR    115
+#define BSL_VERSION_MINOR    116
     // Provide the minor version number of the current (latest) BSL release.
 
 #define BSL_MAKE_VERSION(major, minor) ((major) * 1000000 \
