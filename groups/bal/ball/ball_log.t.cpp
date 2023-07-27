7c421e82a886479975037cdff55a4d0812abdc45
Denis Sokolov <dsokolov7@bloomberg.net>
2023-07-27 13:47:23 -0400

    Cycle is removed from the 'ball_log' test driver (#4357)

--- groups/bal/ball/ball_log.t.cpp
+++ groups/bal/ball/ball_log.t.cpp
@@ -6,14 +6,13 @@
 #include <ball_attributecontainer.h>
 #include <ball_attributecontainerlist.h>
 #include <ball_attributecontext.h>
-#include <ball_scopedattribute.h>
 #include <ball_defaultattributecontainer.h>
-#include <ball_fileobserver2.h>
 #include <ball_loggermanagerconfiguration.h>
 #include <ball_predicate.h>
 #include <ball_record.h>
 #include <ball_recordstringformatter.h>
 #include <ball_rule.h>
+#include <ball_scopedattribute.h>
 #include <ball_streamobserver.h>
 #include <ball_testobserver.h>
 #include <ball_thresholdaggregate.h>
@@ -3378,7 +3377,7 @@ if (verbose) bsl::cout << "printf-style macro usage" << bsl::endl;
         // Plan:
         //: 1 Define three functions, 'recurseStreamBasedMacros',
         //:   'recursePrintfStyleMacros' and 'recurseCallbackMacros', that log
-        //:   using the stream-based macros, 'printf'-style macros and calback
+        //:   using the stream-based macros, 'printf'-style macros and callback
         //:   macros, respectively.  Call these three functions in the context
         //:   of invocations of the various logging macros and verify the
         //:   results.  (C-1, 2)
@@ -3393,8 +3392,7 @@ if (verbose) bsl::cout << "printf-style macro usage" << bsl::endl;
 
         using namespace BALL_LOG_TEST_CASE_35;
 
-        typedef BloombergLP::ball::FileObserver2  FileObserver;
-        typedef BloombergLP::bdls::FilesystemUtil FilesystemUtil;
+        typedef BloombergLP::ball::StreamObserver  StreamObserver;
 
         TestAllocator ta(veryVeryVeryVerbose);
 
@@ -3403,18 +3401,11 @@ if (verbose) bsl::cout << "printf-style macro usage" << bsl::endl;
 
         LoggerManager& manager = LoggerManager::singleton();
 
-        TempDirectoryGuard tempDirGuard("ball_");
+        bsl::ostringstream              os(&ta);
+        bsl::shared_ptr<StreamObserver> observer(
+                                       new (ta) StreamObserver(&os, &ta), &ta);
 
-        bsl::string baseName(tempDirGuard.getTempDirName());
-        BloombergLP::bdls::PathUtil::appendRaw(&baseName, "testLog");
-
-        if (veryVeryVerbose) { T_; T_; P(baseName); }
-
-        bsl::shared_ptr<FileObserver> observer(
-                                              new (ta) FileObserver(&ta), &ta);
-
-        observer->enableFileLogging(baseName.c_str(), false);
-        observer->setLogFileFunctor(
+        observer->setRecordFormatFunctor(
                              BloombergLP::ball::RecordStringFormatter("%m\n"));
 
         ASSERT(0 == manager.registerObserver(observer, "test"));
@@ -3468,23 +3459,11 @@ if (verbose) bsl::cout << "printf-style macro usage" << bsl::endl;
             BALL_LOGVA_INFO("%s %d", "Outer INFO[2]",
                             recursePrintfStyleMacros<2>(Sev::e_INFO));
 
-            FilesystemUtil::FileDescriptor fd = FilesystemUtil::open(
-                                                  baseName,
-                                                  FilesystemUtil::e_OPEN,
-                                                  FilesystemUtil::e_READ_ONLY);
+            bsl::string result = os.str();
 
-            const size_t READ_BUFFER_SIZE = 8192;
-            char         buffer[READ_BUFFER_SIZE];
-            memset(buffer, 0, READ_BUFFER_SIZE);
-
-            int numBytes = static_cast<int>(FilesystemUtil::read(
-                                                fd, buffer, READ_BUFFER_SIZE));
-
-            ASSERTV(EXPECTED_LENGTH, numBytes, EXPECTED_LENGTH == numBytes);
-            ASSERTV(EXPECTED_LOG, buffer,
-                    0 == bsl::strcmp(EXPECTED_LOG, buffer));
-
-            FilesystemUtil::close(fd);
+            ASSERTV(EXPECTED_LENGTH, result.length(),
+                    EXPECTED_LENGTH == result.length());
+            ASSERTV(EXPECTED_LOG, result, EXPECTED_LOG == result);
         }
 
         if (verbose) bsl::cout << "\tStream-based recurses to stream-based.\n";
