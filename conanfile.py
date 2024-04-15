40db3e9397f43e461c1d90a9423060db9cefbfb4
Oleg Subbotin <osubbotin@bloomberg.net>
2024-04-15 11:59:12 -0400

    Add conanfile.py (#4699)

---
+++ conanfile.py
@@ -0,0 +1,15 @@
+# ***************************************************************
+# This is an internal Bloomberg Conan recipe.                   *
+# This recipe does not work outside of Bloomberg infrastructure *
+# ***************************************************************
+
+import re
+import os
+from conan import ConanFile
+
+from conan.tools.cmake import CMake
+from conan.tools.files import collect_libs
+
+class Package(ConanFile):
+    python_requires = "conan-dpkg-recipe/[>=0.7]@test/unstable"
+    python_requires_extend = "conan-dpkg-recipe.BdeBuildSystem"
---
+++ debian/Readme.md
@@ -0,0 +1,5 @@
+# Debian metadata
+
+This folder contains debian metadata for internal Bloomberg use.
+
+This metadata cannot be used outside Bloomberg infrastructure.
