06edbbf119dbb2814bc8c20a4dcb46c403a5feef
Mungo Gill <mgill83@bloomberg.net>
2024-10-02 17:05:13 +0100

    add missing contracts

--- groups/bsl/bslfmt/bslfmt_formatparsecontext.h
+++ groups/bsl/bslfmt/bslfmt_formatparsecontext.h
@@ -3,6 +3,29 @@
 #ifndef INCLUDED_BSLFMT_FORMATPARSECONTEXT
 #define INCLUDED_BSLFMT_FORMATPARSECONTEXT
 
+#include <bsls_ident.h>
+BSLS_IDENT("$Id: $")
+
+//@PURPOSE: Provides access to formatting parsing string and parsing state.
+//
+//@CLASSES:
+//  bslfmt::basic_format_parse_context: compliant format spec parse context
+//
+//@DESCRIPTION: This component provides an implementation of the C++20 Standard
+// Library's `std::basic_format_parse_context`, which provides access to
+// formatting specification string, the parsing state, and the auto-indexing
+// argument counter (where applicable).
+//
+// This type is designed to be constructed from within `bslfmt::format` and
+// cannot be constructed directly from user code.
+//
+// As this type contains a pair of iterator types, therefore users must ensure
+// the lifetime of the format string used in construction does not end before
+// the last usage of objects of this type.
+//
+///Usage
+///-----
+
 #include <bslscm_version.h>
 
 #include <bslalg_numericformatterutil.h>
@@ -51,6 +74,9 @@ typedef basic_format_parse_context<wchar_t> wformat_parse_context;
                  // class basic_format_parse_context<t_CHAR>
                  // ----------------------------------------
 
+/// A format context for use by `formatter::parse()` partial specialization.
+/// It provides access to iterators for the start and end of the remaining
+/// (unparsed) format string, as well as argument counters.
 template <class t_CHAR>
 class basic_format_parse_context {
   public:
@@ -62,35 +88,66 @@ class basic_format_parse_context {
 
   private:
     // TYPES
-    enum Indexing { e_UNKNOWN, e_MANUAL, e_AUTOMATIC };
+    enum IndexingMode { e_UNKNOWN, e_MANUAL, e_AUTOMATIC };
 
     // DATA
-    iterator d_begin;
-    iterator d_end;
-    Indexing d_indexing;
-    size_t   d_next_arg_id;
-    size_t   d_num_args;
+    iterator     d_orig_begin;  // start of format string
+    iterator     d_unparsed;    // start of unparsed portion of format string
+    iterator     d_end;         // end of format string.
+    IndexingMode d_indexing;    // current indexing mode.
+    size_t       d_next_arg_id; // next arg id for automatic indexing
+    size_t       d_num_args;    // number of args
 
   public:
     // CREATORS
+
+    /// Create an instance of this type with the specified `fmt` parse string
+    /// and number of arguments set to zero. The string is stored by reference,
+    /// so the user is required to ensure that the lifetime of `fmt` outlives
+    /// the lifetime of this object.
     BSLS_KEYWORD_CONSTEXPR_CPP20 explicit basic_format_parse_context(
                      bsl::basic_string_view<t_CHAR> fmt) BSLS_KEYWORD_NOEXCEPT;
 
+    /// Create an instance of this type with the specified `fmt` parse string
+    /// and number of arguments set to the specified `numArgs`. The string is
+    /// stored by reference, so the user is required to ensure that the lifetime
+    /// of `fmt` outlives the lifetime of this object.
     BSLS_KEYWORD_CONSTEXPR_CPP20 explicit basic_format_parse_context(
                  bsl::basic_string_view<t_CHAR> fmt,
                  size_t                         numArgs) BSLS_KEYWORD_NOEXCEPT;
 
     // MANIPULATORS
+
+    /// Update the held iterator to the unparsed portion of the format string
+    /// to be the specified `it`. Subsequent calls to `begin` will return this
+    /// value. `it` must be a valid iterator to the format string used in
+    /// construction of this context.
     BSLS_KEYWORD_CONSTEXPR_CPP20 void advance_to(const_iterator it);
 
+    /// Return the index of the next-unused argument. If the indexing mode is
+    /// unset, the indexing mode is set to `e_AUTOMATIC`. If the indexing mode
+    /// is `e_MANUAL`, caused by a previous call to `check_arg_id` then an
+    /// exception of type `format_error` is thrown. If there are no remaining
+    /// unused arguments then an exception of type `format_error` is thrown.
     BSLS_KEYWORD_CONSTEXPR_CPP20 size_t next_arg_id();
 
+    /// Check that the specified `id` is a valid argument index. If the
+    /// indexing mode is unset, the indexing mode is set to `e_MANUAL`. If the
+    /// indexing mode is `e_AUTOMATIC`, caused by a previous call to
+    /// `next_arg_id` then an exception of type `format_error` is thrown. If
+    /// `id` is not less than the number of arguments then an exception of type
+    /// `format_error` is thrown.
     BSLS_KEYWORD_CONSTEXPR_CPP20 void check_arg_id(size_t id);
 
     // ACCESSORS
+
+    /// Return an iterator to the start of the unparsed portion of the format
+    /// string.
     BSLS_KEYWORD_CONSTEXPR_CPP20 const_iterator
     begin() const BSLS_KEYWORD_NOEXCEPT;
 
+    /// Return an iterator to the start of the unparsed portion of the format
+    /// string.
     BSLS_KEYWORD_CONSTEXPR_CPP20 const_iterator
     end() const BSLS_KEYWORD_NOEXCEPT;
 
@@ -109,13 +166,18 @@ class basic_format_parse_context {
                // class Format_ParseContext_Test_Querier
                // --------------------------------------
 
+/// This component-private time enables the indexing mode of a
+/// `basic_parse_context` type to be inspected for the purposes of unit
+/// testing.
 struct Format_ParseContext_Test_Querier {
     // TYPES
-    enum Indexing { e_UNKNOWN, e_MANUAL, e_AUTOMATIC };
+    enum IndexingMode { e_UNKNOWN, e_MANUAL, e_AUTOMATIC };
 
     // CLASS METHODS
+
+    /// Return the indexing mode of the context specified by `parseContext`.
     template <class t_CHAR>
-    static Indexing queryIndexing(
+    static IndexingMode queryIndexingMode(
                        const basic_format_parse_context<t_CHAR>& parseContext);
 };
 
@@ -133,7 +195,8 @@ template <class t_CHAR>
 BSLS_KEYWORD_CONSTEXPR_CPP20
 basic_format_parse_context<t_CHAR>::basic_format_parse_context(
                       bsl::basic_string_view<t_CHAR> fmt) BSLS_KEYWORD_NOEXCEPT
-: d_begin(fmt.begin())
+: d_orig_begin(fmt.begin())
+, d_unparsed(fmt.begin())
 , d_end(fmt.end())
 , d_indexing(e_UNKNOWN)
 , d_next_arg_id(0)
@@ -146,7 +209,8 @@ BSLS_KEYWORD_CONSTEXPR_CPP20
 basic_format_parse_context<t_CHAR>::basic_format_parse_context(
                   bsl::basic_string_view<t_CHAR> fmt,
                   size_t                         numArgs) BSLS_KEYWORD_NOEXCEPT
-: d_begin(fmt.begin())
+: d_orig_begin(fmt.begin())
+, d_unparsed(fmt.begin())
 , d_end(fmt.end())
 , d_indexing(e_UNKNOWN)
 , d_next_arg_id(0)
@@ -159,7 +223,13 @@ template <class t_CHAR>
 BSLS_KEYWORD_CONSTEXPR_CPP20
 void basic_format_parse_context<t_CHAR>::advance_to(const_iterator it)
 {
-    d_begin = it;
+    // This check is safe as, in our implementation, it is guaranteed that
+    // const_iterator is a random access iterator.
+    if (!((it >= d_orig_begin) && (it <= d_end))) {
+        BSLS_THROW(
+               format_error("parse error - advance to iterator out of range"));
+    }
+    d_unparsed = it;
 }
 
 template <class t_CHAR>
@@ -200,7 +270,7 @@ BSLS_KEYWORD_CONSTEXPR_CPP20
 typename basic_format_parse_context<t_CHAR>::const_iterator
 basic_format_parse_context<t_CHAR>::begin() const BSLS_KEYWORD_NOEXCEPT
 {
-    return d_begin;
+    return d_unparsed;
 }
 
 template <class t_CHAR>
@@ -216,8 +286,8 @@ basic_format_parse_context<t_CHAR>::end() const BSLS_KEYWORD_NOEXCEPT
                    // --------------------------------------
 
 template <class t_CHAR>
-Format_ParseContext_Test_Querier::Indexing
-Format_ParseContext_Test_Querier::queryIndexing(
+Format_ParseContext_Test_Querier::IndexingMode
+Format_ParseContext_Test_Querier::queryIndexingMode(
                         const basic_format_parse_context<t_CHAR>& parseContext)
 {
     switch (parseContext.d_indexing) {
