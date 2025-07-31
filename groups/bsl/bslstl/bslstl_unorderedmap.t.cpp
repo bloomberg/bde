396816c55f9169d1c757f702db92a6eafca9a0e8
Denis Sokolov <dsokolov7@bloomberg.net>
2025-07-31 10:21:14 -0400

    Matrix build issues on clang-18 have been fixed (#5509)

--- groups/bsl/bslstl/bslstl_unorderedmap.t.cpp
+++ groups/bsl/bslstl/bslstl_unorderedmap.t.cpp
@@ -10721,18 +10721,18 @@ int main(int argc, char *argv[])
                       bsltf::MovableAllocTestType);
 
 #if BSLS_COMPILERFEATURES_CPLUSPLUS < 202002L ||                              \
-    !defined(BSLS_PLATFORM_CMP_CLANG) ||                                      \
-    !defined(BSLS_LIBRARYFEATURES_STDCPP_LLVM)
-// clang on Darwin (and other platforms where it uses libc++) is unable to
-// compile code in this test case due to the key type being move-only.  For
-// further details see {DRQS 177180189<GO>}, the underlying problem is complex.
+    !(defined(BSLS_PLATFORM_CMP_CLANG)                                        \
+  && BSLS_PLATFORM_CMP_VERSION >= 170000L)
+// clang version 17 and higher is unable to compile code in this test case due
+// to the key type being move-only.  For further details see
+// {DRQS 177180189<GO>}, the underlying problem is complex.
 #if defined(BSLMF_MOVABLEREF_USES_RVALUE_REFERENCES)
         RUN_EACH_TYPE(TestDriver,
                       testCase8,
                       bsltf::MoveOnlyAllocTestType,
                       bsltf::WellBehavedMoveOnlyAllocTestType);
 #endif
-#endif  // Not clang with libc++ in C++20 or later
+#endif  // Not clang-17 and higher in C++20 or later
 
         TestDriver<TestKeyType, TestValueType>::testCase8();
 
