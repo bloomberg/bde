5e23227ed52d788fa0bd9198a68bf7e141c39570
Alisdair Meredith <ameredith1@bloomberg.net>
2026-02-05 14:58:55 -0500

    Fix test driver for bslstl_function_invokerutil on Clang (#5738)
    The failing test case detects and expects a bug from the Clang compiler
    that has since been fixed.  Rather than write a more detailed platform
    detection, note that we are testing for the presence of a C++20 language
    feature that is covered by a Standard feature macro, so create a new
    constant to detect that capability instead, and use that in the test case.

--- groups/bsl/bslstl/bslstl_function_invokerutil.t.cpp
+++ groups/bsl/bslstl/bslstl_function_invokerutil.t.cpp
@@ -113,37 +113,37 @@ static bool veryVeryVeryVerbose;
 // ----------------------------------------------------------------------------
 
 #if BSLS_COMPILERFEATURES_CPLUSPLUS >= 202002L
-#define CPP_20 1
+# define CPP_20 1
 #else
-#define CPP_20 0
+# define CPP_20 0
 #endif
 
 #if defined(BSLS_PLATFORM_CMP_MSVC)
-#define MSVC 1
+# define MSVC 1
 #else
-#define MSVC 0
+# define MSVC 0
 #endif
 
 #if defined(BSLS_PLATFORM_CMP_MSVC) && BSLS_PLATFORM_CMP_VERSION == 1900
-#define MSVC_2015 1
+# define MSVC_2015 1
 #else
-#define MSVC_2015 0
+# define MSVC_2015 0
 #endif
 
 #if defined(BSLS_PLATFORM_CMP_MSVC)   \
  && BSLS_PLATFORM_CMP_VERSION >= 1910 \
  && BSLS_PLATFORM_CMP_VERSION <= 1916
-#define MSVC_2017 1
+# define MSVC_2017 1
 #else
-#define MSVC_2017 0
+# define MSVC_2017 0
 #endif
 
 #if defined(BSLS_PLATFORM_CMP_MSVC)   \
  && BSLS_PLATFORM_CMP_VERSION >= 1920 \
  && BSLS_PLATFORM_CMP_VERSION <  1929
-#define MSVC_2019 1
+# define MSVC_2019 1
 #else
-#define MSVC_2019 0
+# define MSVC_2019 0
 #endif
 
 #if defined(BSLS_PLATFORM_CMP_MSVC) &&                                        \
@@ -151,22 +151,28 @@ static bool veryVeryVeryVerbose;
      BSLS_PLATFORM_CMP_VERSION <= 1929)
     // MSVC displays inconsistent behavior with reference parameters. This was
     // fixed in MSVC 1930 (VS 2022) but only in C++20 mode.
-#define MS_REF_BUG 1
+# define MS_REF_BUG 1
 #else
-#define MS_REF_BUG 0
+# define MS_REF_BUG 0
 #endif
 
 #if defined(BSLS_PLATFORM_CMP_CLANG)
-#define CLANG 1
+# define CLANG 1
 #else
-#define CLANG 0
+# define CLANG 0
 #endif
 
 #if defined(BSLS_PLATFORM_CMP_GNU)      \
  && BSLS_PLATFORM_CMP_VERSION >= 100000
-#define GNU_10PLUS 1
+# define GNU_10PLUS 1
 #else
-#define GNU_10PLUS 0
+# define GNU_10PLUS 0
+#endif
+
+#if defined(__cpp_aggregatee_paren_init)
+# define PAR_INIT 1
+#else
+# define PAR_INIT 0
 #endif
 
 typedef bslstl::Function_InvokerUtil             Util;
@@ -1775,11 +1781,11 @@ int main(int argc, char *argv[])
           TEST.run<rrcD (     ),    rrB      (     )  >(L_, YES);
           TEST.run<rrcD (     ),   rrcB      (     )  >(L_, YES);
 
-          TEST.run<   DA(     ),      BA     (     )  >(L_, CPP_20 && !CLANG);
-          TEST.run<   DA(     ),     rBA     (     )  >(L_, CPP_20 && !CLANG);
-          TEST.run<   DA(     ),    rcBA     (     )  >(L_, CPP_20 && !CLANG);
-          TEST.run<   DA(     ),    rrBA     (     )  >(L_, CPP_20 && !CLANG);
-          TEST.run<   DA(     ),   rrcBA     (     )  >(L_, CPP_20 && !CLANG);
+          TEST.run<   DA(     ),      BA     (     )  >(L_, !PAR_INIT);
+          TEST.run<   DA(     ),     rBA     (     )  >(L_, !PAR_INIT);
+          TEST.run<   DA(     ),    rcBA     (     )  >(L_, !PAR_INIT);
+          TEST.run<   DA(     ),    rrBA     (     )  >(L_, !PAR_INIT);
+          TEST.run<   DA(     ),   rrcBA     (     )  >(L_, !PAR_INIT);
           TEST.run<  rDA(     ),      BA     (     )  >(L_, MS_REF_BUG);
           TEST.run<  rDA(     ),     rBA     (     )  >(L_, YES);
           TEST.run<  rDA(     ),    rcBA     (     )  >(L_, NO );
