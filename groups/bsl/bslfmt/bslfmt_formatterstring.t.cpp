752ee82ce362a70eeebac0e6dfe1c968d5f757f4
Jeffrey Mendelsohn <JMENDELSOHN4@bloomberg.net>
2026-02-12 14:09:49 -0500

    reduce case 4 spew (#5755)

--- groups/bsl/bslfmt/bslfmt_formatterstring.t.cpp
+++ groups/bsl/bslfmt/bslfmt_formatterstring.t.cpp
@@ -750,7 +750,16 @@ void TestDriver<t_CHAR>::testCase4SingleValueType(
             ASSERTV(generator.formatSpec().c_str(), !rv);
         }
         else {
-            ASSERTV(generator.formatSpec().c_str(), message->c_str(), rv);
+            // the following test vector has a tendancy to spew
+            if (!rv) {
+                static int failures = 0;
+                ++failures;
+                if (verbose || failures < 50) {
+                    ASSERTV(generator.formatSpec().c_str(),
+                            message->c_str(),
+                            rv);
+                }
+            }
         }
     }
     else {
