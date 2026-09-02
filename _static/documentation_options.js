5ac7d917ebbe86d8981bb011d81a5104398b735e
Oleg Subbotin <osubbotin@bloomberg.net>
2026-09-02 17:10:38 +0000

    Updated. BDE 4.39.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,6 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: '',
-    LANGUAGE: 'None',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
